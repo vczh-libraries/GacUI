301d28404b3a881ee4fc2732403600432247e4d8
vczh <vczh@163.com>
2015-04-05 20:36:07 -0700

    ParseMiniType

---
+++ Document/Parser/Parser/Parser.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parser
+{
+    class Parser
+    {
+        public static bool Token(string[] tokens, ref int index, string expectedToken)
+        {
+            if (index >= tokens.Length) return false;
+            if (tokens[index] != expectedToken) return false;
+
+            index++;
+            return true;
+        }
+
+        public static bool Id(string[] tokens, ref int index, out string token)
+        {
+            token = null;
+            if (index >= tokens.Length) return false;
+            var c = tokens[index][0];
+            if ('a' <= c && c <= 'z' || 'A' <= c && c <= 'Z' || c == '_')
+            {
+                token = tokens[index++];
+                return true;
+            }
+            return false;
+        }
+    }
+}
--- Document/Parser/Parser/TypeDecl.cs
+++ Document/Parser/Parser/TypeDecl.cs
@@ -8,6 +8,145 @@ namespace Parser
 {
     class TypeDecl
     {
+        public static bool ParseMiniType(string[] tokens, ref int index, out TypeDecl decl)
+        {
+            decl = null;
+            if (Parser.Token(tokens, ref index, "const"))
+            {
+                TypeDecl element = null;
+                if (!ParseMiniType(tokens, ref index, out element))
+                {
+                    throw new ArgumentException("Failed to parse.");
+                }
+                decl = new DecorateTypeDecl
+                {
+                    Decoration = Decoration.Const,
+                    Element = element,
+                };
+                return true;
+            }
+            else if (Parser.Token(tokens, ref index, "volatile"))
+            {
+                TypeDecl element = null;
+                if (!ParseMiniType(tokens, ref index, out element))
+                {
+                    throw new ArgumentException("Failed to parse.");
+                }
+                decl = new DecorateTypeDecl
+                {
+                    Decoration = Decoration.Volatile,
+                    Element = element,
+                };
+                return true;
+            }
+            else if (Parser.Token(tokens, ref index, "signed"))
+            {
+                TypeDecl element = null;
+                if (!ParseMiniType(tokens, ref index, out element))
+                {
+                    throw new ArgumentException("Failed to parse.");
+                }
+                decl = new DecorateTypeDecl
+                {
+                    Decoration = Decoration.Signed,
+                    Element = element,
+                };
+                return true;
+            }
+            else if (Parser.Token(tokens, ref index, "unsigned"))
+            {
+                TypeDecl element = null;
+                if (!ParseMiniType(tokens, ref index, out element))
+                {
+                    throw new ArgumentException("Failed to parse.");
+                }
+                decl = new DecorateTypeDecl
+                {
+                    Decoration = Decoration.Unsigned,
+                    Element = element,
+                };
+                return true;
+            }
+            else
+            {
+                string token = null;
+                if (Parser.Id(tokens, ref index, out token))
+                {
+                    decl = new RefTypeDecl
+                    {
+                        Name = token,
+                    };
+
+                    while (true)
+                    {
+                        if (Parser.Token(tokens, ref index, "<"))
+                        {
+                            var genericDecl = new GenericTypeDecl
+                            {
+                                Element = decl,
+                                TypeArguments = new List<TypeDecl>(),
+                            };
+                            decl = genericDecl;
+                            while (true)
+                            {
+                                if (Parser.Token(tokens, ref index, ">"))
+                                {
+                                    break;
+                                }
+                                else if (Parser.Token(tokens, ref index, ","))
+                                {
+                                    TypeDecl typeArgument = null;
+                                    string name = null;
+                                    if (!ParseType(tokens, ref index, out typeArgument, out name))
+                                    {
+                                        throw new ArgumentException("Failed to parse");
+                                    }
+                                    genericDecl.TypeArguments.Add(typeArgument);
+                                }
+                                else
+                                {
+                                    throw new ArgumentException("Failed to parse.");
+                                }
+                            }
+                        }
+                        else if (Parser.Token(tokens, ref index, ":"))
+                        {
+                            if (!Parser.Token(tokens, ref index, ":"))
+                            {
+                                throw new ArgumentException("Failed to parse.");
+                            }
+                            if (Parser.Id(tokens, ref index, out token))
+                            {
+                                decl = new SubTypeDecl
+                                {
+                                    Parent = decl,
+                                    Name = token,
+                                };
+                            }
+                            else
+                            {
+                                throw new ArgumentException("Failed to parse.");
+                            }
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        public static bool ParseType(string[] tokens, ref int index, out TypeDecl decl, out string name)
+        {
+            throw new NotImplementedException();
+        }
     }
 
     class RefTypeDecl : TypeDecl
@@ -27,6 +166,8 @@ namespace Parser
         Volatile,
         Reference,
         Pointer,
+        Signed,
+        Unsigned,
     }
 
     class DecorateTypeDecl : TypeDecl
@@ -43,6 +184,7 @@ namespace Parser
 
     enum CallingConvention
     {
+        Default,
         CDecl,
         ClrCall,
         StdCall,
@@ -56,5 +198,11 @@ namespace Parser
         public TypeDecl ReturnType { get; set; }
         public TypeDecl ClassType { get; set; }
         public List<VarDecl> Parameters { get; set; }
+    }
+
+    class GenericTypeDecl : TypeDecl
+    {
+        public TypeDecl Element { get; set; }
+        public List<TypeDecl> TypeArguments { get; set; }
     }
 }
