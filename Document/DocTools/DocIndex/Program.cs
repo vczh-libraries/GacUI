b1aa1c1bc2e7d3a8036feeec2f6ef636e45ee810
vczh <vczh@163.com>
2015-05-29 04:34:19 -0700

    ...

--- Document/DocTools/DocIndex/Program.cs
+++ Document/DocTools/DocIndex/Program.cs
@@ -23,21 +23,26 @@ namespace DocIndex
                     g => g.SelectMany(xmlNs => xmlNs.Elements("Symbol")).ToArray()
                 );
 
-            var symbols = new Dictionary<string, SymbolDecl[]>();
-            foreach (var nss in xmlNss)
-            {
-                var ns = nss.Key;
-                var symbolDecls = nss.Value
-                    .Select(xmlSymbol =>
+            var symbols = xml.Root
+                .Elements("Namespace")
+                .GroupBy(xmlNs => xmlNs.Attribute("Name").Value)
+                .ToDictionary(
+                    g => g.Key,
+                    g =>
                     {
-                        var xmlDecl = xmlSymbol.Elements().First();
-                        var decl = SymbolDecl.Deserialize(xmlDecl);
-                        decl.BuildSymbolTree(null, null);
-                        return decl;
-                    })
-                    .ToArray();
-                symbols.Add(ns, symbolDecls);
-            }
+                        return g
+                            .SelectMany(xmlNs => xmlNs.Elements("Symbol"))
+                            .Select(xmlSymbol =>
+                            {
+                                var key = xmlSymbol.Attribute("OverloadKey").Value;
+                                var xmlDecl = xmlSymbol.Elements().First();
+                                var decl = SymbolDecl.Deserialize(xmlDecl);
+                                decl.BuildSymbolTree(null, null);
+                                return Tuple.Create(key, decl);
+                            })
+                            .ToArray();
+                    }
+                );
 
             var output = Path.GetFullPath(args[1]);
             if (!output.EndsWith("\\"))
@@ -46,8 +51,7 @@ namespace DocIndex
             }
 
             Console.WriteLine("Writing nss.xml ...");
-            var nssFileNames = xmlNss
-                .Select(p => p.Key)
+            var namespaceFileNames = symbols.Keys
                 .ToDictionary(
                     ns => ns,
                     ns => "ns("
@@ -58,9 +62,12 @@ namespace DocIndex
                 );
             var outputNss = new XDocument(
                 new XElement("Namespaces",
-                    nssFileNames
+                    namespaceFileNames
+                        .OrderBy(p => p.Key)
                         .Select(p =>
                             new XElement("Namespace",
+                                new XAttribute("Key", p.Key),
+                                new XAttribute("DisplayName", (p.Key == "" ? "::" : p.Key.Substring(2)) + " Namespace"),
                                 new XAttribute("FileName", p.Value)
                             )
                         )
@@ -69,6 +76,44 @@ namespace DocIndex
             outputNss.Save(output + "nss.xml");
 
             Console.WriteLine("Writing ns(*).xml ...");
+            var symbolFileNames = symbols
+                .SelectMany(x => x.Value.Select(t => t.Item1))
+                .ToDictionary(
+                    x => x,
+                    x => x
+                        .Replace("*", "[ptr]")
+                        .Replace("?", "[q]")
+                        .Replace("<", "[lt]")
+                        .Replace(">", "[gt]")
+                        .Replace(":", "[colon]")
+                );
+            foreach (var nsp in symbols)
+            {
+                var outputNs = new XDocument(
+                    new XElement("Namespace",
+                        new XAttribute("DisplayName", nsp.Key),
+                        nsp.Value
+                            .GroupBy(t => GetDisplayName(t.Item2))
+                            .OrderBy(g => g.Key)
+                            .Select(g => new XElement("Overloads",
+                                new XAttribute("DisplayName", g.Key),
+                                g
+                                    .Select(t => new XElement("Symbol",
+                                        new XAttribute("Key", t.Item1),
+                                        new XAttribute("FileName", symbolFileNames[t.Item1])
+                                        )
+                                    )
+                                )
+                            )
+                        )
+                    );
+                outputNs.Save(output + namespaceFileNames[nsp.Key]);
+            }
+        }
+
+        static string GetDisplayName(SymbolDecl decl)
+        {
+            return "";
         }
     }
 }
