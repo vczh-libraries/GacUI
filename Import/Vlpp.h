156390f1d7b0152d727d5874880d443bdc835c3a
Zihan Chen <vczh@163.com>
2017-01-31 18:52:46 -0800

    ...

--- Import/Vlpp.h
+++ Import/Vlpp.h
@@ -12031,7 +12031,7 @@ TypeInfoRetriver Helper Functions (BoxValue, UnboxValue)
 			template<typename T>
 			Value BoxValue(const T& object, ITypeDescriptor* typeDescriptor=0)
 			{
-				using Type = RemoveCVR<T>::Type;
+				using Type = typename RemoveCVR<T>::Type;
 				return ValueAccessor<Type, TypeInfoRetriver<Type>::Decorator>::BoxValue(object, typeDescriptor);
 			}
 			
@@ -12044,7 +12044,7 @@ TypeInfoRetriver Helper Functions (BoxValue, UnboxValue)
 			template<typename T>
 			T UnboxValue(const Value& value, ITypeDescriptor* typeDescriptor=0, const WString& valueName=L"value")
 			{
-				using Type = RemoveCVR<T>::Type;
+				using Type = typename RemoveCVR<T>::Type;
 				return ValueAccessor<Type, TypeInfoRetriver<Type>::Decorator>::UnboxValue(value, typeDescriptor, valueName);
 			}
 
