b4a376ef8d8c2d67ce2b2c89a537034a90cb7992
Zihan Chen <vczh@163.com>
2017-06-20 17:19:14 -0700

    CompressStyleRunVisitor

--- Source/Resources/GuiDocumentEditor_ClearUnnecessaryRun.cpp
+++ Source/Resources/GuiDocumentEditor_ClearUnnecessaryRun.cpp
@@ -5,6 +5,7 @@ namespace vl
 	namespace presentation
 	{
 		using namespace collections;
+		using namespace document_editor;
 
 /***********************************************************************
 Clear all runs that have an empty length
@@ -78,12 +79,100 @@ Clear all runs that have an empty length
 		}
 		using namespace document_operation_visitors;
 
+/***********************************************************************
+Compress run to merge styles
+***********************************************************************/
+
+		namespace document_operation_visitors
+		{
+			class CompressStyleRunVisitor : public Object, public DocumentRun::IVisitor
+			{
+			public:
+				List<Ptr<DocumentRun>>			replacedRuns;
+
+				void VisitContainer(DocumentContainerRun* run)
+				{
+					for (vint i = run->runs.Count() - 1; i >= 0; i--)
+					{
+						Ptr<DocumentRun> subRun = run->runs[i];
+						replacedRuns.Clear();
+						subRun->Accept(this);
+						if (replacedRuns.Count() > 0)
+						{
+							run->runs.RemoveAt(i);
+							for (vint j = 0; j < replacedRuns.Count(); j++)
+							{
+								run->runs.Insert(i + j, replacedRuns[j]);
+							}
+							i += replacedRuns.Count();
+						}
+					}
+				}
+
+				void Visit(DocumentTextRun* run)override
+				{
+				}
+
+				void Visit(DocumentStylePropertiesRun* run)override
+				{
+					VisitContainer(run);
+					if (From(run->runs).Cast<DocumentStylePropertiesRun>().First(nullptr) != nullptr)
+					{
+						FOREACH(Ptr<DocumentRun>, subRun, run->runs)
+						{
+							if (auto styleRun = subRun.Cast<DocumentStylePropertiesRun>())
+							{
+								DocumentModel::MergeStyle(styleRun->style, run->style);
+								replacedRuns.Add(styleRun);
+							}
+							else
+							{
+								auto parentRun = CopyRun(run).Cast<DocumentStylePropertiesRun>();
+								parentRun->runs.Add(subRun);
+								replacedRuns.Add(parentRun);
+							}
+						}
+					}
+				}
+
+				void Visit(DocumentStyleApplicationRun* run)override
+				{
+					VisitContainer(run);
+				}
+
+				void Visit(DocumentHyperlinkRun* run)override
+				{
+					VisitContainer(run);
+				}
+
+				void Visit(DocumentImageRun* run)override
+				{
+				}
+
+				void Visit(DocumentEmbeddedObjectRun* run)override
+				{
+				}
+
+				void Visit(DocumentParagraphRun* run)override
+				{
+					VisitContainer(run);
+				}
+			};
+		}
+		using namespace document_operation_visitors;
+
 		namespace document_editor
 		{
 			void ClearUnnecessaryRun(DocumentParagraphRun* run)
 			{
-				ClearRunVisitor visitor;
-				run->Accept(&visitor);
+				{
+					ClearRunVisitor visitor;
+					run->Accept(&visitor);
+				}
+				{
+					CompressStyleRunVisitor visitor;
+					run->Accept(&visitor);
+				}
 			}
 		}
 	}
