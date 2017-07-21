d5ca51662a1d56df672f0e577b305fca484e2bd4
Zihan Chen <vczh@163.com>
2017-07-20 21:45:09 -0700

    (NOT FINISHED) Manual DarkSkin theme object

--- Test/GacUISrc/TestCppCodegen/Main.cpp
+++ Test/GacUISrc/TestCppCodegen/Main.cpp
@@ -9,7 +9,187 @@ using namespace vl::stream;
 using namespace vl::reflection::description;
 using namespace vl::presentation;
 using namespace vl::presentation::controls;
+using namespace vl::presentation::elements;
+using namespace vl::presentation::templates;
 using namespace demo;
+using namespace darkskin;
+
+class DarkSkin : public Object, public virtual theme::ITheme
+{
+public:
+	GuiWindow::IStyleController* CreateWindowStyle()override
+	{
+	}
+
+	GuiCustomControl::IStyleController* CreateCustomControlStyle()override
+	{
+	}
+
+	GuiTooltip::IStyleController* CreateTooltipStyle()override
+	{
+	}
+
+	GuiLabel::IStyleController* CreateLabelStyle()override
+	{
+	}
+
+	GuiLabel::IStyleController* CreateShortcutKeyStyle()override
+	{
+	}
+
+	GuiScrollContainer::IStyleProvider* CreateScrollContainerStyle()override
+	{
+	}
+
+	GuiControl::IStyleController* CreateGroupBoxStyle()override
+	{
+	}
+
+	GuiTab::IStyleController* CreateTabStyle()override
+	{
+	}
+
+	GuiComboBoxListControl::IStyleController* CreateComboBoxStyle()override
+	{
+	}
+
+	GuiScrollView::IStyleProvider* CreateMultilineTextBoxStyle()override
+	{
+	}
+
+	GuiSinglelineTextBox::IStyleProvider* CreateTextBoxStyle()override
+	{
+	}
+
+	text::ColorEntry GetDefaultTextBoxColorEntry()override
+	{
+	}
+
+	GuiDocumentViewer::IStyleProvider* CreateDocumentViewerStyle()override
+	{
+	}
+
+	GuiDocumentLabel::IStyleController* CreateDocumentLabelStyle()override
+	{
+	}
+
+	GuiDocumentLabel::IStyleController* CreateDocumentTextBoxStyle()override
+	{
+	}
+
+	GuiListView::IStyleProvider* CreateListViewStyle()override
+	{
+	}
+
+	GuiTreeView::IStyleProvider* CreateTreeViewStyle()override
+	{
+	}
+
+	GuiSelectableButton::IStyleController* CreateListItemBackgroundStyle()override
+	{
+	}
+
+	GuiSelectableButton::IStyleController* CreateTreeItemExpanderStyle()override
+	{
+	}
+
+	GuiToolstripMenu::IStyleController* CreateMenuStyle()override
+	{
+	}
+
+	GuiToolstripMenuBar::IStyleController* CreateMenuBarStyle()override
+	{
+	}
+
+	GuiControl::IStyleController* CreateMenuSplitterStyle()override
+	{
+	}
+
+	GuiToolstripButton::IStyleController* CreateMenuBarButtonStyle()override
+	{
+	}
+
+	GuiToolstripButton::IStyleController* CreateMenuItemButtonStyle()override
+	{
+	}
+
+	GuiToolstripToolBar::IStyleController* CreateToolBarStyle()override
+	{
+	}
+
+	GuiToolstripButton::IStyleController* CreateToolBarButtonStyle()override
+	{
+	}
+
+	GuiToolstripButton::IStyleController* CreateToolBarDropdownButtonStyle()override
+	{
+	}
+
+	GuiToolstripButton::IStyleController* CreateToolBarSplitButtonStyle()override
+	{
+	}
+
+	GuiControl::IStyleController* CreateToolBarSplitterStyle()override
+	{
+	}
+
+	GuiButton::IStyleController* CreateButtonStyle()override
+	{
+	}
+
+	GuiSelectableButton::IStyleController* CreateCheckBoxStyle()override
+	{
+	}
+
+	GuiSelectableButton::IStyleController* CreateRadioButtonStyle()override
+	{
+	}
+
+	GuiDatePicker::IStyleProvider* CreateDatePickerStyle()override
+	{
+	}
+
+	GuiScroll::IStyleController* CreateHScrollStyle()override
+	{
+	}
+
+	GuiScroll::IStyleController* CreateVScrollStyle()override
+	{
+	}
+
+	GuiScroll::IStyleController* CreateHTrackerStyle()override
+	{
+	}
+
+	GuiScroll::IStyleController* CreateVTrackerStyle()override
+	{
+	}
+
+	GuiScroll::IStyleController* CreateProgressBarStyle()override
+	{
+	}
+
+	vint GetScrollDefaultSize()override
+	{
+	}
+
+	vint GetTrackerDefaultSize()override
+	{
+	}
+
+	GuiVirtualTextList::IStyleProvider* CreateTextListStyle()override
+	{
+	}
+
+	GuiSelectableButton::IStyleController* CreateCheckTextListItemStyle()override
+	{
+	}
+
+	GuiSelectableButton::IStyleController* CreateRadioTextListItemStyle()override
+	{
+	}
+
+};
 
 void GuiMain()
 {
@@ -20,14 +200,21 @@ void GuiMain()
 	}
 
 	LoadDemoTypes();
-	demo::MainWindow window;
-	window.ForceCalculateSizeImmediately();
-	window.MoveToScreenCenter();
-	GetApplication()->Run(&window);
+
+	DarkSkin darkSkin;
+	auto oldTheme = theme::GetCurrentTheme();
+	theme::SetCurrentTheme(&darkSkin);
+	{
+		demo::MainWindow window;
+		window.ForceCalculateSizeImmediately();
+		window.MoveToScreenCenter();
+		GetApplication()->Run(&window);
+	}
+	theme::SetCurrentTheme(oldTheme);
 }
 
-//#define GUI_GRAPHICS_RENDERER_GDI
-#define GUI_GRAPHICS_RENDERER_DIRECT2D
+#define GUI_GRAPHICS_RENDERER_GDI
+//#define GUI_GRAPHICS_RENDERER_DIRECT2D
 
 int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int CmdShow)
 {
