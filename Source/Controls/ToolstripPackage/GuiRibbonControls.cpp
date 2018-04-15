f98191aab9e010ec50f4022730029a74b3ebcca6
Zihan Chen <vczh@163.com>
2018-04-14 19:41:49 -0700

    ...

--- Source/Controls/ToolstripPackage/GuiRibbonControls.cpp
+++ Source/Controls/ToolstripPackage/GuiRibbonControls.cpp
@@ -75,12 +75,62 @@ GuiRibbonTab
 			}
 
 /***********************************************************************
+GuiRibbonGroupCollection
+***********************************************************************/
+
+			bool GuiRibbonGroupCollection::QueryInsert(vint index, GuiRibbonGroup* const& value)
+			{
+				return !value->GetBoundsComposition()->GetParent();
+			}
+
+			void GuiRibbonGroupCollection::AfterInsert(vint index, GuiRibbonGroup* const& value)
+			{
+				value->GetBoundsComposition()->SetAlignmentToParent(Margin(0, 0, 0, 0));
+
+				auto item = new GuiStackItemComposition();
+				item->AddChild(value->GetBoundsComposition());
+
+				tabPage->stack->InsertStackItem(index, item);
+			}
+
+			void GuiRibbonGroupCollection::AfterRemove(vint index, vint count)
+			{
+				auto item = tabPage->stack->GetStackItems()[index];
+				tabPage->stack->RemoveChild(item);
+
+				item->RemoveChild(item->Children()[0]);
+				delete item;
+			}
+
+			GuiRibbonGroupCollection::GuiRibbonGroupCollection(GuiRibbonTabPage* _tabPage)
+				:tabPage(_tabPage)
+			{
+			}
+
+			GuiRibbonGroupCollection::~GuiRibbonGroupCollection()
+			{
+			}
+
+/***********************************************************************
 GuiRibbonTabPage
 ***********************************************************************/
 
 			GuiRibbonTabPage::GuiRibbonTabPage(theme::ThemeName themeName)
 				:GuiTabPage(themeName)
+				, groups(this)
 			{
+				stack = new GuiStackComposition();
+				stack->SetDirection(GuiStackComposition::Horizontal);
+				stack->SetAlignmentToParent(Margin(0, 0, 0, 0));
+				stack->SetMinSizeLimitation(GuiGraphicsComposition::LimitToElementAndChildren);
+
+				responsiveStack = new GuiResponsiveStackComposition();
+				responsiveStack->SetDirection(ResponsiveDirection::Horizontal);
+				responsiveStack->SetAlignmentToParent(Margin(0, 0, 0, 0));
+				responsiveStack->AddChild(stack);
+
+				containerComposition->AddChild(responsiveStack);
+
 				HighlightedChanged.SetAssociatedComposition(boundsComposition);
 			}
 
@@ -100,6 +150,11 @@ GuiRibbonTabPage
 					highlighted = value;
 					HighlightedChanged.Execute(GetNotifyEventArguments());
 				}
+			}
+
+			collections::ObservableListBase<GuiRibbonGroup*>& GuiRibbonTabPage::GetGroups()
+			{
+				return groups;
 			}
 
 /***********************************************************************
--- Source/Controls/ToolstripPackage/GuiRibbonControls.h
+++ Source/Controls/ToolstripPackage/GuiRibbonControls.h
@@ -23,52 +23,73 @@ namespace vl
 Ribbon Containers
 ***********************************************************************/
 
+			class GuiRibbonTabPage;
 			class GuiRibbonGroup;
 
 			class GuiRibbonTab : public GuiTab, public Description<GuiRibbonTab>
 			{
 				GUI_SPECIFY_CONTROL_TEMPLATE_TYPE(RibbonTabTemplate, GuiTab)
 			protected:
-				compositions::GuiBoundsComposition*		beforeHeaders = nullptr;
-				compositions::GuiBoundsComposition*		afterHeaders = nullptr;
+				compositions::GuiBoundsComposition*					beforeHeaders = nullptr;
+				compositions::GuiBoundsComposition*					afterHeaders = nullptr;
 			public:
 				GuiRibbonTab(theme::ThemeName themeName);
 				~GuiRibbonTab();
 
-				compositions::GuiGraphicsComposition*	GetBeforeHeaders();
-				compositions::GuiGraphicsComposition*	GetAfterHeaders();
+				compositions::GuiGraphicsComposition*				GetBeforeHeaders();
+				compositions::GuiGraphicsComposition*				GetAfterHeaders();
+			};
+
+			class GuiRibbonGroupCollection : public collections::ObservableListBase<GuiRibbonGroup*>
+			{
+			protected:
+				GuiRibbonTabPage*									tabPage = nullptr;
+
+				bool												QueryInsert(vint index, GuiRibbonGroup* const& value)override;
+				void												AfterInsert(vint index, GuiRibbonGroup* const& value)override;
+				void												AfterRemove(vint index, vint count)override;
+
+			public:
+				GuiRibbonGroupCollection(GuiRibbonTabPage* _tabPage);
+				~GuiRibbonGroupCollection();
 			};
 
 			class GuiRibbonTabPage : public GuiTabPage, public Description<GuiRibbonTabPage>
 			{
+				friend class GuiRibbonGroupCollection;
 			protected:
-				bool									highlighted = false;
+				bool												highlighted = false;
+				GuiRibbonGroupCollection							groups;
+				compositions::GuiResponsiveStackComposition*		responsiveStack = nullptr;
+				compositions::GuiStackComposition*					stack = nullptr;
 
 			public:
 				GuiRibbonTabPage(theme::ThemeName themeName);
 				~GuiRibbonTabPage();
 
-				compositions::GuiNotifyEvent			HighlightedChanged;
+				compositions::GuiNotifyEvent						HighlightedChanged;
 
-				bool									GetHighlighted();
-				void									SetHighlighted(bool value);
+				bool												GetHighlighted();
+				void												SetHighlighted(bool value);
+
+				collections::ObservableListBase<GuiRibbonGroup*>&	GetGroups();
 			};
 
 			class GuiRibbonGroup : public GuiControl, public Description<GuiRibbonGroup>
 			{
 				GUI_SPECIFY_CONTROL_TEMPLATE_TYPE(RibbonGroupTemplate, GuiControl)
 			protected:
-				bool									expandable = false;
+				bool												expandable = false;
 
 			public:
 				GuiRibbonGroup(theme::ThemeName themeName);
 				~GuiRibbonGroup();
 
-				compositions::GuiNotifyEvent			ExpandableChanged;
-				compositions::GuiNotifyEvent			ExpandButtonClicked;
+				compositions::GuiNotifyEvent						ExpandableChanged;
+				compositions::GuiNotifyEvent						ExpandButtonClicked;
 
-				bool									GetExpandable();
-				void									SetExpandable(bool value);
+				bool												GetExpandable();
+				void												SetExpandable(bool value);
 			};
 
 /***********************************************************************
