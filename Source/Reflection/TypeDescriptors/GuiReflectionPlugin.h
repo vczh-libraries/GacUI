745c27353b96f70681e514a4c4e5ae4d82395ccb
Zihan Chen <vczh@163.com>
2018-06-06 22:58:40 -0700

    ...

--- Source/Reflection/TypeDescriptors/GuiReflectionPlugin.h
+++ Source/Reflection/TypeDescriptors/GuiReflectionPlugin.h
@@ -268,6 +268,7 @@ Type List (Controls)
 			F(presentation::controls::list::FixedHeightMultiColumnItemArranger)\
 			F(presentation::controls::list::ITextItemView)\
 			F(presentation::controls::list::TextItem)\
+			F(presentation::controls::list::TextItemProvider)\
 			F(presentation::controls::TextListView)\
 			F(presentation::controls::list::IListViewItemView)\
 			F(presentation::controls::list::ListViewColumnItemArranger)\
@@ -275,6 +276,8 @@ Type List (Controls)
 			F(presentation::controls::list::ListViewColumnItemArranger::IColumnItemView)\
 			F(presentation::controls::list::ListViewItem)\
 			F(presentation::controls::list::ListViewColumn)\
+			F(presentation::controls::ListViewView)\
+			F(presentation::controls::list::ListViewItemProvider)\
 			F(presentation::controls::IGuiMenuService)\
 			F(presentation::controls::IGuiMenuService::Direction)\
 			F(presentation::controls::IGuiMenuDropdownProvider)\
@@ -282,6 +285,7 @@ Type List (Controls)
 			F(presentation::controls::tree::INodeProvider)\
 			F(presentation::controls::tree::INodeRootProvider)\
 			F(presentation::controls::tree::INodeItemView)\
+			F(presentation::controls::tree::NodeItemProvider)\
 			F(presentation::controls::tree::MemoryNodeProvider)\
 			F(presentation::controls::tree::NodeRootProviderBase)\
 			F(presentation::controls::tree::MemoryNodeRootProvider)\
@@ -342,7 +346,6 @@ Type List (Controls)
 			F(presentation::controls::GuiTextList)\
 			F(presentation::controls::GuiListViewColumnHeader)\
 			F(presentation::controls::GuiListViewBase)\
-			F(presentation::controls::ListViewView)\
 			F(presentation::controls::GuiVirtualListView)\
 			F(presentation::controls::GuiListView)\
 			F(presentation::controls::GuiMenu)\
