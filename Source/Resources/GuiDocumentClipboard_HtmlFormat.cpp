376e77832fe76bdce46395d25e21d6d9508dedee
Zihan Chen <vczh@163.com>
2018-06-23 21:30:08 -0700

    ...

--- Source/Resources/GuiDocumentClipboard_HtmlFormat.cpp
+++ Source/Resources/GuiDocumentClipboard_HtmlFormat.cpp
@@ -25,18 +25,17 @@ namespace vl
 				writer.WriteString(NAME); \
 				NAME##Stream.SeekFromBegin(0); \
 			}
-			CONVERT_TO_UTF8(header)
-				CONVERT_TO_UTF8(content)
-				CONVERT_TO_UTF8(footer)
+			CONVERT_TO_UTF8(header);
+			CONVERT_TO_UTF8(content);
+			CONVERT_TO_UTF8(footer);
 #undef CONVERT_TO_UTF8
 
-				char clipboardHeader[] =
-				R"(
-StartHTML:-1
-EndHTML:-1
-StartFragment:0000000000
-EndFragment:0000000000
-)";
+			char clipboardHeader[] =
+				"StartHTML:-1\r\n"
+				"EndHTML:-1\r\n"
+				"StartFragment:0000000000\r\n"
+				"EndFragment:0000000000\r\n"
+				;
 			char commentStart[] = "<!--StartFragment-->";
 			char commentEnd[] = "<!--EndFragment-->";
 			vint offsetStart = sizeof(clipboardHeader) - 1 + (vint)headerStream.Size() + sizeof(commentStart) - 1;
@@ -44,15 +43,15 @@ EndFragment:0000000000
 
 			AString offsetStartString = itoa(offsetStart);
 			AString offsetEndString = itoa(offsetEnd);
-			memcpy(strstr(clipboardHeader, "EndFragment:") - offsetStartString.Length(), offsetStartString.Buffer(), offsetStartString.Length());
-			memcpy(clipboardHeader + sizeof(clipboardHeader) - 1 - offsetEndString.Length(), offsetEndString.Buffer(), offsetEndString.Length());
+			memcpy(strstr(clipboardHeader, "EndFragment:") - offsetStartString.Length() - 2, offsetStartString.Buffer(), offsetStartString.Length());
+			memcpy(clipboardHeader + sizeof(clipboardHeader) - 1 - offsetEndString.Length() - 2, offsetEndString.Buffer(), offsetEndString.Length());
 
 			stream.Write(clipboardHeader, sizeof(clipboardHeader) - 1);
-			stream.Write(headerStream.GetInternalBuffer(), (vint)headerStream.Size());
+			if (headerStream.Size() > 0) stream.Write(headerStream.GetInternalBuffer(), (vint)headerStream.Size());
 			stream.Write(commentStart, sizeof(commentStart) - 1);
-			stream.Write(contentStream.GetInternalBuffer(), (vint)contentStream.Size());
+			if (contentStream.Size() > 0) stream.Write(contentStream.GetInternalBuffer(), (vint)contentStream.Size());
 			stream.Write(commentEnd, sizeof(commentEnd) - 1);
-			stream.Write(footerStream.GetInternalBuffer(), (vint)footerStream.Size());
+			if (footerStream.Size() > 0) stream.Write(footerStream.GetInternalBuffer(), (vint)footerStream.Size());
 		}
 	}
 }
