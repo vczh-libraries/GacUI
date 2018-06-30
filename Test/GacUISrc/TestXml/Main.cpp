16528836d9b8674933458b252846871a87b1c8dd
Zihan Chen <vczh@163.com>
2018-06-29 22:38:55 -0700

    ...

--- Test/GacUISrc/TestXml/Main.cpp
+++ Test/GacUISrc/TestXml/Main.cpp
@@ -30,6 +30,54 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
 
 #ifndef VCZH_DEBUG_NO_REFLECTION
 
+void AttachDumpCompositionMenu(GuiWindow* window)
+{
+	auto menu = new GuiToolstripMenu(theme::ThemeName::Menu, window);
+	window->AddControlHostComponent(menu);
+	{
+		auto menuItem = new GuiToolstripButton(theme::ThemeName::MenuItemButton);
+		menuItem->SetText(L"Dump Composition to " + FilePath(L"TestXml.xml").GetFullPath());
+		menu->GetToolstripItems().Add(menuItem);
+
+		window->GetBoundsComposition()->GetEventReceiver()->rightButtonUp.AttachLambda([=](auto, auto arguments)
+		{
+			menu->ShowPopup(window, Point(arguments.x, arguments.y));
+		});
+
+		menuItem->Clicked.AttachLambda([=](auto, auto)
+		{
+			FileStream fileStream(L"TestXml.xml", FileStream::WriteOnly);
+			BomEncoder encoder(BomEncoder::Utf8);
+			EncoderStream encoderStream(fileStream, encoder);
+			StreamWriter writer(encoderStream);
+
+			GuiGraphicsComposition* composition = window->GetBoundsComposition();
+			while (composition->GetParent())
+			{
+				composition = composition->GetParent();
+			}
+
+			DumpComposition(composition, writer);
+			GetCurrentController()->DialogService()->ShowMessageBox(window->GetNativeWindow(), L"Finished!", L"Dump Composition");
+		});
+	}
+	{
+		auto menuItem = new GuiToolstripButton(theme::ThemeName::MenuItemButton);
+		menuItem->SetText(L"Open New Window");
+		menu->GetToolstripItems().Add(menuItem);
+
+		menuItem->Clicked.AttachLambda([=](auto, auto)
+		{
+			auto newWindow = new GuiWindow(theme::ThemeName::Window);
+			newWindow->SetText(L"New Window");
+			newWindow->SetClientSize(Size(640, 480));
+			newWindow->ForceCalculateSizeImmediately();
+			newWindow->MoveToScreenCenter();
+			newWindow->ShowModalAndDelete(window, []() {});
+		});
+	}
+}
+
 void OpenMainWindow()
 {
 	{
@@ -37,57 +85,12 @@ void OpenMainWindow()
 		RegisterTheme(L"DarkSkin", theme);
 	}
 	{
-		auto theme = UnboxValue<Ptr<ThemeTemplates>>(Value::Create(L"darkskin::RibbonTheme"));
-		RegisterTheme(L"RibbonTheme", theme);
+		//auto theme = UnboxValue<Ptr<ThemeTemplates>>(Value::Create(L"darkskin::RibbonTheme"));
+		//RegisterTheme(L"RibbonTheme", theme);
 	}
 	{
 		auto window = UnboxValue<GuiWindow*>(Value::Create(L"demo::MainWindow"));
-		{
-			auto menu = new GuiToolstripMenu(theme::ThemeName::Menu, window);
-			window->AddControlHostComponent(menu);
-			{
-				auto menuItem = new GuiToolstripButton(theme::ThemeName::MenuItemButton);
-				menuItem->SetText(L"Dump Composition to " + FilePath(L"TestXml.xml").GetFullPath());
-				menu->GetToolstripItems().Add(menuItem);
-
-				window->GetBoundsComposition()->GetEventReceiver()->rightButtonUp.AttachLambda([=](auto, auto arguments)
-				{
-					menu->ShowPopup(window, Point(arguments.x, arguments.y));
-				});
-
-				menuItem->Clicked.AttachLambda([=](auto, auto)
-				{
-					FileStream fileStream(L"TestXml.xml", FileStream::WriteOnly);
-					BomEncoder encoder(BomEncoder::Utf8);
-					EncoderStream encoderStream(fileStream, encoder);
-					StreamWriter writer(encoderStream);
-
-					GuiGraphicsComposition* composition = window->GetBoundsComposition();
-					while (composition->GetParent())
-					{
-						composition = composition->GetParent();
-					}
-
-					DumpComposition(composition, writer);
-					GetCurrentController()->DialogService()->ShowMessageBox(window->GetNativeWindow(), L"Finished!", L"Dump Composition");
-				});
-			}
-			{
-				auto menuItem = new GuiToolstripButton(theme::ThemeName::MenuItemButton);
-				menuItem->SetText(L"Open New Window");
-				menu->GetToolstripItems().Add(menuItem);
-
-				menuItem->Clicked.AttachLambda([=](auto, auto)
-				{
-					auto newWindow = new GuiWindow(theme::ThemeName::Window);
-					newWindow->SetText(L"New Window");
-					newWindow->SetClientSize(Size(640, 480));
-					newWindow->ForceCalculateSizeImmediately();
-					newWindow->MoveToScreenCenter();
-					newWindow->ShowModalAndDelete(window, []() {});
-				});
-			}
-		}
+		AttachDumpCompositionMenu(window);
 		window->ForceCalculateSizeImmediately();
 		window->MoveToScreenCenter();
 		GetApplication()->Run(window);
