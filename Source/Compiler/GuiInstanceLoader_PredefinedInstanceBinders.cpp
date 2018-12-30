d04a06a8102c11f97fed44596916ee5dcff3173a
Zihan Chen <vczh@163.com>
2018-12-30 10:55:06 -0800

    ...

--- Source/Compiler/GuiInstanceLoader_PredefinedInstanceBinders.cpp
+++ Source/Compiler/GuiInstanceLoader_PredefinedInstanceBinders.cpp
@@ -325,18 +325,8 @@ GuiLocalizedStringInstanceBinder (str)
 
 					if (errorCount == errors.Count())
 					{
-						auto stringExpr = MakePtr<WfStringExpression>();
-						stringExpr->value.value = L"";
-						stringExpr->codeRange = expression->codeRange;
-
-						auto recoveryExpr = MakePtr<WfBinaryExpression>();
-						recoveryExpr->op = WfBinaryOperator::FailedThen;
-						recoveryExpr->first = expression;
-						recoveryExpr->second = stringExpr;
-						recoveryExpr->codeRange = expression->codeRange;
-
 						auto bindExpr = MakePtr<WfBindExpression>();
-						bindExpr->expression = recoveryExpr;
+						bindExpr->expression = expression;
 						bindExpr->codeRange = expression->codeRange;
 
 						return Workflow_InstallBindProperty(precompileContext, resolvingResult, variableName, propertyInfo, bindExpr);
