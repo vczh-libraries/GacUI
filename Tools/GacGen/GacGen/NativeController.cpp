d4bf504fb931fa2d05967a021527596a0c530fb4
vczh <vczh@outlook.com>
2021-10-03 15:55:28 -0700

    Update NativeController.cpp

--- Tools/GacGen/GacGen/NativeController.cpp
+++ Tools/GacGen/GacGen/NativeController.cpp
@@ -1,59 +1,176 @@
 #include "GacUI.h"
 
 using namespace vl;
+using namespace vl::stream;
 using namespace vl::presentation;
 
-class GacGenNativeController : public Object, public INativeController
+class GacGenNativeController
+	: public Object
+	, public INativeController
+	, protected INativeImageService
+	, protected INativeInputService
 {
 public:
-	virtual INativeCallbackService* CallbackService() override
+	INativeCallbackService* CallbackService() override
 	{
-		CHECK_FAIL(L"GacGenNativeController::CallbackService()#Not implemented for GacGen.exe");
+		return this;
 	}
 
-	virtual INativeResourceService* ResourceService() override
+	INativeResourceService* ResourceService() override
 	{
-		CHECK_FAIL(L"GacGenNativeController::CallbackService()#Not implemented for GacGen.exe");
+		CHECK_FAIL(L"Not implemented!");
 	}
 
-	virtual INativeAsyncService* AsyncService() override
+	INativeAsyncService* AsyncService() override
 	{
-		CHECK_FAIL(L"GacGenNativeController::CallbackService()#Not implemented for GacGen.exe");
+		CHECK_FAIL(L"Not implemented!");
 	}
 
-	virtual INativeClipboardService* ClipboardService() override
+	INativeClipboardService* ClipboardService() override
 	{
-		CHECK_FAIL(L"GacGenNativeController::CallbackService()#Not implemented for GacGen.exe");
+		CHECK_FAIL(L"Not implemented!");
 	}
 
-	virtual INativeImageService* ImageService() override
+	INativeImageService* ImageService() override
 	{
-		CHECK_FAIL(L"GacGenNativeController::CallbackService()#Not implemented for GacGen.exe");
+		return this;
 	}
 
-	virtual INativeScreenService* ScreenService() override
+	INativeScreenService* ScreenService() override
 	{
-		CHECK_FAIL(L"GacGenNativeController::CallbackService()#Not implemented for GacGen.exe");
+		CHECK_FAIL(L"Not implemented!");
 	}
 
-	virtual INativeWindowService* WindowService() override
+	INativeWindowService* WindowService() override
 	{
-		CHECK_FAIL(L"GacGenNativeController::CallbackService()#Not implemented for GacGen.exe");
+		CHECK_FAIL(L"Not implemented!");
 	}
 
-	virtual INativeInputService* InputService() override
+	INativeInputService* InputService() override
 	{
-		CHECK_FAIL(L"GacGenNativeController::CallbackService()#Not implemented for GacGen.exe");
+		return this;
 	}
 
-	virtual INativeDialogService* DialogService() override
+	INativeDialogService* DialogService() override
 	{
-		CHECK_FAIL(L"GacGenNativeController::CallbackService()#Not implemented for GacGen.exe");
+		CHECK_FAIL(L"Not implemented!");
 	}
 
-	virtual WString GetExecutablePath() override
+	WString GetExecutablePath() override
 	{
-		CHECK_FAIL(L"GacGenNativeController::CallbackService()#Not implemented for GacGen.exe");
+		CHECK_FAIL(L"Not implemented!");
+	}
+
+	////////////////////////////////////////////////////////////////////
+	// INativeCallbackService
+	////////////////////////////////////////////////////////////////////
+
+	bool InstallListener(INativeControllerListener* listener) override
+	{
+		return true;
+	}
+
+	bool UninstallListener(INativeControllerListener* listener) override
+	{
+		return true;
+	}
+
+	////////////////////////////////////////////////////////////////////
+	// INativeImageService
+	////////////////////////////////////////////////////////////////////
+
+	class NativeImage : public Object, public INativeImage
+	{
+	protected:
+		INativeImageService*		imageService;
+		MemoryStream				memoryStream;
+
+	public:
+		NativeImage(INativeImageService* _imageService, IStream& inputStream)
+			: imageService(_imageService)
+		{
+			CopyStream(inputStream, memoryStream);
+		}
+
+		INativeImageService* GetImageService() override
+		{
+			return imageService;
+		}
+
+		FormatType GetFormat() override
+		{
+			CHECK_FAIL(L"Not implemented!");
+		}
+
+		vint GetFrameCount() override
+		{
+			return 0;
+		}
+
+		INativeImageFrame* GetFrame(vint index) override
+		{
+			CHECK_FAIL(L"Not implemented!");
+		}
+
+		void SaveToStream(stream::IStream& imageStream, FormatType formatType) override
+		{
+			CHECK_ERROR(formatType == FormatType::Unknown, L"Not Implemented!");
+			CopyStream(imageStream, memoryStream);
+		}
+	};
+
+	Ptr<INativeImage> CreateImageFromFile(const WString& path) override
+	{
+		FileStream imageStream(path, FileStream::ReadOnly);
+		return new NativeImage(this, imageStream);
+	}
+
+	Ptr<INativeImage> CreateImageFromMemory(void* buffer, vint length) override
+	{
+		MemoryWrapperStream imageStream(buffer, length);
+		return new NativeImage(this, imageStream);
+	}
+
+	Ptr<INativeImage> CreateImageFromStream(stream::IStream& imageStream) override
+	{
+		return new NativeImage(this, imageStream);
+	}
+
+	////////////////////////////////////////////////////////////////////
+	// INativeInputService
+	////////////////////////////////////////////////////////////////////
+
+	void StartTimer() override
+	{
+	}
+
+	void StopTimer() override
+	{
+	}
+
+	bool IsTimerEnabled() override
+	{
+		CHECK_FAIL(L"Not implemented!");
+	}
+
+	bool IsKeyPressing(VKEY code) override
+	{
+		CHECK_FAIL(L"Not implemented!");
+	}
+
+	bool IsKeyToggled(VKEY code) override
+	{
+		CHECK_FAIL(L"Not implemented!");
+	}
+
+	WString GetKeyName(VKEY code) override
+	{
+		CHECK_FAIL(L"Not implemented!");
+	}
+
+	VKEY GetKey(const WString& name) override
+	{
+		CHECK_FAIL(L"Not implemented!");
 	}
 
 };
