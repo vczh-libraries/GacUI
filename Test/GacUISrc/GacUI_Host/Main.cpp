740c520eacada0e489e80722c30c3c5476902b80
vczh <vczh@outlook.com>
2021-10-08 22:45:45 -0700

    GacUI_Host

---
+++ Test/GacUISrc/GacUI_Host/Main.cpp
@@ -0,0 +1,56 @@
+#pragma comment(linker,"\"/manifestdependency:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
+#define GAC_HEADER_USE_NAMESPACE
+#include "../../../Source/GacUI.h"
+#include "../../../Source/Reflection/TypeDescriptors/GuiReflectionPlugin.h"
+#include <Windows.h>
+
+using namespace vl;
+using namespace vl::collections;
+using namespace vl::stream;
+using namespace vl::reflection::description;
+
+int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int CmdShow)
+{
+	int result = SetupWindowsDirect2DRenderer();
+#if VCZH_CHECK_MEMORY_LEAKS
+	_CrtDumpMemoryLeaks();
+#endif
+	return result;
+}
+
+extern void GuiMain_GrammarIntellisense();
+
+WString GetResourcePath()
+{
+#ifdef _WIN64
+	return GetApplication()->GetExecutableFolder() + L"../../../Resources/";
+#else
+	return GetApplication()->GetExecutableFolder() + L"../../Resources/";
+#endif
+}
+
+void GuiMain()
+{
+#define DARKSKIN_BINARY			L"../GacUISrc/Generated_DarkSkin/Resource/DarkSkin.bin"
+#define FULLCONTROLTEST_BINARY	L"../GacUISrc/Generated_FullControlTest/Resource/FullControlTest.bin"
+	{
+		FileStream fileStream(GetResourcePath() + DARKSKIN_BINARY, FileStream::ReadOnly);
+		GetResourceManager()->LoadResourceOrPending(fileStream, GuiResourceUsage::InstanceClass);
+	}
+	{
+		FileStream fileStream(GetResourcePath() + FULLCONTROLTEST_BINARY, FileStream::ReadOnly);
+		GetResourceManager()->LoadResourceOrPending(fileStream, GuiResourceUsage::InstanceClass);
+	}
+	{
+		auto theme = UnboxValue<Ptr<ThemeTemplates>>(Value::Create(L"darkskin::Theme"));
+		RegisterTheme(theme);
+	}
+	{
+		auto window = UnboxValue<GuiWindow*>(Value::Create(L"demo::MainWindow"));
+		window->ForceCalculateSizeImmediately();
+		window->MoveToScreenCenter();
+		GetApplication()->Run(window);
+		delete window;
+	}
+}
---
+++ Test/GacUISrc/GacUI_Host/TsfTestWindow.cpp
@@ -0,0 +1,165 @@
+#define GAC_HEADER_USE_NAMESPACE
+#include "..\..\..\Source\GacUI.h"
+#include "..\..\..\Source\NativeWindow\Windows\WinNativeWindow.h"
+#include "..\..\..\Source\Compiler\InstanceQuery\GuiInstanceQuery.h"
+#include "..\..\..\Source\Resources\GuiParserManager.h"
+#include <Windows.h>
+#include <msctf.h>
+
+using namespace vl::collections;
+using namespace vl::stream;
+using namespace vl::presentation;
+
+namespace test
+{
+
+/***********************************************************************
+TsfTestWindow
+***********************************************************************/
+
+	class TsfTestWindow : public GuiWindow, private ITfThreadMgrEventSink, private ITfContextOwnerCompositionSink
+	{
+	private:
+		ComPtr<ITfThreadMgr>				threadManager;
+		ComPtr<ITfSource>					threadManagerSource;
+		ComPtr<ITfDocumentMgr>				documentManager;
+		ComPtr<ITfContext>					editContext;
+		TfClientId							clientId;
+		TfEditCookie						editCookie;
+
+		IUnknown* GetUnknown()
+		{
+			return static_cast<IUnknown*>(static_cast<ITfThreadMgrEventSink*>(this));
+		}
+
+		HRESULT STDMETHODCALLTYPE QueryInterface(REFIID riid, void** ppvObject)
+		{
+			if(!ppvObject)
+			{
+				return E_POINTER;
+			}
+			if(riid==IID_IUnknown)
+			{
+				*ppvObject=GetUnknown();
+			}
+			else if(riid==IID_ITfThreadMgrEventSink)
+			{
+				*ppvObject=static_cast<ITfThreadMgrEventSink*>(this);
+			}
+			else if(riid==IID_ITfContextOwnerCompositionSink)
+			{
+				*ppvObject=static_cast<ITfContextOwnerCompositionSink*>(this);
+			}
+			else
+			{
+				*ppvObject=NULL;
+				return E_NOINTERFACE;
+			}
+			return S_OK;
+		}
+
+		ULONG STDMETHODCALLTYPE AddRef(void)
+		{
+			return 1;
+		}
+
+		ULONG STDMETHODCALLTYPE Release(void)
+		{
+			return 1;
+		}
+
+		HRESULT STDMETHODCALLTYPE OnInitDocumentMgr(ITfDocumentMgr *pdim)
+		{
+			return S_OK;
+		}
+        
+		HRESULT STDMETHODCALLTYPE OnUninitDocumentMgr(ITfDocumentMgr *pdim)
+		{
+			return S_OK;
+		}
+        
+		HRESULT STDMETHODCALLTYPE OnSetFocus(ITfDocumentMgr *pdimFocus, ITfDocumentMgr *pdimPrevFocus)
+		{
+			return S_OK;
+		}
+        
+		HRESULT STDMETHODCALLTYPE OnPushContext(ITfContext *pic)
+		{
+			return S_OK;
+		}
+        
+		HRESULT STDMETHODCALLTYPE OnPopContext(ITfContext *pic)
+		{
+			return S_OK;
+		}
+
+		HRESULT STDMETHODCALLTYPE OnStartComposition(ITfCompositionView *pComposition, BOOL *pfOk)
+		{
+			return S_OK;
+		}
+        
+		HRESULT STDMETHODCALLTYPE OnUpdateComposition(ITfCompositionView *pComposition, ITfRange *pRangeNew)
+		{
+			return S_OK;
+		}
+        
+		HRESULT STDMETHODCALLTYPE OnEndComposition(ITfCompositionView *pComposition)
+		{
+			return S_OK;
+		}
+
+	public:
+		TsfTestWindow()
+			:GuiWindow(theme::ThemeName::Window)
+			,clientId(0)
+		{
+			SetText(GetApplication()->GetExecutableFolder());
+			GetContainerComposition()->SetPreferredMinSize(Size(640, 480));
+			ForceCalculateSizeImmediately();
+			MoveToScreenCenter();
+
+			Initialize();
+		}
+
+		~TsfTestWindow()
+		{
+		}
+
+		void Initialize()
+		{
+			ITfThreadMgr*		pThreadMgr=0;
+			ITfSource*			pSource=0;
+			ITfDocumentMgr*		pDocumentMgr=0;
+			ITfContext*			pContext=0;
+
+			HRESULT hr=CoCreateInstance(
+				CLSID_TF_ThreadMgr, 
+                NULL, 
+                CLSCTX_INPROC_SERVER, 
+                IID_ITfThreadMgr, 
+                (void**)&pThreadMgr
+				);
+			hr=pThreadMgr->QueryInterface(IID_ITfSource, (void**)&pSource);
+			DWORD cookie=0;
+			hr=pSource->AdviseSink(IID_ITfThreadMgrEventSink, static_cast<ITfThreadMgrEventSink*>(this), &cookie);
+			hr=pThreadMgr->Activate(&clientId);
+			hr=pThreadMgr->CreateDocumentMgr(&pDocumentMgr);
+			hr=pDocumentMgr->CreateContext(clientId, 0, GetUnknown(), &pContext, &editCookie);
+			hr=pDocumentMgr->Push(pContext);
+			{
+				HWND hwnd=windows::GetWindowsForm(GetNativeWindow())->GetWindowHandle();
+				ITfDocumentMgr* pPrevious=0;
+				hr=pThreadMgr->AssociateFocus(hwnd, pDocumentMgr, &pPrevious);
+				if(pPrevious)
+				{
+					pPrevious->Release();
+				}
+			}
+
+			threadManager=pThreadMgr;
+			threadManagerSource=pSource;
+			documentManager=pDocumentMgr;
+			editContext=pContext;
+		}
+	};
+}
