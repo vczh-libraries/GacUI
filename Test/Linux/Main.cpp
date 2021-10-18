2f8cd5f1a9e85a8fb8f34c4ffa1b7d78b1ea128b
vczh <vczh@outlook.com>
2021-10-18 04:04:39 -0700

    Fix ubuntu build script

--- Test/Linux/Main.cpp
+++ Test/Linux/Main.cpp
@@ -1,12 +1,31 @@
-#include "../../Source/GacUI.h"
+#include "DarkSkin.h"
+#include "MainWindow.h"
 
 using namespace vl;
-
-int main()
-{
-	return 0;
-}
+using namespace vl::collections;
+using namespace vl::stream;
+using namespace vl::reflection::description;
+using namespace vl::presentation;
+using namespace vl::presentation::controls;
+using namespace vl::presentation::elements;
+using namespace vl::presentation::templates;
+using namespace demo;
 
 void GuiMain()
 {
+	windows::SetWindowDefaultIcon(MAINICON);
+	theme::RegisterTheme(MakePtr<darkskin::Theme>());
+	{
+		demo::MainWindow window;
+		window.ForceCalculateSizeImmediately();
+		window.MoveToScreenCenter();
+		GetApplication()->Run(&window);
+	}
+}
+
+int main()
+{
+	// Test application created for building only
+	// Linux port should go to github.com/vczh-libraries/gGac
+	return 0;
 }
