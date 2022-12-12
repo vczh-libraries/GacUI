20756564fe82af1593552f9524c24c0e42346489
vczh <vczh@outlook.com>
2022-12-11 21:28:53 -0800

    Begin GuiHostedWindow.h

---
+++ Source/PlatformProviders/Hosted/GuiHostedWindow.h
@@ -0,0 +1,30 @@
+/***********************************************************************
+Vczh Library++ 3.0
+Developer: Zihan Chen(vczh)
+GacUI::Hosted Window
+
+Interfaces:
+  GuiHostedController
+  GuiHostedScreen
+  GuiHostedMainWindow
+  GuiHostedWindow
+
+***********************************************************************/
+
+#ifndef VCZH_PRESENTATION_GUIHOSTEDWINDOW
+#define VCZH_PRESENTATION_GUIHOSTEDWINDOW
+
+#include "../../NativeWindow/GuiNativeWindow.h"
+
+namespace vl
+{
+	namespace presentation
+	{
+
+/***********************************************************************
+GuiHostedController
+***********************************************************************/
+	}
+}
+
+#endif
