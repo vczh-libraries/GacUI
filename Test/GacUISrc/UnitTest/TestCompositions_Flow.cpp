515c2aeb6e20719bd87aa40f3db3dee3c2f18dde
vczh <vczh@outlook.com>
2023-05-17 04:39:57 -0700

    ...

--- Test/GacUISrc/UnitTest/TestCompositions_Flow.cpp
+++ Test/GacUISrc/UnitTest/TestCompositions_Flow.cpp
@@ -8,141 +8,196 @@ TEST_FILE
 		TestBoundsWithTrivialChildren<GuiFlowComposition>();
 	});
 
-	TEST_CASE(L"Test <Flow> with <FlowItem> in different direction")
+	TEST_CATEGORY(L"Test <Flow> with <FlowItem> in different directions")
 	{
-		auto flow = new GuiFlowComposition;
-
-		flow->SetMinSizeLimitation(GuiGraphicsComposition::LimitToElementAndChildren);
-		flow->SetPreferredMinSize(Size(200, 250));
-		flow->ForceCalculateSizeImmediately();
-
-		TEST_ASSERT(flow->GetExtraMargin() == Margin(0, 0, 0, 0));
-		TEST_ASSERT(flow->GetRowPadding() == 0);
-		TEST_ASSERT(flow->GetColumnPadding() == 0);
-		TEST_ASSERT(flow->GetAlignment() == FlowAlignment::Left);
-		TEST_ASSERT(flow->GetAxis().Cast<GuiDefaultAxis>());
-		flow->SetExtraMargin(Margin(11, 22, 33, 44));
-		flow->SetRowPadding(20);
-		flow->SetColumnPadding(10);
-		TEST_ASSERT(flow->GetExtraMargin() == Margin(11, 22, 33, 44));
-		TEST_ASSERT(flow->GetRowPadding() == 20);
-		TEST_ASSERT(flow->GetColumnPadding() == 10);
-
-		GuiFlowItemComposition* flowItems[6];
-		const vint ITEM_COUNT = sizeof(flowItems) / sizeof(*flowItems);
-		for (vint i = 0; i < ITEM_COUNT; i++)
+		auto testHorizontalDirections = []<vint ITEM_COUNT>(GuiFlowComposition * flow, GuiFlowItemComposition * (&flowItems)[ITEM_COUNT])
 		{
-			flowItems[i] = new GuiFlowItemComposition;
-			flowItems[i]->SetPreferredMinSize(Size(30 + i * 10, 20 + i * 20));
-			flow->InsertFlowItem(i, flowItems[i]);
-		}
+			auto testHorizontal = [&]
+			{
+				TEST_ASSERT(flow->GetClientArea() == Rect({ 0,0 }, { 200,386 }));
+				TEST_ASSERT(flow->GetMinPreferredClientSize() == flow->GetClientArea().GetSize());
+				TEST_ASSERT(flow->GetPreferredBounds() == flow->GetClientArea());
+				TEST_ASSERT(flow->GetBounds() == flow->GetClientArea());
+			};
 
-		auto testHorizontal = [&]
-		{
-			TEST_ASSERT(flow->GetClientArea() == Rect({ 0,0 }, { 200,386 }));
-			TEST_ASSERT(flow->GetMinPreferredClientSize() == flow->GetClientArea().GetSize());
-			TEST_ASSERT(flow->GetPreferredBounds() == flow->GetClientArea());
-			TEST_ASSERT(flow->GetBounds() == flow->GetClientArea());
+			auto testLeftDown = [&](bool expand)
+			{
+				testHorizontal();
+			};
+
+			auto testRightDown = [&](bool expand)
+			{
+				testHorizontal();
+			};
+
+			auto testLeftUp = [&](bool expand)
+			{
+				testHorizontal();
+			};
+
+			auto testRightUp = [&](bool expand)
+			{
+				testHorizontal();
+			};
+
+			auto testAllDirections = [&](bool expand)
+			{
+				flow->SetAxis(Ptr(new GuiDefaultAxis));
+				TEST_ASSERT(flow->GetAxis().Cast<GuiDefaultAxis>());
+				testLeftDown(expand);
+
+				flow->SetAxis(Ptr(new GuiAxis(AxisDirection::LeftDown)));
+				TEST_ASSERT(flow->GetAxis().Cast<GuiAxis>()->GetDirection() == AxisDirection::LeftDown);
+				testLeftDown(expand);
+
+				flow->SetAxis(Ptr(new GuiAxis(AxisDirection::RightDown)));
+				TEST_ASSERT(flow->GetAxis().Cast<GuiAxis>()->GetDirection() == AxisDirection::RightDown);
+				testRightDown(expand);
+
+				flow->SetAxis(Ptr(new GuiAxis(AxisDirection::LeftUp)));
+				TEST_ASSERT(flow->GetAxis().Cast<GuiAxis>()->GetDirection() == AxisDirection::LeftUp);
+				testLeftUp(expand);
+
+				flow->SetAxis(Ptr(new GuiAxis(AxisDirection::RightUp)));
+				TEST_ASSERT(flow->GetAxis().Cast<GuiAxis>()->GetDirection() == AxisDirection::RightUp);
+				testRightUp(expand);
+			};
+
+			testAllDirections(false);
+			for (vint i = 0; i < ITEM_COUNT; i++)
+			{
+				Margin extraMargin(1 * i, 2 * i, 3 * i, 4 * i);
+				flowItems[i]->SetExtraMargin(extraMargin);
+				TEST_ASSERT(flowItems[i]->GetExtraMargin() == extraMargin);
+			}
+			testAllDirections(true);
 		};
 
-		auto testVertical = [&]
+		auto testVerticalDirections = []<vint ITEM_COUNT>(GuiFlowComposition * flow, GuiFlowItemComposition * (&flowItems)[ITEM_COUNT])
 		{
-			auto fuck = flow->GetClientArea();
-			TEST_ASSERT(flow->GetClientArea() == Rect({ 0,0 }, { 264,266 }));
-			TEST_ASSERT(flow->GetMinPreferredClientSize() == flow->GetClientArea().GetSize());
-			TEST_ASSERT(flow->GetPreferredBounds() == flow->GetClientArea());
-			TEST_ASSERT(flow->GetBounds() == flow->GetClientArea());
+			auto testVertical = [&]
+			{
+				auto fuck = flow->GetClientArea();
+				TEST_ASSERT(flow->GetClientArea() == Rect({ 0,0 }, { 386,200 }));
+				TEST_ASSERT(flow->GetMinPreferredClientSize() == flow->GetClientArea().GetSize());
+				TEST_ASSERT(flow->GetPreferredBounds() == flow->GetClientArea());
+				TEST_ASSERT(flow->GetBounds() == flow->GetClientArea());
+			};
+
+			auto testDownLeft = [&](bool expand)
+			{
+				testVertical();
+			};
+
+			auto testDownRight = [&](bool expand)
+			{
+				testVertical();
+			};
+
+			auto testUpLeft = [&](bool expand)
+			{
+				testVertical();
+			};
+
+			auto testUpRight = [&](bool expand)
+			{
+				testVertical();
+			};
+
+			auto testAllDirections = [&](bool expand)
+			{
+				flow->SetAxis(Ptr(new GuiAxis(AxisDirection::DownLeft)));
+				TEST_ASSERT(flow->GetAxis().Cast<GuiAxis>()->GetDirection() == AxisDirection::DownLeft);
+				testDownLeft(expand);
+
+				flow->SetAxis(Ptr(new GuiAxis(AxisDirection::DownRight)));
+				TEST_ASSERT(flow->GetAxis().Cast<GuiAxis>()->GetDirection() == AxisDirection::DownRight);
+				testDownRight(expand);
+
+				flow->SetAxis(Ptr(new GuiAxis(AxisDirection::UpLeft)));
+				TEST_ASSERT(flow->GetAxis().Cast<GuiAxis>()->GetDirection() == AxisDirection::UpLeft);
+				testUpLeft(expand);
+
+				flow->SetAxis(Ptr(new GuiAxis(AxisDirection::UpRight)));
+				TEST_ASSERT(flow->GetAxis().Cast<GuiAxis>()->GetDirection() == AxisDirection::UpRight);
+				testUpRight(expand);
+			};
+
+			testAllDirections(false);
+			for (vint i = 0; i < ITEM_COUNT; i++)
+			{
+				Margin extraMargin(1 * i, 2 * i, 3 * i, 4 * i);
+				flowItems[i]->SetExtraMargin(extraMargin);
+				TEST_ASSERT(flowItems[i]->GetExtraMargin() == extraMargin);
+			}
+			testAllDirections(true);
 		};
 
-		auto testLeftDown = [&](bool expand)
+		auto testAllDirections = [&testHorizontalDirections, &testVerticalDirections](bool horizontal)
 		{
-			//testHorizontal();
-		};
+			auto flow = new GuiFlowComposition;
 
-		auto testRightDown = [&](bool expand)
-		{
-			//testHorizontal();
-		};
+			flow->SetMinSizeLimitation(GuiGraphicsComposition::LimitToElementAndChildren);
+			flow->SetPreferredMinSize(Size(200, 200));
+			flow->ForceCalculateSizeImmediately();
 
-		auto testLeftUp = [&](bool expand)
-		{
-			//testHorizontal();
-		};
-
-		auto testRightUp = [&](bool expand)
-		{
-			//testHorizontal();
-		};
-
-		auto testDownLeft = [&](bool expand)
-		{
-			testVertical();
-		};
-
-		auto testDownRight = [&](bool expand)
-		{
-			testVertical();
-		};
-
-		auto testUpLeft = [&](bool expand)
-		{
-			testVertical();
-		};
-
-		auto testUpRight = [&](bool expand)
-		{
-			testVertical();
-		};
-
-		auto testAllDirections = [&](bool expand)
-		{
-			flow->SetAxis(Ptr(new GuiDefaultAxis));
+			TEST_ASSERT(flow->GetExtraMargin() == Margin(0, 0, 0, 0));
+			TEST_ASSERT(flow->GetRowPadding() == 0);
+			TEST_ASSERT(flow->GetColumnPadding() == 0);
+			TEST_ASSERT(flow->GetAlignment() == FlowAlignment::Left);
 			TEST_ASSERT(flow->GetAxis().Cast<GuiDefaultAxis>());
-			testLeftDown(expand);
+			if (horizontal)
+			{
+				flow->SetExtraMargin(Margin(11, 22, 33, 44));
+				TEST_ASSERT(flow->GetExtraMargin() == Margin(11, 22, 33, 44));
+			}
+			else
+			{
+				flow->SetExtraMargin(Margin(22, 11, 44, 33));
+				TEST_ASSERT(flow->GetExtraMargin() == Margin(22, 11, 44, 33));
+			}
+			flow->SetRowPadding(20);
+			flow->SetColumnPadding(10);
+			TEST_ASSERT(flow->GetRowPadding() == 20);
+			TEST_ASSERT(flow->GetColumnPadding() == 10);
 
-			flow->SetAxis(Ptr(new GuiAxis(AxisDirection::LeftDown)));
-			TEST_ASSERT(flow->GetAxis().Cast<GuiAxis>()->GetDirection() == AxisDirection::LeftDown);
-			testLeftDown(expand);
+			GuiFlowItemComposition* flowItems[6];
+			const vint ITEM_COUNT = sizeof(flowItems) / sizeof(*flowItems);
+			for (vint i = 0; i < ITEM_COUNT; i++)
+			{
+				flowItems[i] = new GuiFlowItemComposition;
+				if (horizontal)
+				{
+					flowItems[i]->SetPreferredMinSize(Size(30 + i * 10, 20 + i * 20));
+				}
+				else
+				{
+					flowItems[i]->SetPreferredMinSize(Size(20 + i * 20, 30 + i * 10));
+				}
+				flow->InsertFlowItem(i, flowItems[i]);
+			}
 
-			flow->SetAxis(Ptr(new GuiAxis(AxisDirection::RightDown)));
-			TEST_ASSERT(flow->GetAxis().Cast<GuiAxis>()->GetDirection() == AxisDirection::RightDown);
-			testRightDown(expand);
+			if (horizontal)
+			{
+				testHorizontalDirections(flow, flowItems);
+			}
+			else
+			{
+				testVerticalDirections(flow, flowItems);
+			}
 
-			flow->SetAxis(Ptr(new GuiAxis(AxisDirection::LeftUp)));
-			TEST_ASSERT(flow->GetAxis().Cast<GuiAxis>()->GetDirection() == AxisDirection::LeftUp);
-			testLeftUp(expand);
-
-			flow->SetAxis(Ptr(new GuiAxis(AxisDirection::RightUp)));
-			TEST_ASSERT(flow->GetAxis().Cast<GuiAxis>()->GetDirection() == AxisDirection::RightUp);
-			testRightUp(expand);
-
-			flow->SetAxis(Ptr(new GuiAxis(AxisDirection::DownLeft)));
-			TEST_ASSERT(flow->GetAxis().Cast<GuiAxis>()->GetDirection() == AxisDirection::DownLeft);
-			testDownLeft(expand);
-
-			flow->SetAxis(Ptr(new GuiAxis(AxisDirection::DownRight)));
-			TEST_ASSERT(flow->GetAxis().Cast<GuiAxis>()->GetDirection() == AxisDirection::DownRight);
-			testDownRight(expand);
-
-			flow->SetAxis(Ptr(new GuiAxis(AxisDirection::UpLeft)));
-			TEST_ASSERT(flow->GetAxis().Cast<GuiAxis>()->GetDirection() == AxisDirection::UpLeft);
-			testUpLeft(expand);
-
-			flow->SetAxis(Ptr(new GuiAxis(AxisDirection::UpRight)));
-			TEST_ASSERT(flow->GetAxis().Cast<GuiAxis>()->GetDirection() == AxisDirection::UpRight);
-			testUpRight(expand);
+			SafeDeleteComposition(flow);
 		};
 
-		testAllDirections(false);
-		for (vint i = 0; i < ITEM_COUNT; i++)
+		TEST_CASE(L"Horizontal directions")
 		{
-			Margin extraMargin(1 * i, 2 * i, 3 * i, 4 * i);
-			flowItems[i]->SetExtraMargin(extraMargin);
-			TEST_ASSERT(flowItems[i]->GetExtraMargin() == extraMargin);
-		}
-		testAllDirections(true);
+			testAllDirections(true);
+		});
 
-		SafeDeleteComposition(flow);
+		TEST_CASE(L"Vertical directions")
+		{
+			testAllDirections(false);
+		});
 	});
 
 	TEST_CASE(L"Test <Flow> with different <Flow> alignment")
@@ -150,10 +205,6 @@ TEST_FILE
 	});
 
 	TEST_CASE(L"Test <Flow> with different <FlowItem> option")
-	{
-	});
-
-	TEST_CASE(L"Test <Stack> with clipped <StackItem>")
 	{
 	});
 }
