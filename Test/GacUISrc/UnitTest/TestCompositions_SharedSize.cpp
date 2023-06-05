a51eec9802397c7a14d191a06c1c5be2da6ff239
vczh <vczh@outlook.com>
2023-06-05 16:18:00 -0700

    ...

--- Test/GacUISrc/UnitTest/TestCompositions_SharedSize.cpp
+++ Test/GacUISrc/UnitTest/TestCompositions_SharedSize.cpp
@@ -56,6 +56,39 @@ TEST_FILE
 
 	TEST_CASE(L"Test <SharedSizeRoot> with heights")
 	{
+		auto root = new GuiSharedSizeRootComposition;
+
+		GuiSharedSizeItemComposition* items[2][3];
+		for (vint group = 0; group < 2; group++)
+		{
+			for (vint item = 0; item < 3; item++)
+			{
+				auto bounds = new GuiBoundsComposition;
+				root->AddChild(bounds);
+
+				auto shared = new GuiSharedSizeItemComposition;
+				shared->SetGroup(itow(group));
+				shared->SetSharedHeight(true);
+				bounds->AddChild(shared);
+
+				auto content = new GuiBoundsComposition;
+				content->SetPreferredMinSize(Size(10, (group * 3 + item + 1) * 10));
+				shared->AddChild(content);
+
+				items[group][item] = shared;
+			}
+		}
+
+		TEST_ASSERT(root->GetBounds() == Rect({ 0,0 }, { 0,0 }));
+		for (vint group = 0; group < 2; group++)
+		{
+			for (vint item = 0; item < 3; item++)
+			{
+				TEST_ASSERT(items[group][item]->GetBounds() == Rect({ 0,0 }, { 10,(group + 1) * 30 }));
+			}
+		}
+
+		SafeDeleteComposition(root);
 	});
 
 	TEST_CASE(L"Test <SharedSizeRoot> with widths and heights")
