e8c3c9338ff894a7aad4c7a93daedaf3988b60c0
vczh <vczh@outlook.com>
2023-06-06 14:58:54 -0700

    TestCompositions_Responsive.cpp

--- Test/GacUISrc/UnitTest/TestCompositions_Responsive.cpp
+++ Test/GacUISrc/UnitTest/TestCompositions_Responsive.cpp
@@ -56,4 +56,86 @@ TEST_FILE
 	{
 		TestBoundsWithTrivialChildren<GuiResponsiveContainerComposition>();
 	});
+
+	TEST_CATEGORY(L"Test level management")
+	{
+		TEST_CASE(L"Test <ResponsiveFixed>")
+		{
+			auto fixed = new GuiResponsiveFixedComposition;
+
+			TEST_ASSERT(fixed->GetLevelCount() == 1);
+			TEST_ASSERT(fixed->GetCurrentLevel() == 0);
+
+			TEST_ASSERT(fixed->LevelUp() == false);
+			TEST_ASSERT(fixed->GetCurrentLevel() == 0);
+
+			TEST_ASSERT(fixed->LevelDown() == false);
+			TEST_ASSERT(fixed->GetCurrentLevel() == 0);
+
+			SafeDeleteComposition(fixed);
+		});
+
+		TEST_CASE(L"Test <ResponsiveView>")
+		{
+			auto view = new GuiResponsiveViewComposition;
+
+			auto fixed1 = new GuiResponsiveFixedComposition;
+			auto fixed2 = new GuiResponsiveFixedComposition;
+			auto fixed3 = new GuiResponsiveFixedComposition;
+
+			view->GetViews().Add(fixed1);
+			view->GetViews().Add(fixed2);
+			view->GetViews().Add(fixed3);
+
+			TEST_ASSERT(view->GetLevelCount() == 3);
+			TEST_ASSERT(view->GetCurrentLevel() == 2);
+			TEST_ASSERT(view->GetCurrentView() == fixed1);
+
+			TEST_ASSERT(view->LevelDown() == true);
+			TEST_ASSERT(view->GetCurrentLevel() == 1);
+			TEST_ASSERT(view->GetCurrentView() == fixed2);
+
+			TEST_ASSERT(view->LevelDown() == true);
+			TEST_ASSERT(view->GetCurrentLevel() == 0);
+			TEST_ASSERT(view->GetCurrentView() == fixed3);
+
+			TEST_ASSERT(view->LevelDown() == false);
+			TEST_ASSERT(view->GetCurrentLevel() == 0);
+			TEST_ASSERT(view->GetCurrentView() == fixed3);
+
+			TEST_ASSERT(view->LevelUp() == true);
+			TEST_ASSERT(view->GetCurrentLevel() == 1);
+			TEST_ASSERT(view->GetCurrentView() == fixed2);
+
+			TEST_ASSERT(view->LevelUp() == true);
+			TEST_ASSERT(view->GetCurrentLevel() == 2);
+			TEST_ASSERT(view->GetCurrentView() == fixed1);
+
+			TEST_ASSERT(view->LevelUp() == false);
+			TEST_ASSERT(view->GetCurrentLevel() == 2);
+			TEST_ASSERT(view->GetCurrentView() == fixed1);
+
+			SafeDeleteComposition(view);
+		});
+
+		TEST_CASE(L"Test <ResponsiveStack>")
+		{
+		});
+
+		TEST_CASE(L"Test <ResponsiveGroup>")
+		{
+		});
+
+		TEST_CASE(L"Test Stack -> Group -> View -> Fixed")
+		{
+		});
+
+		TEST_CASE(L"Test Stack -> Shared -> View -> Fixed")
+		{
+		});
+	});
+
+	TEST_CATEGORY(L"Test shared controls management")
+	{
+	});
 }
