660e3aaf72eb9bb1fd1e59796aaf63d0030eee98
vczh <vczh@outlook.com>
2023-06-17 22:21:08 -0700

    Update TestCompositions_AllNested.cpp

--- Test/GacUISrc/UnitTest/TestCompositions_AllNested.cpp
+++ Test/GacUISrc/UnitTest/TestCompositions_AllNested.cpp
@@ -5,5 +5,117 @@ TEST_FILE
 {
 	TEST_CATEGORY(L"Test all compositions nested in other compositions")
 	{
+		auto root = new GuiSharedSizeRootComposition;
+		root->SetMinSizeLimitation(GuiGraphicsComposition::LimitToElementAndChildren);
+		root->SetPreferredMinSize(Size(340, 340));
+
+		// <Table> ======================================================
+
+		auto table = new GuiTableComposition;
+		root->AddChild(table);
+		table->SetMinSizeLimitation(GuiGraphicsComposition::LimitToElementAndChildren);
+		table->SetAlignmentToParent(Margin(10, 10, 10, 10));
+		table->SetBorderVisible(false);
+		table->SetCellPadding(10);
+		table->SetRowsAndColumns(3, 3);
+		table->SetRowOption(0, GuiCellOption::MinSizeOption());
+		table->SetRowOption(1, GuiCellOption::AbsoluteOption(100));
+		table->SetRowOption(2, GuiCellOption::PercentageOption(0.1));
+		table->SetColumnOption(0, GuiCellOption::MinSizeOption());
+		table->SetColumnOption(1, GuiCellOption::AbsoluteOption(100));
+		table->SetColumnOption(2, GuiCellOption::PercentageOption(0.1));
+
+		auto cellStack = new GuiCellComposition;
+		table->AddChild(cellStack);
+		cellStack->SetSite(0, 0, 1, 1);
+
+		auto cellFlow = new GuiCellComposition;
+		table->AddChild(cellFlow);
+		cellFlow->SetSite(1, 1, 1, 1);
+
+		auto cellSpec = new GuiCellComposition;
+		table->AddChild(cellSpec);
+		cellSpec->SetSite(2, 2, 1, 1);
+
+		// <Stack> ======================================================
+
+		auto stack = new GuiStackComposition;
+		cellStack->AddChild(stack);
+		stack->SetMinSizeLimitation(GuiGraphicsComposition::LimitToElementAndChildren);
+		stack->SetAlignmentToParent(Margin(10, 10, 10, 10));
+		stack->SetDirection(GuiStackComposition::Vertical);
+		stack->SetPadding(10);
+
+		GuiStackItemComposition* stackItems[3];
+		for (vint i = 0; i < sizeof(stackItems) / sizeof(*stackItems); i++)
+		{
+			auto& item = stackItems[i];
+			item = new GuiStackItemComposition;
+			stack->AddChild(item);
+
+			auto shared = new GuiSharedSizeItemComposition;
+			item->AddChild(shared);
+			shared->SetAlignmentToParent(Margin(0, 0, 0, 0));
+			shared->SetPreferredMinSize(Size(40 + i * 30, 20 - i * 5));
+			shared->SetSharedWidth(true);
+			shared->SetSharedHeight(true);
+			shared->SetGroup(L"stack");
+		}
+
+		// <Flow> =======================================================
+
+		// <Specialized> ================================================
+
+		// <SharedSize> =================================================
+
+		// <Bounds> =====================================================
+
+		root->ForceCalculateSizeImmediately();
+
+		TEST_CASE(L"<Table>")
+		{
+			TEST_ASSERT(table->GetCachedMinSize() == Size(320, 320));
+			TEST_ASSERT(table->GetCachedBounds() == Rect({ 10,10 }, { 320,320 }));
+			TEST_ASSERT(cellStack->GetCachedMinSize() == Size(100, 100));
+			TEST_ASSERT(cellStack->GetCachedBounds() == Rect({ 0,0 }, { 100,100 }));
+			TEST_ASSERT(cellFlow->GetCachedMinSize() == Size(100, 100));
+			TEST_ASSERT(cellFlow->GetCachedBounds() == Rect({ 110,110 }, { 100,100 }));
+			TEST_ASSERT(cellSpec->GetCachedMinSize() == Size(100, 100));
+			TEST_ASSERT(cellSpec->GetCachedBounds() == Rect({ 220,220 }, { 100,100 }));
+		});
+
+		TEST_CASE(L"<Stack>")
+		{
+		});
+
+		TEST_CASE(L"<Flow>")
+		{
+		});
+
+		TEST_CASE(L"<Specialized>")
+		{
+		});
+
+		TEST_CASE(L"<SharedSize>")
+		{
+			TEST_ASSERT(root->GetCachedMinSize() == Size(340, 340));
+			TEST_ASSERT(root->GetCachedBounds() == Rect({ 0,0 }, { 340,340 }));
+			for (auto item : stackItems)
+			{
+				auto shared = dynamic_cast<GuiSharedSizeItemComposition*>(item->Children()[0]);
+				TEST_ASSERT(shared->GetCachedMinSize() == Size(30, 30));
+				TEST_ASSERT(shared->GetCachedBounds() == Rect({ 0,0 }, { 100,30 }));
+			}
+		});
+
+		TEST_CASE(L"<Bounds>")
+		{
+		});
+
+		TEST_CASE(L"MinSizeLimitation <- NoLimite")
+		{
+		});
+
+		SafeDeleteComposition(root);
 	});
 }
