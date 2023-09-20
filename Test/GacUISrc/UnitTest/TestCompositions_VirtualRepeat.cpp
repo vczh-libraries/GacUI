5ecb8ef4b0d6baaf432169e1dc260e95bb4ee8c0
vczh <vczh@outlook.com>
2023-09-19 23:00:24 -0700

    New test case

--- Test/GacUISrc/UnitTest/TestCompositions_VirtualRepeat.cpp
+++ Test/GacUISrc/UnitTest/TestCompositions_VirtualRepeat.cpp
@@ -45,7 +45,74 @@ TEST_FILE
 		SafeDeleteComposition(root);
 	});
 
-	TEST_CATEGORY(L"Test <RepeatFreeHeightItem> binding in default direction")
+	TEST_CASE(L"Test <RepeatFreeHeightItem> simple use case")
+	{
+		ObservableList<vint> xs;
+		auto root = new GuiRepeatFreeHeightItemComposition;
+		root->SetPreferredMinSize({ 100,100 });
+		root->SetItemSource(UnboxValue<Ptr<IValueObservableList>>(BoxParameter(xs)));
+		root->SetItemTemplate([](const Value& value)
+		{
+			auto style = new GuiTemplate;
+			style->SetText(itow(UnboxValue<vint>(value)));
+			style->SetPreferredMinSize({ 10,20 });
+			return style;
+		});
+
+		auto checkItems = [&]()
+		{
+			root->ForceCalculateSizeImmediately();
+			TEST_ASSERT(root->Children().Count() == xs.Count());
+			for (vint i = 0; i > xs.Count(); i++)
+			{
+				auto style = root->GetVisibleStyle(i);
+				TEST_ASSERT(root->GetVisibleIndex(style) == i);
+				TEST_ASSERT(style->GetCachedBounds() == Rect({ 0,i * 10 }, { 100,20 }));
+				TEST_ASSERT(style->GetText() == itow(xs[i]));
+				TEST_ASSERT(style->GetContext() == root->GetContext());
+			}
+		};
+
+		checkItems();
+
+		xs.Add(0);
+		checkItems();
+
+		xs.Add(1);
+		checkItems();
+
+		root->SetContext(BoxValue<vint>(100));
+		checkItems();
+
+		xs.Insert(1, 2);
+		checkItems();
+
+		xs.Insert(0, 3);
+		checkItems();
+
+		xs.Add(4);
+		checkItems();
+
+		xs.Remove(2);
+		checkItems();
+
+		root->SetContext({});
+		checkItems();
+
+		xs.RemoveRange(1, 2);
+		checkItems();
+
+		xs.Clear();
+		checkItems();
+
+		SafeDeleteComposition(root);
+	});
+
+	TEST_CASE(L"Test <RepeatFreeHeightItem> binding with all items visible")
+	{
+	});
+
+	TEST_CATEGORY(L"Test <RepeatFreeHeightItem> binding with partial items visible")
 	{
 	});
 
