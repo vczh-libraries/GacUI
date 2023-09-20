07ae0ed3c6163458d8cca7962aa256574d291e19
vczh <vczh@outlook.com>
2023-09-19 22:49:10 -0700

    ...

--- Test/GacUISrc/UnitTest/TestCompositions_VirtualRepeat.cpp
+++ Test/GacUISrc/UnitTest/TestCompositions_VirtualRepeat.cpp
@@ -11,8 +11,38 @@ TEST_FILE
 		TestBoundsWithTrivialChildren<GuiRepeatFreeHeightItemComposition>();
 	});
 
-	TEST_CATEGORY(L"Test <RepeatFreeHeightItem> binding without item template")
+	TEST_CASE(L"Test <RepeatFreeHeightItem> binding without item template")
 	{
+		ObservableList<vint> xs;
+		auto root = new GuiRepeatFreeHeightItemComposition;
+		root->SetPreferredMinSize({ 100,100 });
+		root->SetItemSource(UnboxValue<Ptr<IValueObservableList>>(BoxParameter(xs)));
+		TEST_ASSERT(root->Children().Count() == 0);
+
+		root->ForceCalculateSizeImmediately();
+		TEST_ASSERT(root->Children().Count() == 0);
+
+		xs.Add(0);
+		root->ForceCalculateSizeImmediately();
+		TEST_ASSERT(root->Children().Count() == 0);
+
+		xs.Insert(0, 1);
+		root->ForceCalculateSizeImmediately();
+		TEST_ASSERT(root->Children().Count() == 0);
+
+		xs.Insert(1, 2);
+		root->ForceCalculateSizeImmediately();
+		TEST_ASSERT(root->Children().Count() == 0);
+
+		xs.RemoveAt(1);
+		root->ForceCalculateSizeImmediately();
+		TEST_ASSERT(root->Children().Count() == 0);
+
+		xs.Clear();
+		root->ForceCalculateSizeImmediately();
+		TEST_ASSERT(root->Children().Count() == 0);
+
+		SafeDeleteComposition(root);
 	});
 
 	TEST_CATEGORY(L"Test <RepeatFreeHeightItem> binding in default direction")
