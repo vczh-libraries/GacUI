dbb059ce245b08401b1c98e8f3f1b12a411a2bb8
vczh <vczh@outlook.com>
2024-02-07 23:28:50 -0800

    New test cases

--- Test/GacUISrc/UnitTest/TestRemote_GraphicsHost_BasicElements.cpp
+++ Test/GacUISrc/UnitTest/TestRemote_GraphicsHost_BasicElements.cpp
@@ -109,4 +109,131 @@ TEST_FILE
 		SetGuiMainProxy(MakeGuiMain(protocol, eventLogs, controlHost));
 		StartRemoteControllerTest(protocol);
 	});
+
+	TEST_CATEGORY(L"Other simple elements")
+	{
+		List<WString> eventLogs;
+		GraphicsHostRenderingProtocol protocol(eventLogs);
+		GuiWindow* controlHost = nullptr;
+
+		protocol.OnNextFrame([&]()
+		{
+			auto table = new GuiTableComposition;
+			table->SetAlignmentToParent(Margin(10, 10, 10, 10));
+			table->SetRowsAndColumns(3, 2);
+			table->SetRowOption(0, GuiCellOption::PercentageOption(0.5));
+			table->SetRowOption(1, GuiCellOption::MinSizeOption());
+			table->SetRowOption(2, GuiCellOption::PercentageOption(0.5));
+			table->SetColumnOption(0, GuiCellOption::MinSizeOption());
+			table->SetColumnOption(1, GuiCellOption::PercentageOption(1.0));
+			{
+				auto cell = new GuiCellComposition;
+				cell->SetSite(0, 0, 1, 1);
+				auto element = Ptr(Gui3DBorderElement::Create());
+				cell->SetOwnedElement(element);
+				table->AddChild(cell);
+
+				element->SetColor1(Color(255, 0, 0));
+				element->SetColor2(Color(0, 255, 0));
+			}
+			{
+				auto cell = new GuiCellComposition;
+				cell->SetSite(1, 0, 1, 1);
+				auto element = Ptr(GuiPolygonElement::Create());
+				cell->SetOwnedElement(element);
+				table->AddChild(cell);
+
+				Point points[] = {
+					{0,1},
+					{2,3},
+					{4,5},
+					{6,7}
+				};
+				element->SetSize({ 200,100 });
+				element->SetPoints(&*points, sizeof(points) / sizeof(*points));
+				element->SetBorderColor(Color(255, 0, 0));
+				element->SetBackgroundColor(Color(0, 255, 0));
+			}
+			{
+				auto cell = new GuiCellComposition;
+				cell->SetSite(2, 0, 1, 1);
+				auto element = Ptr(Gui3DSplitterElement::Create());
+				cell->SetOwnedElement(element);
+				table->AddChild(cell);
+
+				element->SetColor1(Color(255, 0, 0));
+				element->SetColor2(Color(0, 255, 0));
+				element->SetDirection(Gui3DSplitterElement::Horizontal);
+			}
+			{
+				auto cell = new GuiCellComposition;
+				cell->SetSite(0, 1, 1, 1);
+				auto element = Ptr(GuiSolidBackgroundElement::Create());
+				cell->SetOwnedElement(element);
+				table->AddChild(cell);
+
+				element->SetColor(Color(255, 0, 0));
+				element->SetShape({ ElementShapeType::Rectangle });
+			}
+			{
+				auto cell = new GuiCellComposition;
+				cell->SetSite(1, 1, 1, 1);
+				auto element = Ptr(GuiGradientBackgroundElement::Create());
+				cell->SetOwnedElement(element);
+				table->AddChild(cell);
+
+				element->SetColor1(Color(255, 0, 0));
+				element->SetColor2(Color(0, 255, 0));
+				element->SetDirection(GuiGradientBackgroundElement::Vertical);
+				element->SetShape({ ElementShapeType::Ellipse });
+			}
+			{
+				auto cell = new GuiCellComposition;
+				cell->SetSite(2, 1, 1, 1);
+				auto element = Ptr(GuiInnerShadowElement::Create());
+				cell->SetOwnedElement(element);
+				table->AddChild(cell);
+
+				element->SetColor(Color(255, 0, 0));
+				element->SetThickness(5);
+			}
+
+			controlHost->GetContainerComposition()->AddChild(table);
+			controlHost->ForceCalculateSizeImmediately();
+		});
+
+		protocol.OnNextFrame([&]()
+		{
+			AssertEventLogs(
+				eventLogs,
+
+				L"Created(<1:SinkBorder>)",
+				L"Created(<2:Polygon>)",
+				L"Created(<3:SinkSplitter>)",
+				L"Created(<4:SolidBackground>)",
+				L"Created(<5:GradientBackground>)",
+				L"Created(<6:InnerShadow>)",
+
+				L"Updated(1, #FF0000, #00FF00)"
+				L"Updated(2, {200,100}, {0,1:2,3:4,5:6,7}, #FF0000, #00FF00)"
+				L"Updated(3, #FF0000, #00FF00, Horizontal)"
+				L"Updated(4, #FF0000, Rectangle)"
+				L"Updated(5, #FF0000, #00FF00, Vertical, Ellipse)"
+				L"Updated(6, #FF0000, 5)"
+
+				L"Begin()",
+				L"Render(1, {10,10:200,180}, {10,10:620,460})",
+				L"Render(2, {10,190:200,100}, {10,10:620,460})",
+				L"Render(3, {10,290:200,180}, {10,10:620,460})",
+				L"Render(4, {210,10:420,180}, {10,10:620,460})",
+				L"Render(5, {210,190:420,100}, {10,10:620,460})",
+				L"Render(6, {210,290:420,180}, {10,10:620,460})",
+				L"End()"
+				);
+			controlHost->Hide();
+		});
+
+		SetGuiMainProxy(MakeGuiMain(protocol, eventLogs, controlHost));
+		StartRemoteControllerTest(protocol);
+	});
 }
