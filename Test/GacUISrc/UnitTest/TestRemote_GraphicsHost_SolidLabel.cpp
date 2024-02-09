ca162434a78c4cdddaef943caea168d1bf0fa4f4
vczh <vczh@outlook.com>
2024-02-09 00:53:30 -0800

    ...

--- Test/GacUISrc/UnitTest/TestRemote_GraphicsHost_SolidLabel.cpp
+++ Test/GacUISrc/UnitTest/TestRemote_GraphicsHost_SolidLabel.cpp
@@ -244,5 +244,118 @@ TEST_FILE
 
 	TEST_CATEGORY(L"Disconnect after caching font heights to trigger recache")
 	{
+		List<WString> eventLogs;
+		GraphicsHostRenderingProtocol protocol(eventLogs);
+		GuiWindow* controlHost = nullptr;
+		Ptr<GuiSolidLabelElement> e1, e2;
+
+		protocol.OnNextFrame([&]()
+		{
+			auto table = new GuiTableComposition;
+			table->SetAlignmentToParent(Margin(0, 0, 0, 0));
+			table->SetRowsAndColumns(3, 2);
+			table->SetRowOption(0, GuiCellOption::MinSizeOption());
+			table->SetRowOption(1, GuiCellOption::MinSizeOption());
+			table->SetRowOption(2, GuiCellOption::PercentageOption(1.0));
+			table->SetColumnOption(0, GuiCellOption::AbsoluteOption(100));
+			table->SetColumnOption(1, GuiCellOption::PercentageOption(1.0));
+			{
+				auto cell = new GuiCellComposition;
+				cell->SetSite(0, 0, 1, 1);
+				auto element = Ptr(GuiSolidLabelElement::Create());
+				cell->SetOwnedElement(element);
+				table->AddChild(cell);
+
+				element->SetText(L"Hello");
+				element->SetFont(GetCurrentController()->ResourceService()->GetDefaultFont());
+				e1 = element;
+			}
+			{
+				auto cell = new GuiCellComposition;
+				cell->SetSite(1, 0, 1, 1);
+				auto element = Ptr(GuiSolidLabelElement::Create());
+				cell->SetOwnedElement(element);
+				table->AddChild(cell);
+
+				element->SetText(L"World");
+				element->SetEllipse(true);
+				element->SetFont(GetCurrentController()->ResourceService()->GetDefaultFont());
+				e2 = element;
+			}
+
+			controlHost->GetContainerComposition()->AddChild(table);
+
+			protocol.measuringForNextRendering.minSizes = Ptr(new List<ElementMeasuring_ElementMinSize>);
+			protocol.measuringForNextRendering.minSizes->Add({ 1,{60,12} });
+
+			protocol.measuringForNextRendering.fontHeights = Ptr(new List<ElementMeasuring_FontHeight>);
+			protocol.measuringForNextRendering.fontHeights->Add({ L"One",12,12 });
+		});
+
+		protocol.OnNextFrame([&]()
+		{
+			// Render for the first time and send back size/height of texts
+			// No rendered element because the table is still 0x0
+			AssertEventLogs(
+				eventLogs,
+				L"Created(<1:SolidLabel>, <2:SolidLabel>)",
+				L"Updated(1, #000000, Left, Top, <flags:>, <font:One:12>, <text:Hello>, <request:TotalSize>)",
+				L"Updated(2, #000000, Left, Top, <flags:[e]>, <font:One:12>, <text:World>, <request:FontHeight>)",
+				L"Begin()",
+				L"End()"
+				);
+			TEST_ASSERT(!protocol.measuringForNextRendering.fontHeights);
+			TEST_ASSERT(!protocol.measuringForNextRendering.minSizes);
+		});
+
+		protocol.OnNextFrame([&]()
+		{
+			// Render for the second time and the size/height of texts is updated to the cells
+			AssertEventLogs(
+				eventLogs,
+				L"Begin()",
+				L"Render(1, {0,0:100,0}, {0,0:640,480})",
+				L"Render(2, {0,0:100,0}, {0,0:640,480})",
+				L"End()"
+				);
+		});
+
+		AssertRenderingEventLogs(
+			protocol,
+			eventLogs,
+			[&]()
+			{
+				protocol.events->OnControllerDisconnect();
+				protocol.events->OnControllerConnect();
+
+				protocol.measuringForNextRendering.minSizes = Ptr(new List<ElementMeasuring_ElementMinSize>);
+				protocol.measuringForNextRendering.minSizes->Add({ 1,{60,12} });
+
+				protocol.measuringForNextRendering.fontHeights = Ptr(new List<ElementMeasuring_FontHeight>);
+				protocol.measuringForNextRendering.fontHeights->Add({ L"One",12,12 });
+			},
+			// Layout stablized
+			L"Render(1, {0,0:100,12}, {0,0:640,480})",
+			L"Render(2, {0,12:100,12}, {0,0:640,480})"
+			);
+
+		protocol.OnNextFrame([&]()
+		{
+			// Reconnect and send back size/height of texts
+			// No rendered element because sizes are not changed
+			AssertEventLogs(
+				eventLogs,
+				L"Created(<1:SolidLabel>, <2:SolidLabel>)",
+				L"Updated(1, #000000, Left, Top, <flags:>, <font:One:12>, <text:Hello>, <request:TotalSize>)",
+				L"Updated(2, #000000, Left, Top, <flags:[e]>, <font:One:12>, <text:World>, <request:FontHeight>)"
+				);
+			TEST_ASSERT(!protocol.measuringForNextRendering.fontHeights);
+			TEST_ASSERT(!protocol.measuringForNextRendering.minSizes);
+
+			controlHost->Hide();
+		});
+
+		SetGuiMainProxy(MakeGuiMain(protocol, eventLogs, controlHost));
+		StartRemoteControllerTest(protocol);
 	});
 }
