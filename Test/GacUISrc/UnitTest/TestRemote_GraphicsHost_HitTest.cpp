7ce244a94a7792c807d2bff7ce45ba0a3460150d
vczh <vczh@outlook.com>
2024-02-16 03:40:09 -0800

    ...

--- Test/GacUISrc/UnitTest/TestRemote_GraphicsHost_HitTest.cpp
+++ Test/GacUISrc/UnitTest/TestRemote_GraphicsHost_HitTest.cpp
@@ -117,6 +117,90 @@ TEST_FILE
 
 	TEST_CATEGORY(L"Nested HitTest of the same value")
 	{
+		List<WString> eventLogs;
+		GraphicsHostRenderingProtocol protocol(eventLogs);
+		GuiWindow* controlHost = nullptr;
+
+		protocol.OnNextFrame([&]()
+		{
+			auto bounds = new GuiBoundsComposition;
+			bounds->SetAlignmentToParent(Margin(10, 10, 10, 10));
+			bounds->SetAssociatedHitTestResult(INativeWindowListener::Title);
+
+			auto childBounds = new GuiBoundsComposition;
+			childBounds->SetAlignmentToParent(Margin(10, 10, 10, 10));
+			childBounds->SetAssociatedHitTestResult(INativeWindowListener::Title);
+
+			auto element = Ptr(GuiFocusRectangleElement::Create());
+			childBounds->SetOwnedElement(element);
+			bounds->AddChild(childBounds);
+
+			controlHost->GetContainerComposition()->AddChild(bounds);
+			controlHost->ForceCalculateSizeImmediately();
+			TEST_ASSERT(bounds->GetCachedBounds() == Rect({ 10,10 }, { 620,460 }));
+		});
+
+		protocol.OnNextFrame([&]()
+		{
+			AssertEventLogs(
+				eventLogs,
+				L"Created(<1:FocusRectangle>)",
+				L"Begin()",
+				L"BeginBoundary(Title, {10,10:620,460}, {10,10:620,460})",
+				L"Render(1, {20,20:600,440}, {10,10:620,460})",
+				L"EndBoundary()",
+				L"End()"
+				);
+			controlHost->Hide();
+		});
+
+		SetGuiMainProxy(MakeGuiMain(protocol, eventLogs, controlHost));
+		StartRemoteControllerTest(protocol);
+	});
+
+	TEST_CATEGORY(L"Nested HitTest of the different values")
+	{
+		List<WString> eventLogs;
+		GraphicsHostRenderingProtocol protocol(eventLogs);
+		GuiWindow* controlHost = nullptr;
+
+		protocol.OnNextFrame([&]()
+		{
+			auto bounds = new GuiBoundsComposition;
+			bounds->SetAlignmentToParent(Margin(10, 10, 10, 10));
+			bounds->SetAssociatedHitTestResult(INativeWindowListener::Title);
+
+			auto childBounds = new GuiBoundsComposition;
+			childBounds->SetAlignmentToParent(Margin(10, 10, 10, 10));
+			childBounds->SetAssociatedHitTestResult(INativeWindowListener::Icon);
+
+			auto element = Ptr(GuiFocusRectangleElement::Create());
+			childBounds->SetOwnedElement(element);
+			bounds->AddChild(childBounds);
+
+			controlHost->GetContainerComposition()->AddChild(bounds);
+			controlHost->ForceCalculateSizeImmediately();
+			TEST_ASSERT(bounds->GetCachedBounds() == Rect({ 10,10 }, { 620,460 }));
+		});
+
+		protocol.OnNextFrame([&]()
+		{
+			AssertEventLogs(
+				eventLogs,
+				L"Created(<1:FocusRectangle>)",
+				L"Begin()",
+				L"BeginBoundary(Title, {10,10:620,460}, {10,10:620,460})",
+				L"Render(1, {20,20:600,440}, {10,10:620,460})",
+				L"BeginBoundary(Icon, {20,20:600,440}, {20,20:600,440})",
+				L"EndBoundary()",
+				L"EndBoundary()",
+				L"End()"
+				);
+			controlHost->Hide();
+		});
+
+		SetGuiMainProxy(MakeGuiMain(protocol, eventLogs, controlHost));
+		StartRemoteControllerTest(protocol);
 	});
 
 	TEST_CATEGORY(L"HitTest in non-main window")
