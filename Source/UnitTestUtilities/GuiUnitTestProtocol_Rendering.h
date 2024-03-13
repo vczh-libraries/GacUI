735c44dd4f490b33e90848f43a52861ac1bb8b98
vczh <vczh@outlook.com>
2024-03-13 02:53:20 -0700

    ...

--- Source/UnitTestUtilities/GuiUnitTestProtocol_Rendering.h
+++ Source/UnitTestUtilities/GuiUnitTestProtocol_Rendering.h
@@ -35,8 +35,9 @@ UnitTestRemoteProtocol
 		using ElementDescMap = collections::Dictionary<vint, ElementDescVariant>;
 		using ImageMetadataMap = collections::Dictionary<vint, remoteprotocol::ImageMetadata>;
 	public:
-		ElementDescMap				createdElements;
-		ImageMetadataMap			createdImages;
+		ElementDescMap							createdElements;
+		ImageMetadataMap						createdImages;
+		remoteprotocol::ElementMeasurings		measuringForNextRendering;
 
 		template<typename ...TArgs>
 		UnitTestRemoteProtocol_Rendering(TArgs&& ...args)
@@ -48,103 +49,209 @@ UnitTestRemoteProtocol
 IGuiRemoteProtocolMessages (Rendering)
 ***********************************************************************/
 
-		void RequestRendererCreated(const Ptr<collections::List<remoteprotocol::RendererCreation>>& arguments) override
-		{
-			CHECK_FAIL(L"Not Implemented!");
-		}
-
-		void RequestRendererDestroyed(const Ptr<collections::List<vint>>& arguments) override
-		{
-			CHECK_FAIL(L"Not Implemented!");
-		}
-
 		void RequestRendererBeginRendering() override
 		{
-			CHECK_FAIL(L"Not Implemented!");
 		}
 
 		void RequestRendererEndRendering(vint id) override
 		{
-			CHECK_FAIL(L"Not Implemented!");
+			this->GetEvents()->RespondRendererEndRendering(id, measuringForNextRendering);
+			measuringForNextRendering = {};
 		}
 
 		void RequestRendererBeginBoundary(const remoteprotocol::ElementBoundary& arguments) override
 		{
-			CHECK_FAIL(L"Not Implemented!");
 		}
 
 		void RequestRendererEndBoundary() override
 		{
-			CHECK_FAIL(L"Not Implemented!");
 		}
 
 		void RequestRendererRenderElement(const remoteprotocol::ElementRendering& arguments) override
 		{
-			CHECK_FAIL(L"Not Implemented!");
+#define ERROR_MESSAGE_PREFIX L"vl::presentation::unittest::UnitTestRemoteProtocol_Rendering<TProtocol>::RequestRendererRenderElement(const ElementRendering&)#"
+			CHECK_ERROR(createdElements.Keys().Contains(arguments.id), ERROR_MESSAGE_PREFIX L"Renderer with the specified id has not been created.");
+#undef ERROR_MESSAGE_PREFIX
 		}
+
+/***********************************************************************
+IGuiRemoteProtocolMessages (Elements)
+***********************************************************************/
+
+		void RequestRendererCreated(const Ptr<collections::List<remoteprotocol::RendererCreation>>& arguments) override
+		{
+#define ERROR_MESSAGE_PREFIX L"vl::presentation::unittest::UnitTestRemoteProtocol_Rendering<TProtocol>::RequestRendererCreated(const Ptr<List<RendererCreation>>&)#"
+			if (arguments)
+			{
+				for (auto creation : *arguments.Obj())
+				{
+					CHECK_ERROR(!createdElements.Keys().Contains(creation.id), ERROR_MESSAGE_PREFIX L"Renderer with the specified id has been created.");
+					createdElements.Add(creation.id, creation.type);
+				}
+			}
+#undef ERROR_MESSAGE_PREFIX
+		}
+
+		void RequestRendererDestroyed(const Ptr<collections::List<vint>>& arguments) override
+		{
+#define ERROR_MESSAGE_PREFIX L"vl::presentation::unittest::UnitTestRemoteProtocol_Rendering<TProtocol>::RequestRendererDestroyed(const Ptr<List<vint>>&)#"
+			if (arguments)
+			{
+				for (auto id : *arguments.Obj())
+				{
+					CHECK_ERROR(createdElements.Keys().Contains(id), ERROR_MESSAGE_PREFIX L"Renderer with the specified id has not been created.");
+					createdElements.Remove(id);
+				}
+			}
+#undef ERROR_MESSAGE_PREFIX
+		}
+
+		template<remoteprotocol::RendererType RendererType, typename TElementDesc>
+		void RequestRendererUpdateElement(const TElementDesc& arguments, const wchar_t* emWrongId, const wchar_t* emWrongType)
+		{
+			vint index = createdElements.Keys().IndexOf(arguments.id);
+			CHECK_ERROR(index != -1, emWrongId);
+
+			auto& element = const_cast<collections::List<ElementDescVariant>&>(createdElements.Values())[index];
+			{
+				auto rendererType = element.TryGet<remoteprotocol::RendererType>();
+				if (rendererType) element = arguments;
+				return;
+			}
+			{
+				auto desc = element.TryGet<TElementDesc>();
+				if (desc) *desc = arguments;
+			}
+
+			CHECK_FAIL(emWrongType);
+		}
+
+#define REQUEST_RENDERER_UPDATE_ELEMENT(RENDERER_TYPE)\
+			RequestRendererUpdateElement<RENDERER_TYPE>(\
+				arguments,\
+				ERROR_MESSAGE_PREFIX L"Renderer with the specified id has not been created.",\
+				ERROR_MESSAGE_PREFIX L"Renderer with the specified id is not of the expected type."\
+				)
 
 		void RequestRendererUpdateElement_SolidBorder(const remoteprotocol::ElementDesc_SolidBorder& arguments) override
 		{
-			CHECK_FAIL(L"Not Implemented!");
+#define ERROR_MESSAGE_PREFIX L"vl::presentation::unittest::RequestRendererUpdateElement_SolidBorder<TProtocol>::RequestRendererCreated(const ElementDesc_SolidBorder&)#"
+			REQUEST_RENDERER_UPDATE_ELEMENT(remoteprotocol::RendererType::SolidBorder);
+#undef ERROR_MESSAGE_PREFIX
 		}
 
 		void RequestRendererUpdateElement_SinkBorder(const remoteprotocol::ElementDesc_SinkBorder& arguments) override
 		{
-			CHECK_FAIL(L"Not Implemented!");
+#define ERROR_MESSAGE_PREFIX L"vl::presentation::unittest::RequestRendererUpdateElement_SinkBorder<TProtocol>::RequestRendererCreated(const ElementDesc_SinkBorder&)#"
+			REQUEST_RENDERER_UPDATE_ELEMENT(remoteprotocol::RendererType::SinkBorder);
+#undef ERROR_MESSAGE_PREFIX
 		}
 
 		void RequestRendererUpdateElement_SinkSplitter(const remoteprotocol::ElementDesc_SinkSplitter& arguments) override
 		{
-			CHECK_FAIL(L"Not Implemented!");
+#define ERROR_MESSAGE_PREFIX L"vl::presentation::unittest::RequestRendererUpdateElement_SinkSplitter<TProtocol>::RequestRendererCreated(const ElementDesc_SinkSplitter&)#"
+			REQUEST_RENDERER_UPDATE_ELEMENT(remoteprotocol::RendererType::SinkSplitter);
+#undef ERROR_MESSAGE_PREFIX
 		}
 
 		void RequestRendererUpdateElement_SolidBackground(const remoteprotocol::ElementDesc_SolidBackground& arguments) override
 		{
-			CHECK_FAIL(L"Not Implemented!");
+#define ERROR_MESSAGE_PREFIX L"vl::presentation::unittest::RequestRendererUpdateElement_SolidBackground<TProtocol>::RequestRendererCreated(const ElementDesc_SolidBackground&)#"
+			REQUEST_RENDERER_UPDATE_ELEMENT(remoteprotocol::RendererType::SolidBackground);
+#undef ERROR_MESSAGE_PREFIX
 		}
 
 		void RequestRendererUpdateElement_GradientBackground(const remoteprotocol::ElementDesc_GradientBackground& arguments) override
 		{
-			CHECK_FAIL(L"Not Implemented!");
+#define ERROR_MESSAGE_PREFIX L"vl::presentation::unittest::RequestRendererUpdateElement_GradientBackground<TProtocol>::RequestRendererCreated(const ElementDesc_GradientBackground&)#"
+			REQUEST_RENDERER_UPDATE_ELEMENT(remoteprotocol::RendererType::GradientBackground);
+#undef ERROR_MESSAGE_PREFIX
 		}
 
 		void RequestRendererUpdateElement_InnerShadow(const remoteprotocol::ElementDesc_InnerShadow& arguments) override
 		{
-			CHECK_FAIL(L"Not Implemented!");
+#define ERROR_MESSAGE_PREFIX L"vl::presentation::unittest::RequestRendererUpdateElement_InnerShadow<TProtocol>::RequestRendererCreated(const ElementDesc_InnerShadow&)#"
+			REQUEST_RENDERER_UPDATE_ELEMENT(remoteprotocol::RendererType::InnerShadow);
+#undef ERROR_MESSAGE_PREFIX
 		}
 
 		void RequestRendererUpdateElement_Polygon(const remoteprotocol::ElementDesc_Polygon& arguments) override
 		{
-			CHECK_FAIL(L"Not Implemented!");
+#define ERROR_MESSAGE_PREFIX L"vl::presentation::unittest::RequestRendererUpdateElement_Polygon<TProtocol>::RequestRendererCreated(const ElementDesc_Polygon&)#"
+			REQUEST_RENDERER_UPDATE_ELEMENT(remoteprotocol::RendererType::Polygon);
+#undef ERROR_MESSAGE_PREFIX
 		}
 
 /***********************************************************************
-IGuiRemoteProtocolMessages (Rendering - SolidLabel)
+IGuiRemoteProtocolMessages (Elements - SolidLabel)
 ***********************************************************************/
 
 		void RequestRendererUpdateElement_SolidLabel(const remoteprotocol::ElementDesc_SolidLabel& arguments) override
 		{
-			CHECK_FAIL(L"Not Implemented!");
+#define ERROR_MESSAGE_PREFIX L"vl::presentation::unittest::RequestRendererUpdateElement_SolidLabel<TProtocol>::RequestRendererCreated(const ElementDesc_SolidLabel&)#"
+			if (arguments.measuringRequest)
+			{
+				CHECK_FAIL(L"Not Implemented!");
+			}
+			REQUEST_RENDERER_UPDATE_ELEMENT(remoteprotocol::RendererType::SolidLabel);
+#undef ERROR_MESSAGE_PREFIX
 		}
 
 /***********************************************************************
-IGuiRemoteProtocolMessages (Rendering - Image)
+IGuiRemoteProtocolMessages (Elements - Image)
 ***********************************************************************/
+
+		remoteprotocol::ImageMetadata MakeImageMetadata(const remoteprotocol::ImageCreation& arguments)
+		{
+			CHECK_FAIL(L"Not Implemented!");
+		}
 
 		void RequestImageCreated(vint id, const remoteprotocol::ImageCreation& arguments) override
 		{
-			CHECK_FAIL(L"Not Implemented!");
+#define ERROR_MESSAGE_PREFIX L"vl::presentation::unittest::UnitTestRemoteProtocol_Rendering<TProtocol>::RequestImageCreated(vint, const vint&)#"
+			CHECK_ERROR(!createdElements.Keys().Contains(arguments.id), ERROR_MESSAGE_PREFIX L"Image with the specified id has been created.");
+			this->GetEvents()->RespondImageCreated(id, MakeImageMetadata(arguments));
+#undef ERROR_MESSAGE_PREFIX
 		}
 
 		void RequestImageDestroyed(const vint& arguments) override
 		{
-			CHECK_FAIL(L"Not Implemented!");
+#define ERROR_MESSAGE_PREFIX L"vl::presentation::unittest::UnitTestRemoteProtocol_Rendering<TProtocol>::RequestImageDestroyed(const vint&)#"
+			CHECK_ERROR(createdElements.Keys().Contains(arguments), ERROR_MESSAGE_PREFIX L"Image with the specified id has not been created.");
+			createdElements.Remove(arguments);
+#undef ERROR_MESSAGE_PREFIX
 		}
 
 		void RequestRendererUpdateElement_ImageFrame(const remoteprotocol::ElementDesc_ImageFrame& arguments) override
 		{
-			CHECK_FAIL(L"Not Implemented!");
+#define ERROR_MESSAGE_PREFIX L"vl::presentation::unittest::RequestRendererUpdateElement_ImageFrame<TProtocol>::RequestRendererCreated(const ElementDesc_ImageFrame&)#"
+			if (arguments.imageCreation)
+			{
+				if (!arguments.imageCreation.Value().imageDataOmitted)
+				{
+					CHECK_ERROR(arguments.id != !arguments.imageCreation.Value().id, ERROR_MESSAGE_PREFIX L"It should satisfy that (arguments.id == arguments.imageCreation.Value().id).");
+					CHECK_ERROR(!createdElements.Keys().Contains(arguments.id), ERROR_MESSAGE_PREFIX L"Image with the specified id has been created.");
+					CHECK_ERROR(arguments.imageCreation.Value().imageData, ERROR_MESSAGE_PREFIX L"When imageDataOmitted == false, imageData should not be null.");
+					if (!measuringForNextRendering.createdImages)
+					{
+						measuringForNextRendering.createdImages = Ptr(new collections::List<remoteprotocol::ImageMetadata>);
+					}
+					measuringForNextRendering.createdImages->Add(MakeImageMetadata(arguments.imageCreation.Value()));
+				}
+				else
+				{
+					CHECK_ERROR(!arguments.imageCreation.Value().imageData, ERROR_MESSAGE_PREFIX L"When imageDataOmitted == true, imageData should be null.");
+				}
+			}
+			else
+			{
+				CHECK_ERROR(createdElements.Keys().Contains(arguments.id), ERROR_MESSAGE_PREFIX L"Image with the specified id has not been created.");
+			}
+			REQUEST_RENDERER_UPDATE_ELEMENT(remoteprotocol::RendererType::ImageFrame);
+#undef ERROR_MESSAGE_PREFIX
 		}
+
+#undef REQUEST_RENDERER_UPDATE_ELEMENT
 	};
 }
 
