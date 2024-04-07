5aeca2b17541465bf446415bf030cedd11c9c779
vczh <vczh@outlook.com>
2024-04-07 05:31:16 -0700

    One more snapshot file for testing snapshot viewer

--- Test/GacUISrc/UnitTest/TestRemote_UnitTestFramework.cpp
+++ Test/GacUISrc/UnitTest/TestRemote_UnitTestFramework.cpp
@@ -8,13 +8,66 @@
 using namespace vl;
 using namespace vl::stream;
 using namespace vl::presentation;
+using namespace vl::presentation::elements;
+using namespace vl::presentation::compositions;
 using namespace vl::presentation::controls;
 using namespace vl::presentation::unittest;
+
+namespace unittest_framework_tests
+{
+	class EmptyWindowTheme : public theme::ThemeTemplates
+	{
+	public:
+		EmptyWindowTheme()
+		{
+			Name = WString::Unmanaged(L"EmptyWindowTheme");
+			PreferCustomFrameWindow = true;
+
+			CustomFrameWindow = [](auto)
+			{
+				auto ct = new templates::GuiWindowTemplate;
+				ct->SetMinSizeLimitation(GuiGraphicsComposition::LimitToElementAndChildren);
+				ct->SetContainerComposition(ct);
+				ct->SetFocusableComposition(ct);
+
+				auto background = Ptr(GuiSolidBackgroundElement::Create());
+				background->SetColor({ 255,255,255 });
+				ct->SetOwnedElement(background);
+				return ct;
+			};
+		}
+	};
+}
+using namespace unittest_framework_tests;
 
 TEST_FILE
 {
 	TEST_CATEGORY(L"DOM Recovery")
 	{
+		TEST_CATEGORY(L"Empty Window")
+		{
+			GacUIUnitTest_SetGuiMainProxy([](UnitTestRemoteProtocol* protocol, IUnitTestContext*)
+			{
+				protocol->GetEvents()->OnControllerConnect();
+				auto emptyWindowTheme = Ptr(new EmptyWindowTheme);
+				theme::RegisterTheme(emptyWindowTheme);
+
+				GuiWindow window(theme::ThemeName::Window);
+				window.SetClientSize({ 640,480 });
+				window.MoveToScreenCenter();
+
+				protocol->OnNextIdleFrame([&]()
+				{
+					TEST_ASSERT(protocol->GetLoggedRenderingResults().Count() == 1);
+					window.Hide();
+				});
+
+				GetApplication()->Run(&window);
+				theme::UnregisterTheme(emptyWindowTheme->Name);
+			});
+			GacUIUnitTest_Start(WString::Unmanaged(L"DomRecovery/EmptyWindow"));
+		});
+
 		// TODO: test dom recovery from logging
 	});
 
@@ -40,6 +93,6 @@ TEST_FILE
 			GetApplication()->Run(&window);
 			theme::UnregisterTheme(darkskinTheme->Name);
 		});
-		GacUIUnitTest_Start(WString::Unmanaged(L"UnitTestFramework_HelloWorld"));
+		GacUIUnitTest_Start(WString::Unmanaged(L"HelloWorld"));
 	});
 }
