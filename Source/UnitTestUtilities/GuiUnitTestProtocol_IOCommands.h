3159aa6e716c6feac0cd36b81a65603ee75fb14a
vczh <vczh@outlook.com>
2024-05-23 04:24:18 -0700

    ...

--- Source/UnitTestUtilities/GuiUnitTestProtocol_IOCommands.h
+++ Source/UnitTestUtilities/GuiUnitTestProtocol_IOCommands.h
@@ -20,6 +20,36 @@ UnitTestRemoteProtocol
 	class UnitTestRemoteProtocol_IOCommands : public TProtocol
 	{
 	protected:
+		Nullable<NativePoint>				mousePosition;
+		collections::SortedList<VKEY>		pressingKeys;
+		bool								leftPressing = false;
+		bool								middlePressing = false;
+		bool								rightPressing = false;
+
+		IGuiRemoteProtocolEvents& UseEvents()
+		{
+			return *this->GetEvents();
+		}
+
+		bool IsPressing(VKEY key)
+		{
+			return pressingKeys.Contains(key);
+		}
+
+		NativeWindowMouseInfo MakeMouseInfo(NativePoint position)
+		{
+			NativeWindowMouseInfo info;
+			info.ctrl = IsPressing(VKEY::KEY_CONTROL) || IsPressing(VKEY::KEY_LCONTROL) || IsPressing(VKEY::KEY_RCONTROL);
+			info.shift = IsPressing(VKEY::KEY_SHIFT) || IsPressing(VKEY::KEY_LSHIFT) || IsPressing(VKEY::KEY_RSHIFT);
+			info.left = leftPressing;
+			info.middle = middlePressing;
+			info.right = rightPressing;
+			info.x = position.x.value;
+			info.y = position.y.value;
+			info.wheel = 0;
+			info.nonClient = false;
+		}
+
 	public:
 
 		template<typename ...TArgs>
@@ -27,6 +57,10 @@ UnitTestRemoteProtocol
 			: TProtocol(std::forward<TArgs&&>(args)...)
 		{
 		}
+
+/***********************************************************************
+Helper Functions
+***********************************************************************/
 
 		NativePoint LocationOf(compositions::GuiGraphicsComposition* composition, double ratioX = 0.5, double ratioY = 0.5, vint offsetX = 0, vint offsetY = 0)
 		{
@@ -42,6 +76,29 @@ UnitTestRemoteProtocol
 		NativePoint LocationOf(controls::GuiControl* control, double ratioX = 0.5, double ratioY = 0.5, vint offsetX = 0, vint offsetY = 0)
 		{
 			return LocationOf(control->GetBoundsComposition(), ratioX, ratioY, offsetX, offsetY);
+		}
+
+/***********************************************************************
+Keys
+***********************************************************************/
+
+/***********************************************************************
+Mouse
+***********************************************************************/
+
+		void MouseMove(NativePoint position)
+		{
+			if (!mousePosition)
+			{
+				UseEvents().OnIOMouseEntered();
+				goto DO_MOUSE_MOVE;
+			}
+
+			if (mousePosition.Value() == position) return;
+		DO_MOUSE_MOVE:
+
+			mousePosition = position;
+			UseEvents().OnIOMouseMoving(MakeMouseInfo(position));
 		}
 	};
 }
