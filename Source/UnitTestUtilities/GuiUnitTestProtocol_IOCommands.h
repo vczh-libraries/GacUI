2052c44515afbe38959e0d6d429f437939b9eb19
vczh <vczh@outlook.com>
2024-05-23 03:57:28 -0700

    Update GuiUnitTestProtocol_IOCommands.h

--- Source/UnitTestUtilities/GuiUnitTestProtocol_IOCommands.h
+++ Source/UnitTestUtilities/GuiUnitTestProtocol_IOCommands.h
@@ -27,6 +27,22 @@ UnitTestRemoteProtocol
 			: TProtocol(std::forward<TArgs&&>(args)...)
 		{
 		}
+
+		NativePoint LocationOf(compositions::GuiGraphicsComposition* composition, double ratioX = 0.5, double ratioY = 0.5, vint offsetX = 0, vint offsetY = 0)
+		{
+			INativeWindow* nativeWindow = composition->GetRelatedControlHost()->GetNativeWindow();
+			Rect bounds = composition->GetGlobalBounds();
+			NativeRect nativeBounds = nativeWindow->Convert(bounds);
+			vint x = nativeBounds.x1 + (vint)(nativeBounds.Width().value * ratioX) + offsetX;
+			vint y = nativeBounds.y1 + (vint)(nativeBounds.Height().value * ratioY) + offsetY;
+			NativePoint windowLocation = nativeWindow->GetBounds().LeftTop();
+			return { windowLocation.x.value + x,windowLocation.y.value + y };
+		}
+
+		NativePoint LocationOf(controls::GuiControl* control, double ratioX = 0.5, double ratioY = 0.5, vint offsetX = 0, vint offsetY = 0)
+		{
+			return LocationOf(control->GetBoundsComposition(), ratioX, ratioY, offsetX, offsetY);
+		}
 	};
 }
 
