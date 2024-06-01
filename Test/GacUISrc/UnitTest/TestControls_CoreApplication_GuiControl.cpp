485491c615f1894a483be934771eb1b6972cc197
vczh <vczh@outlook.com>
2024-05-31 21:48:21 -0700

    ...

--- Test/GacUISrc/UnitTest/TestControls_CoreApplication_GuiControl.cpp
+++ Test/GacUISrc/UnitTest/TestControls_CoreApplication_GuiControl.cpp
@@ -20,13 +20,10 @@ TEST_FILE
 <Resource>
   <Instance name="MyControlTemplate">
     <Instance ref.Class="gacuisrc_unittest::MyControlTemplate">
-      <ControlTemplate ref.Name="self" ContainerComposition-ref="container">
+      <ControlTemplate ref.Name="self" ContainerComposition-ref="container" MinSizeLimitation="LimitToElementAndChildren">
         <SolidBorder Color="#FFFFFF"/>
-        <Table CellPadding="2" BorderVisible="true" AlignmentToParent="left:0 top:0 right:0 bottom:0" MinSizeLimitation="LimitToElementAndChildren">
+        <Table CellPadding="5" BorderVisible="true" AlignmentToParent="left:0 top:0 right:0 bottom:0" MinSizeLimitation="LimitToElementAndChildren">
           <att.Rows>
-            <_>composeType:MinSize</_>
-            <_>composeType:MinSize</_>
-            <_>composeType:MinSize</_>
             <_>composeType:MinSize</_>
             <_>composeType:MinSize</_>
             <_>composeType:MinSize</_>
@@ -35,27 +32,15 @@ TEST_FILE
           </att.Rows>
           <att.Columns>
             <_>composeType:MinSize</_>
-            <_>composeType:MinSize</_>
             <_>composeType:Percentage percentage:1.0</_>
           </att.Columns>
 
-          <Cell Site="row:0 column:0 columnSpan:2"> <SolidLabel Font-bind="self.Font" Text="Text: "/>            </Cell>
-          <Cell Site="row:1 column:0">              <SolidLabel Font-bind="self.Font" Text="Enabled: "/>         </Cell>
-          <Cell Site="row:2 column:0">              <SolidLabel Font-bind="self.Font" Text="VisuallyEnabled: "/> </Cell>
-          <Cell Site="row:3 column:0">              <SolidLabel Font-bind="self.Font" Text="Font: "/>            </Cell>
-          <Cell Site="row:4 column:0">              <SolidLabel Font-bind="self.Font" Text="DisplayFont: "/>     </Cell>
-          <Cell Site="row:5 column:0">              <SolidLabel Font-bind="self.Font" Text="Focused: "/>         </Cell>
-          <Cell Site="row:6 column:0">              <SolidLabel Font-bind="self.Font" Text="Alt: "/>             </Cell>
-                                                    
-          <Cell Site="row:0 column:2"> <SolidLabel Font-bind="self.Font" Text-bind="self.Text"/>                                     </Cell>
-          <Cell Site="row:1 column:1"> <SolidLabel Font-bind="self.Font" Text-bind="self.Enabled ? 'Yes' : 'No'"/>                   </Cell>
-          <Cell Site="row:2 column:1"> <SolidLabel Font-bind="self.Font" Text-bind="self.VisuallyEnabled ? 'Yes' : 'No'"/>           </Cell>
-          <Cell Site="row:3 column:1"> <SolidLabel Font-bind="self.Font" Text-bind="self.Font is null ? '' : self.Font.fontFamily"/> </Cell>
-          <Cell Site="row:4 column:1"> <SolidLabel Font-bind="self.Font" Text-bind="self.DisplayFont.fontFamily"/>                   </Cell>
-          <Cell Site="row:5 column:1"> <SolidLabel Font-bind="self.Font" Text-bind="self.Focused ? 'Yes' : 'No'"/>                   </Cell>
-          <Cell Site="row:6 column:1"> <SolidLabel Font-bind="self.Font" Text-bind="self.Alt"/>                                      </Cell>
+          <Cell Site="row:0 column:0 columnSpan:2"> <SolidLabel Font-bind="self.Font" Color="#FFFFFF" Text-bind="self.Text"/>                                     </Cell>
+          <Cell Site="row:1 column:0 columnSpan:2"> <SolidLabel Font-bind="self.Font" Color="#FFFFFF" Text-bind="self.Font.fontFamily"/>                          </Cell>
+          <Cell Site="row:2 column:0">              <SolidLabel Font-bind="self.Font" Color="#FFFFFF" Text-bind="self.VisuallyEnabled ? 'Enabled' : 'Disabled'"/> </Cell>
+          <Cell Site="row:3 column:0">              <SolidLabel Font-bind="self.Font" Color="#FFFFFF" Text-bind="self.Focused ? 'Focused' : 'Unfocused'"/>        </Cell>
 
-          <Cell Site="row:1 column:1 rowSpan:7 columnSpan:2">
+          <Cell Site="row:2 column:1 rowSpan:3">
             <SolidBorder Color="#FFFFFF"/>
             <Bounds ref.Name="container" AlignmentToParent="left:1 top:1 right:1 bottom:1" MinSizeLimitation="LimitToElementAndChildren"/>
           </Cell>
@@ -66,7 +51,7 @@ TEST_FILE
 
   <Instance name="MainWindowResource">
     <Instance ref.Class="gacuisrc_unittest::MainWindow" xmlns:ut="gacuisrc_unittest::*">
-      <Window ref.Name="self" Text="GuiLabel" ClientSize="x:320 y:240">
+      <Window ref.Name="self" Text="MyControlTemplate" ClientSize="x:320 y:240">
         <CustomControl Text="This is a control with a template">
           <att.ControlTemplate>ut:MyControlTemplate</att.ControlTemplate>
         </CustomControl>
@@ -88,7 +73,7 @@ TEST_FILE
 			});
 		});
 		GacUIUnitTest_StartFast_WithResourceAsText<darkskin::Theme>(
-			WString::Unmanaged(L"Controls/CoreApplication/GuiLabel"),
+			WString::Unmanaged(L"Controls/CoreApplication/GuiControl/MyControlTemplate"),
 			WString::Unmanaged(L"gacuisrc_unittest::MainWindow"),
 			resource
 			);
