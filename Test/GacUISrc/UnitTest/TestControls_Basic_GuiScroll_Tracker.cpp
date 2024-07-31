2117c373c6b97b8645ddde2bd96f391aa07086a7
vczh <vczh@outlook.com>
2024-07-31 04:46:11 -0700

    ...

--- Test/GacUISrc/UnitTest/TestControls_Basic_GuiScroll_Tracker.cpp
+++ Test/GacUISrc/UnitTest/TestControls_Basic_GuiScroll_Tracker.cpp
@@ -21,7 +21,7 @@ TEST_FILE
   <Instance name="MainWindowResource">
     <Instance ref.Class="gacuisrc_unittest::MainWindow">
       <Window ref.Name="self" Text="GuiTab" ClientSize="x:320 y:240">
-        <HTracker ref.Name="progress" TotalSize="10" SmallMove="1" BigMove="3">
+        <HTracker ref.Name="tracker" TotalSize="10" SmallMove="1" BigMove="3">
           <att.BoundsComposition-set PreferredMinSize="x:150 y:20" AlignmentToParent="left:5 top:5 right:-1 bottom:-1"/>
         </HTracker>
       </Window>
@@ -35,7 +35,7 @@ TEST_FILE
   <Instance name="MainWindowResource">
     <Instance ref.Class="gacuisrc_unittest::MainWindow">
       <Window ref.Name="self" Text="GuiTab" ClientSize="x:320 y:240">
-        <VTracker ref.Name="progress" TotalSize="10" SmallMove="1" BigMove="3">
+        <VTracker ref.Name="tracker" TotalSize="10" SmallMove="1" BigMove="3">
           <att.BoundsComposition-set PreferredMinSize="x:20 y:150" AlignmentToParent="left:5 top:5 right:-1 bottom:-1"/>
         </VTracker>
       </Window>
@@ -51,6 +51,43 @@ TEST_FILE
 			GacUIUnitTest_SetGuiMainProxy([](UnitTestRemoteProtocol* protocol, IUnitTestContext*)
 			{
 				protocol->OnNextIdleFrame(L"Ready", [=]()
+				{
+					auto window = GetApplication()->GetMainWindow();
+					auto tracker = FindObjectByName<GuiScroll>(window, L"tracker");
+					TEST_ASSERT(tracker->GetTotalSize() == 10);
+					TEST_ASSERT(tracker->GetPageSize() == 0);
+					TEST_ASSERT(tracker->GetSmallMove() == 1);
+					TEST_ASSERT(tracker->GetBigMove() == 3);
+					TEST_ASSERT(tracker->GetPosition() == 0);
+					TEST_ASSERT(tracker->GetMinPosition() == 0);
+					TEST_ASSERT(tracker->GetMaxPosition() == 10);
+					TEST_ASSERT(tracker->GetAutoFocus() == true);
+					tracker->SetPosition(1);
+					TEST_ASSERT(tracker->GetPosition() == 1);
+					tracker->SetPosition(-1);
+					TEST_ASSERT(tracker->GetPosition() == 0);
+					tracker->SetPosition(1);
+					TEST_ASSERT(tracker->GetPosition() == 1);
+				});
+				protocol->OnNextIdleFrame(L"1", [=]()
+				{
+					auto window = GetApplication()->GetMainWindow();
+					auto tracker = FindObjectByName<GuiScroll>(window, L"tracker");
+					tracker->SetPosition(3);
+					TEST_ASSERT(tracker->GetPosition() == 3);
+				});
+				protocol->OnNextIdleFrame(L"3", [=]()
+				{
+					auto window = GetApplication()->GetMainWindow();
+					auto tracker = FindObjectByName<GuiScroll>(window, L"tracker");
+					tracker->SetPosition(11);
+					TEST_ASSERT(tracker->GetPosition() == 10);
+					tracker->SetPosition(3);
+					TEST_ASSERT(tracker->GetPosition() == 3);
+					tracker->SetPosition(10);
+					TEST_ASSERT(tracker->GetPosition() == 10);
+				});
+				protocol->OnNextIdleFrame(L"10", [=]()
 				{
 					auto window = GetApplication()->GetMainWindow();
 					window->Hide();
@@ -71,6 +108,43 @@ TEST_FILE
 			GacUIUnitTest_SetGuiMainProxy([](UnitTestRemoteProtocol* protocol, IUnitTestContext*)
 			{
 				protocol->OnNextIdleFrame(L"Ready", [=]()
+				{
+					auto window = GetApplication()->GetMainWindow();
+					auto tracker = FindObjectByName<GuiScroll>(window, L"tracker");
+					TEST_ASSERT(tracker->GetTotalSize() == 10);
+					TEST_ASSERT(tracker->GetPageSize() == 0);
+					TEST_ASSERT(tracker->GetSmallMove() == 1);
+					TEST_ASSERT(tracker->GetBigMove() == 3);
+					TEST_ASSERT(tracker->GetPosition() == 0);
+					TEST_ASSERT(tracker->GetMinPosition() == 0);
+					TEST_ASSERT(tracker->GetMaxPosition() == 10);
+					TEST_ASSERT(tracker->GetAutoFocus() == true);
+					tracker->SetPosition(1);
+					TEST_ASSERT(tracker->GetPosition() == 1);
+					tracker->SetPosition(-1);
+					TEST_ASSERT(tracker->GetPosition() == 0);
+					tracker->SetPosition(1);
+					TEST_ASSERT(tracker->GetPosition() == 1);
+				});
+				protocol->OnNextIdleFrame(L"1", [=]()
+				{
+					auto window = GetApplication()->GetMainWindow();
+					auto tracker = FindObjectByName<GuiScroll>(window, L"tracker");
+					tracker->SetPosition(3);
+					TEST_ASSERT(tracker->GetPosition() == 3);
+				});
+				protocol->OnNextIdleFrame(L"3", [=]()
+				{
+					auto window = GetApplication()->GetMainWindow();
+					auto tracker = FindObjectByName<GuiScroll>(window, L"tracker");
+					tracker->SetPosition(11);
+					TEST_ASSERT(tracker->GetPosition() == 10);
+					tracker->SetPosition(3);
+					TEST_ASSERT(tracker->GetPosition() == 3);
+					tracker->SetPosition(10);
+					TEST_ASSERT(tracker->GetPosition() == 10);
+				});
+				protocol->OnNextIdleFrame(L"10", [=]()
 				{
 					auto window = GetApplication()->GetMainWindow();
 					window->Hide();
