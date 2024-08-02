cd2bf19200fdfc2a45eb4c5199dcf0765ea00d94
vczh <vczh@outlook.com>
2024-08-02 15:07:43 -0700

    GuiListControl

---
+++ Test/GacUISrc/UnitTest/TestControls_List_GuiSelectableListControl.cpp
@@ -0,0 +1,52 @@
+#include "../../../Source//UnitTestUtilities/GuiUnitTestUtilities.h"
+#ifdef VCZH_64
+#include "../Generated_DarkSkin/Source_x64/DarkSkin.h"
+#else
+#include "../Generated_DarkSkin/Source_x86/DarkSkin.h"
+#endif
+
+using namespace vl;
+using namespace vl::stream;
+using namespace vl::reflection::description;
+using namespace vl::presentation;
+using namespace vl::presentation::elements;
+using namespace vl::presentation::compositions;
+using namespace vl::presentation::controls;
+using namespace vl::presentation::unittest;
+
+TEST_FILE
+{
+	const auto resourceListControl= LR"GacUISrc(
+<Resource>
+  <Instance name="MainWindowResource">
+    <Instance ref.Class="gacuisrc_unittest::MainWindow">
+      <Window ref.Name="self" Text="GuiSelectableListControl" ClientSize="x:320 y:240">
+        <TextList ref.Name="list" HorizontalAlwaysVisible="false" VerticalAlwaysVisible="false">
+          <att.BoundsComposition-set PreferredMinSize="x:400 y:300" AlignmentToParent="left:0 top:5 right:0 bottom:0"/>
+        </TextList>
+      </Window>
+    </Instance>
+  </Instance>
+</Resource>
+)GacUISrc";
+
+	TEST_CATEGORY(L"GuiSelectableListControl")
+	{
+		TEST_CASE(L"Properties")
+		{
+			GacUIUnitTest_SetGuiMainProxy([](UnitTestRemoteProtocol* protocol, IUnitTestContext*)
+			{
+				protocol->OnNextIdleFrame(L"Ready", [=]()
+				{
+					auto window = GetApplication()->GetMainWindow();
+					window->Hide();
+				});
+			});
+			GacUIUnitTest_StartFast_WithResourceAsText<darkskin::Theme>(
+				WString::Unmanaged(L"Controls/List/GuiSelectableListControl/Properties"),
+				WString::Unmanaged(L"gacuisrc_unittest::MainWindow"),
+				resourceListControl
+				);
+		});
+	});
+}
