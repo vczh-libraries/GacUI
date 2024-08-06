92b35a1e0453a976b1b78556cffafd0e9a448709
vczh <vczh@outlook.com>
2024-08-05 23:14:40 -0700

    ...

--- Test/GacUISrc/UnitTest/TestControls_List_GuiListControl.cpp
+++ Test/GacUISrc/UnitTest/TestControls_List_GuiListControl.cpp
@@ -20,10 +20,34 @@ TEST_FILE
 <Resource>
   <Instance name="MainWindowResource">
     <Instance ref.Class="gacuisrc_unittest::MainWindow">
-      <Window ref.Name="self" Text="GuiListControl" ClientSize="x:320 y:240">
-        <TextList ref.Name="list" HorizontalAlwaysVisible="false" VerticalAlwaysVisible="false">
-          <att.BoundsComposition-set PreferredMinSize="x:400 y:300" AlignmentToParent="left:0 top:5 right:0 bottom:0"/>
-        </TextList>
+      <Window ref.Name="self" Text="GuiListControl" ClientSize="x:480 y:320">
+        <Table BorderVisible="false" CellPadding="5" AlignmentToParent="left:0 top:5 right:0 bottom:0">
+          <att.Rows>
+            <_>composeType:Percentage percentage:1.0</_>
+          </att.Rows>
+          <att.Columns>
+            <_>composeType:Percentage percentage:1.0</_>
+            <_>composeType:Absolute absolute:160</_>
+          </att.Columns>
+
+          <Cell Site="row:0 column:0">
+            <GroupBox Text="Items">
+              <att.BoundsComposition-set AlignmentToParent="left:0 top:0 right:0 bottom:0"/>
+              <TextList ref.Name="list" HorizontalAlwaysVisible="false" VerticalAlwaysVisible="false">
+                <att.BoundsComposition-set AlignmentToParent="left:5 top:5 right:5 bottom:5"/>
+              </TextList>
+            </GroupBox>
+          </Cell>
+
+          <Cell Site="row:0 column:1">
+            <GroupBox Text="Logs">
+              <att.BoundsComposition-set AlignmentToParent="left:0 top:0 right:0 bottom:0"/>
+              <TextList ref.Name="logs" HorizontalAlwaysVisible="false" VerticalAlwaysVisible="false">
+                <att.BoundsComposition-set AlignmentToParent="left:5 top:5 right:5 bottom:5"/>
+              </TextList>
+            </GroupBox>
+          </Cell>
+        </Table>
       </Window>
     </Instance>
   </Instance>
