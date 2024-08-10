e6f0ff7f04dfc9609bdb93f64fa577a99784d9ba
vczh <vczh@outlook.com>
2024-08-10 06:15:35 -0700

    ...

--- Test/GacUISrc/UnitTest/TestControls_List_GuiListItemTemplate.cpp
+++ Test/GacUISrc/UnitTest/TestControls_List_GuiListItemTemplate.cpp
@@ -259,6 +259,17 @@ TEST_FILE
 				protocol->OnNextIdleFrame(L"DisplayItemBackground = true", [=]()
 				{
 					auto window = GetApplication()->GetMainWindow();
+					auto listControl = FindObjectByName<GuiTextList>(window, L"list");
+
+					{
+						auto itemStyle = listControl->GetArranger()->GetVisibleStyle(1);
+						auto location = protocol->LocationOf(itemStyle);
+						protocol->MouseMove({ location.x + 1,location.y });
+					}
+				});
+				protocol->OnNextIdleFrame(L"Move", [=]()
+				{
+					auto window = GetApplication()->GetMainWindow();
 					window->Hide();
 				});
 			});
