ca52e2588cf3d90e0535bca925641a53e33f7e71
vczh <vczh@outlook.com>
2024-08-10 06:13:22 -0700

    Bug doesn't exist, it is an error in the test case

--- Test/GacUISrc/UnitTest/TestControls_List_GuiListItemTemplate.cpp
+++ Test/GacUISrc/UnitTest/TestControls_List_GuiListItemTemplate.cpp
@@ -239,12 +239,18 @@ TEST_FILE
 					auto listControl = FindObjectByName<GuiTextList>(window, L"list");
 
 					{
-						auto itemStyle = listControl->GetArranger()->GetVisibleStyle(0);
+						auto itemStyle = listControl->GetArranger()->GetVisibleStyle(2);
+						auto location = protocol->LocationOf(itemStyle);
+						protocol->MouseMove(location);
+					}
+					protocol->LClick();
+					{
+						auto itemStyle = listControl->GetArranger()->GetVisibleStyle(1);
 						auto location = protocol->LocationOf(itemStyle);
 						protocol->MouseMove(location);
 					}
 				});
-				protocol->OnNextIdleFrame(L"Hover 3rd", [=]()
+				protocol->OnNextIdleFrame(L"Select 3rd and Hover 2nd", [=]()
 				{
 					auto window = GetApplication()->GetMainWindow();
 					auto listControl = FindObjectByName<GuiTextList>(window, L"list");
