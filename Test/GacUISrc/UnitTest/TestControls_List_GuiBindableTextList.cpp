0cf9ef6f699165cd8c4c7e8d0108d857c3e1af4b
vczh <vczh@outlook.com>
2024-08-15 03:54:00 -0700

    ...

--- Test/GacUISrc/UnitTest/TestControls_List_GuiBindableTextList.cpp
+++ Test/GacUISrc/UnitTest/TestControls_List_GuiBindableTextList.cpp
@@ -46,6 +46,52 @@ TEST_FILE
 
 	const auto resourceTextListItemTemplate = LR"GacUISrc(
 <Resource>
+  <Instance name="CheckedBulletTemplateResource">
+    <Instance ref.Class="gacuisrc_unittest::CheckedBulletTemplate">
+      <SelectableButtonTemplate ref.Name="self" PreferredMinSize="x:32 y:16" MinSizeLimitation="LimitToElementAndChildren">
+        <SolidBorder Color="#00FF00"/>
+        <Bounds Visible-bind="self.Selected" PreferredMinSize="x:12 y:12" AlignmentToParent="left:2 top:2 right:-1 bottom:2">
+          <SolidBackground Color="#00FF00"/>
+        </Bounds>
+        <Bounds Visible-bind="not self.Selected" PreferredMinSize="x:12 y:12" AlignmentToParent="left:-1 top:2 right:2 bottom:2">
+          <SolidBackground Color="#888888"/>
+        </Bounds>
+      </SelectableButtonTemplate>
+    </Instance>
+  </Instance>
+
+  <Instance name="ItemTemplateResource">
+    <Instance ref.Class="gacuisrc_unittest::ItemTemplate">
+      <TextListItemTemplate ref.Name="self" MinSizeLimitation="LimitToElementAndChildren">
+        <Table BorderVisible="true" CellPadding="2" MinSizeLimitation="LimitToElementAndChildren" AlignmentToParent="left:0 top:0 right:0 bottom:0">
+          <att.Rows>
+            <_>composeType:Percentage percentage:0.5</_>
+            <_>composeType:MinSize</_>
+            <_>composeType:Percentage percentage:0.5</_>
+          </att.Rows>
+          <att.Columns>
+            <_>composeType:MinSize</_>
+            <_>composeType:Percentage percentage:1.0</_>
+          </att.Columns>
+
+          <Cell Site="row:1 column:0">
+            <CheckBox ref.Name="bullet" Selected-bind="self.Checked" AutoFocus="false" AutoSelection="false">
+              <att.ControlTemplate>gacuisrc_unittest::CheckedBulletTemplate</att.ControlTemplate>
+              <att.BoundsComposition-set AlignmentToParent="left:0 top:0 right:0 bottom:0"/>
+              <ev.Clicked-eval><![CDATA[{
+                var textItemView = self.AssociatedListControl.ItemProvider.RequestView(ITextItemView::GetIdentifier()) as ITextItemView*;
+                textItemView.SetChecked(self.Index, not bullet.Selected);
+              }]]></ev.Clicked-eval>
+            </CheckBox>
+          </Cell>
+          <Cell Site="row:1 column:1">
+            <SolidLabel Font-bind="self.Font" Color-bind="self.TextColor" Text-bind="self.Text" Ellipse="true" VerticalAlignment="Center" HorizontalAlignment="Left"/>
+          </Cell>
+        </Table>
+      </TextListItemTemplate>
+    </Instance>
+  </Instance>
+
   <Instance name="MainWindowResource">
     <Instance ref.Class="gacuisrc_unittest::MainWindow">
       <ref.Members><![CDATA[
@@ -60,6 +106,7 @@ TEST_FILE
       ]]></ref.Members>
       <Window ref.Name="self" Text-format="GuiBindableTextList [$(list.SelectedItemIndex)] -&gt; [$(list.SelectedItemText)]" ClientSize="x:320 y:240">
         <GuiBindableTextList ref.Name="list" env.ItemType="TextItem^" HorizontalAlwaysVisible="false" VerticalAlwaysVisible="false">
+          <att.ItemTemplate>gacuisrc_unittest::ItemTemplate</att.ItemTemplate>
           <att.BoundsComposition-set PreferredMinSize="x:400 y:300" AlignmentToParent="left:0 top:5 right:0 bottom:0"/>
           <att.TextProperty>Text</att.TextProperty>
           <att.CheckedProperty>Checked</att.CheckedProperty>
@@ -395,7 +442,6 @@ TEST_FILE
 				{
 					auto window = GetApplication()->GetMainWindow();
 					auto listControl = FindObjectByName<GuiBindableTextList>(window, L"list");
-					listControl->SetView(TextListView::Check);
 					Value::From(window).Invoke(L"InitializeItems", (Value_xs(), BoxParameter(*items.Obj()), BoxValue<vint>(5)));
 				});
 				protocol->OnNextIdleFrame(L"5 Items", [=]()
@@ -448,7 +494,6 @@ TEST_FILE
 				{
 					auto window = GetApplication()->GetMainWindow();
 					auto listControl = FindObjectByName<GuiBindableTextList>(window, L"list");
-					listControl->SetView(TextListView::Radio);
 					Value::From(window).Invoke(L"InitializeItems", (Value_xs(), BoxParameter(*items.Obj()), BoxValue<vint>(20)));
 				});
 				protocol->OnNextIdleFrame(L"20 Items", [=]()
@@ -496,7 +541,6 @@ TEST_FILE
 				{
 					auto window = GetApplication()->GetMainWindow();
 					auto listControl = FindObjectByName<GuiBindableTextList>(window, L"list");
-					listControl->SetView(TextListView::Check);
 					Value::From(window).Invoke(L"InitializeItems", (Value_xs(), BoxParameter(*items.Obj()), BoxValue<vint>(5)));
 				});
 				protocol->OnNextIdleFrame(L"5 Items", [=]()
