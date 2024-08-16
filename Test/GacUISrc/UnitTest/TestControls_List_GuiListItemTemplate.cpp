8d6d1e17ff3e732134c1b5c1e8fbc67d90d6b28d
vczh <vczh@outlook.com>
2024-08-16 03:25:46 -0700

    TestControls_List_GuiListItemTemplate_Template.cpp

--- Test/GacUISrc/UnitTest/TestControls_List_GuiListItemTemplate.cpp
+++ Test/GacUISrc/UnitTest/TestControls_List_GuiListItemTemplate.cpp
@@ -131,4 +131,73 @@ TEST_FILE
 				WString::Unmanaged(L"GuiListItemTemplate/GuiTextList"));
 		});
 	}
+
+	/***********************************************************************
+	GuiBindableTextList
+	***********************************************************************/
+	{
+		const WString resourceListItemTemplate = LR"GacUISrc(
+<Resource>)GacUISrc" + fragmentListItemTemplate + LR"GacUISrc(
+  <Instance name="MainWindowResource">
+    <Instance ref.Class="gacuisrc_unittest::MainWindow">
+      <ref.Members><![CDATA[
+        var items:observe TextItem^[] = {};
+      ]]></ref.Members>
+      <ref.Ctor><![CDATA[{
+        for (item in range[1, 20])
+        {
+          items.Add(new TextItem^($"Item $(item)"));
+        }
+      }]]></ref.Ctor>
+      <Window ref.Name="self" Text="GuiListItemTemplate" ClientSize="x:320 y:240">
+        <GuiBindableTextList ref.Name="list" env.ItemType="TextItem^" HorizontalAlwaysVisible="false" VerticalAlwaysVisible="false">
+          <att.BoundsComposition-set PreferredMinSize="x:400 y:300" AlignmentToParent="left:0 top:5 right:0 bottom:0"/>
+          <att.ItemTemplate>gacuisrc_unittest::MyListItemTemplate</att.ItemTemplate>
+          <att.ItemSource-eval>self.items</ItemSource-eval>
+          <att.TextProperty>Text</att.TextProperty>
+        </GuiBindableTextList>
+      </Window>
+    </Instance>
+  </Instance>
+</Resource>
+)GacUISrc";
+
+		const WString resourceGridItemTemplate = LR"GacUISrc(
+<Resource>)GacUISrc" + fragmentGridItemTemplate + LR"GacUISrc(
+  <Instance name="MainWindowResource">
+    <Instance ref.Class="gacuisrc_unittest::MainWindow">
+      <ref.Members><![CDATA[
+        var items:observe TextItem^[] = {};
+        func InitializeItems(count:int) : void
+        {
+          for (item in range[1, count])
+          {
+            items.Add(new TextItem^($"Item $(item)"));
+          }
+        }
+      ]]></ref.Members>
+      <Window ref.Name="self" Text-format="GuiListItemTemplate $(list.SelectedItemIndex)" ClientSize="x:320 y:240">
+        <GuiBindableTextList ref.Name="list" env.ItemType="TextItem^" HorizontalAlwaysVisible="false" VerticalAlwaysVisible="false">
+          <att.BoundsComposition-set PreferredMinSize="x:400 y:300" AlignmentToParent="left:0 top:5 right:0 bottom:0"/>
+          <att.ItemTemplate>gacuisrc_unittest::MyListItemTemplate</att.ItemTemplate>
+          <att.ItemSource-eval>self.items</ItemSource-eval>
+          <att.TextProperty>Text</att.TextProperty>
+        </GuiBindableTextList>
+      </Window>
+    </Instance>
+  </Instance>
+</Resource>
+)GacUISrc";
+
+		TEST_CATEGORY(L"GuiTextList")
+		{
+			GuiListItemTemplate_TestCases(
+				resourceListItemTemplate,
+				WString::Unmanaged(L"GuiListItemTemplate/GuiBindableTextList"));
+
+			GuiListItemTemplate_WithAxis_TestCases(
+				resourceGridItemTemplate,
+				WString::Unmanaged(L"GuiListItemTemplate/GuiBindableTextList"));
+		});
+	}
 }
