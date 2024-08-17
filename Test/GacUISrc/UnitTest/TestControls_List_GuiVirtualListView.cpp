90bf15501c102619d7de66e62c826aba2f8dd583
vczh <vczh@outlook.com>
2024-08-17 15:20:34 -0700

    ...

--- Test/GacUISrc/UnitTest/TestControls_List_GuiVirtualListView.cpp
+++ Test/GacUISrc/UnitTest/TestControls_List_GuiVirtualListView.cpp
@@ -58,10 +58,17 @@ TEST_FILE
   <Script name="ListViewDataResource"><Workflow><![CDATA[
     module treeviewdata;
     using system::*;
+    using presentation::*;
 
     class ListViewData
     {
-      prop Text:string = "" {not observe}
+      prop Id:string = "" {not observe}
+      prop First:string = "" {not observe}
+      prop Second:string = "" {not observe}
+      prop Third:string = "" {not observe}
+      prop Fourth:string = "" {not observe}
+      prop LargeImage:GuiImageData^ = null {not observe}
+      prop SmallImage:GuiImageData^ = null {not observe}
     }
   ]]></Workflow></Script>
 )GacUISrc";
@@ -151,6 +158,84 @@ TEST_FILE
 				WString::Unmanaged(L"GuiListView"),
 				getItems,
 				notifyItemDataModified);
+		});
+	}
+
+	/***********************************************************************
+	GuiBindableListView
+	***********************************************************************/
+	{
+		const WString fragmentMembers = LR"GacUISrc(
+        var items:observe ListViewData^[] = {};
+        func InitializeItems(count:int) : void
+        {
+          for (item in range[1, count])
+          {
+            var listViewItem = new ListViewData^();
+            listViewItem.Id = $"Item $(item)";
+            listViewItem.First = $"1st:$(item * 1)";
+            listViewItem.Second = $"2nd:$(item * 2)";
+            listViewItem.Third = $"3rd:$(item * 3)";
+            listViewItem.Fourth = $"4th:$(item * 4)";
+            listViewItem.LargeImage = GetLargeImage((item - 1) % 5);
+            listViewItem.SmallImage = GetSmallImage((item - 1) % 5);
+            items.Add(listViewItem);
+          }
+        }
+)GacUISrc";
+
+		const WString fragmentWindow = LR"GacUISrc(
+      <Window ref.Name="self" Text-format="GuiListView [$(list.SelectedItemIndex)] -&gt; [$(list.SelectedItemText)]" ClientSize="x:640 y:480">
+        <BindableListView ref.Name="list" env.ItemType="ListViewData^" HorizontalAlwaysVisible="false" VerticalAlwaysVisible="false">
+          <att.BoundsComposition-set PreferredMinSize="x:400 y:300" AlignmentToParent="left:0 top:5 right:0 bottom:0"/>
+          <att.ItemSource-eval>self.items</ItemSource-eval>
+          <att.LargeImageProperty>LargeImage</att.LargeImageProperty>
+          <att.SmallImageProperty>SmallImage</att.SmallImageProperty>
+          <att.Columns>
+            <_ Text="Id" TextProperty="Id"/>
+            <_ Size="100" Text="First" TextProperty="First"/>
+            <_ Size="100" Text="Second" TextProperty="Second"/>
+            <_ Size="100" Text="Third" TextProperty="Third"/>
+            <_ Size="100" Text="Fourth" TextProperty="Fourth"/>
+          </att.Columns>
+          <att.DataColumns>
+            <_>0</_>
+            <_>2</_>
+            <_>3</_>
+          </att.DataColumns>
+        </BindableListView>
+      </Window>
+)GacUISrc";
+
+		const WString resourceWithImage = LR"GacUISrc(
+<Resource>)GacUISrc" + fragmentListViewData + LR"GacUISrc(
+  <Folder name="UnitTestConfig" content="Link">ListViewImagesData.xml</Folder>
+  <Folder name="ListViewImages" content="Link">ListViewImagesFolder.xml</Folder>
+  <Instance name="MainWindowResource">
+    <Instance ref.Class="gacuisrc_unittest::MainWindow">
+      <ref.Members><![CDATA[)GacUISrc" + fragmentLoadImages + fragmentMembers + LR"GacUISrc(
+      ]]></ref.Members>)GacUISrc" + fragmentWindow + LR"GacUISrc(
+    </Instance>
+  </Instance>
+</Resource>
+)GacUISrc";
+
+		const WString resourceWithoutImage = LR"GacUISrc(
+<Resource>)GacUISrc" + fragmentListViewData + LR"GacUISrc(
+  <Instance name="MainWindowResource">
+    <Instance ref.Class="gacuisrc_unittest::MainWindow">
+      <ref.Members><![CDATA[)GacUISrc" + fragmentNotLoadImages + fragmentMembers + LR"GacUISrc(
+      ]]></ref.Members>)GacUISrc" + fragmentWindow + LR"GacUISrc(
+    </Instance>
+  </Instance>
+</Resource>
+)GacUISrc";
+
+		TEST_CATEGORY(L"GuiBindableListView")
+		{
+			GuiVirtualListView_ViewAndImages_TestCases(
+				resourceWithImage,
+				WString::Unmanaged(L"GuiBindableListView"));
 		});
 	}
 }
