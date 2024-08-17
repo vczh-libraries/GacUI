3a5d7515756eb01c728ddf7422720b48219f8570
vczh <vczh@outlook.com>
2024-08-17 05:16:15 -0700

    ...

--- Test/GacUISrc/UnitTest/TestControls_List_GuiVirtualListView.cpp
+++ Test/GacUISrc/UnitTest/TestControls_List_GuiVirtualListView.cpp
@@ -34,13 +34,29 @@ TEST_FILE
           {
             var listViewItem = new ListViewItem^();
             listViewItem.Text = $"Item $(item)";
+            listViewItem.SubItems.Add($"1st:$(item * 1)");
+            listViewItem.SubItems.Add($"2nd:$(item * 2)");
+            listViewItem.SubItems.Add($"3rd:$(item * 3)");
+            listViewItem.SubItems.Add($"4th:$(item * 4)");
             list.Items.Add(listViewItem);
           }
         }
       ]]></ref.Members>
-      <Window ref.Name="self" Text-format="GuiListView [$(list.SelectedItemIndex)] -&gt; [$(list.SelectedItemText)]" ClientSize="x:480 y:320">
+      <Window ref.Name="self" Text-format="GuiListView [$(list.SelectedItemIndex)] -&gt; [$(list.SelectedItemText)]" ClientSize="x:640 y:480">
         <ListView ref.Name="list" HorizontalAlwaysVisible="false" VerticalAlwaysVisible="false">
           <att.BoundsComposition-set PreferredMinSize="x:400 y:300" AlignmentToParent="left:0 top:5 right:0 bottom:0"/>
+          <att.Columns>
+            <_ Text="Id"/>
+            <_ Size="100" Text="First"/>
+            <_ Size="100" Text="Second"/>
+            <_ Size="100" Text="Third"/>
+            <_ Size="100" Text="Fourth"/>
+          </att.Columns>
+          <att.DataColumns>
+            <_>0</_>
+            <_>2</_>
+            <_>3</_>
+          </att.DataColumns>
         </ListView>
       </Window>
     </Instance>
