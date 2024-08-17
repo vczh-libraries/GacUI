10e2ad33bd08c7c8a45e88cb49611045abde1785
vczh <vczh@outlook.com>
2024-08-17 15:06:07 -0700

    ...

--- Test/GacUISrc/UnitTest/TestControls_List_GuiVirtualListView.cpp
+++ Test/GacUISrc/UnitTest/TestControls_List_GuiVirtualListView.cpp
@@ -7,39 +7,6 @@ TEST_FILE
 	Shared
 	***********************************************************************/
 
-	const WString fragmentImages = LR"GacUISrc(
-  <Folder name="UnitTestConfig">
-    <Xml name="ImageData">
-      <ImageData>
-        <Image Path="LargeImages/Cert" Format="Png" Width="32" Height="32"/>
-        <Image Path="LargeImages/Data" Format="Png" Width="32" Height="32"/>
-        <Image Path="LargeImages/Link" Format="Png" Width="32" Height="32"/>
-        <Image Path="LargeImages/Folder" Format="Png" Width="32" Height="32"/>
-        <Image Path="LargeImages/Light" Format="Png" Width="32" Height="32"/>
-        <Image Path="SmallImages/Cert" Format="Png" Width="16" Height="16"/>
-        <Image Path="SmallImages/Data" Format="Png" Width="16" Height="16"/>
-        <Image Path="SmallImages/Link" Format="Png" Width="16" Height="16"/>
-        <Image Path="SmallImages/Folder" Format="Png" Width="16" Height="16"/>
-        <Image Path="SmallImages/Light" Format="Png" Width="16" Height="16"/>
-      </ImageData>
-    </Xml>
-  </Folder>
-  <Folder name="LargeImages">
-    <Image name="Cert" content="File">Cert_32.png</Image>
-    <Image name="Data" content="File">Data_32.png</Image>
-    <Image name="Link" content="File">Link_32.png</Image>
-    <Image name="Folder" content="File">Folder_32.png</Image>
-    <Image name="Light" content="File">Light_32.png</Image>
-  </Folder>
-  <Folder name="SmallImages">
-    <Image name="Cert" content="File">Cert_16.png</Image>
-    <Image name="Data" content="File">Data_16.png</Image>
-    <Image name="Link" content="File">Link_16.png</Image>
-    <Image name="Folder" content="File">Folder_16.png</Image>
-    <Image name="Light" content="File">Light_16.png</Image>
-  </Folder>
-)GacUISrc";
-
 	const WString fragmentLoadImages = LR"GacUISrc(
         var largeImages : GuiImageData^[] = null;
         var smallImages : GuiImageData^[] = null;
@@ -50,18 +17,18 @@ TEST_FILE
             return;
           }
           largeImages = {
-            (cast (GuiImageData^) self.ResolveResource("res", "LargeImages/Cert", true));
-            (cast (GuiImageData^) self.ResolveResource("res", "LargeImages/Data", true));
-            (cast (GuiImageData^) self.ResolveResource("res", "LargeImages/Link", true));
-            (cast (GuiImageData^) self.ResolveResource("res", "LargeImages/Folder", true));
-            (cast (GuiImageData^) self.ResolveResource("res", "LargeImages/Light", true));
+            (cast (GuiImageData^) self.ResolveResource("res", "ListViewImages/LargeImages/Cert", true));
+            (cast (GuiImageData^) self.ResolveResource("res", "ListViewImages/LargeImages/Data", true));
+            (cast (GuiImageData^) self.ResolveResource("res", "ListViewImages/LargeImages/Link", true));
+            (cast (GuiImageData^) self.ResolveResource("res", "ListViewImages/LargeImages/Folder", true));
+            (cast (GuiImageData^) self.ResolveResource("res", "ListViewImages/LargeImages/Light", true));
           };
           smallImages = {
-            (cast (GuiImageData^) self.ResolveResource("res", "SmallImages/Cert", true));
-            (cast (GuiImageData^) self.ResolveResource("res", "SmallImages/Data", true));
-            (cast (GuiImageData^) self.ResolveResource("res", "SmallImages/Link", true));
-            (cast (GuiImageData^) self.ResolveResource("res", "SmallImages/Folder", true));
-            (cast (GuiImageData^) self.ResolveResource("res", "SmallImages/Light", true));
+            (cast (GuiImageData^) self.ResolveResource("res", "ListViewImages/SmallImages/Cert", true));
+            (cast (GuiImageData^) self.ResolveResource("res", "ListViewImages/SmallImages/Data", true));
+            (cast (GuiImageData^) self.ResolveResource("res", "ListViewImages/SmallImages/Link", true));
+            (cast (GuiImageData^) self.ResolveResource("res", "ListViewImages/SmallImages/Folder", true));
+            (cast (GuiImageData^) self.ResolveResource("res", "ListViewImages/SmallImages/Light", true));
           };
         }
         func GetLargeImage(index:int) : GuiImageData^
@@ -73,6 +40,17 @@ TEST_FILE
         {
           EnsureImages();
           return smallImages[index];
+        }
+)GacUISrc";
+
+	const WString fragmentNotLoadImages = LR"GacUISrc(
+        func GetLargeImage(index:int) : GuiImageData^
+        {
+          return null;
+        }
+        func GetSmallImage(index:int) : GuiImageData^
+        {
+          return null;
         }
 )GacUISrc";
 
@@ -92,11 +70,7 @@ TEST_FILE
 	GuiListView
 	***********************************************************************/
 	{
-		const WString resourceWithImage = LR"GacUISrc(
-<Resource>)GacUISrc" + fragmentImages + LR"GacUISrc(
-  <Instance name="MainWindowResource">
-    <Instance ref.Class="gacuisrc_unittest::MainWindow">
-      <ref.Members><![CDATA[)GacUISrc" + fragmentLoadImages + LR"GacUISrc(
+		const WString fragmentMembers = LR"GacUISrc(
         func InitializeItems(count:int) : void
         {
           for (item in range[1, count])
@@ -112,7 +86,9 @@ TEST_FILE
             list.Items.Add(listViewItem);
           }
         }
-      ]]></ref.Members>
+)GacUISrc";
+
+		const WString fragmentWindow = LR"GacUISrc(
       <Window ref.Name="self" Text-format="GuiListView [$(list.SelectedItemIndex)] -&gt; [$(list.SelectedItemText)]" ClientSize="x:640 y:480">
         <ListView ref.Name="list" HorizontalAlwaysVisible="false" VerticalAlwaysVisible="false">
           <att.BoundsComposition-set PreferredMinSize="x:400 y:300" AlignmentToParent="left:0 top:5 right:0 bottom:0"/>
@@ -130,6 +106,27 @@ TEST_FILE
           </att.DataColumns>
         </ListView>
       </Window>
+)GacUISrc";
+
+		const WString resourceWithImage = LR"GacUISrc(
+<Resource>
+  <Folder name="UnitTestConfig" content="Link">ListViewImagesData.xml</Folder>
+  <Folder name="ListViewImages" content="Link">ListViewImagesFolder.xml</Folder>
+  <Instance name="MainWindowResource">
+    <Instance ref.Class="gacuisrc_unittest::MainWindow">
+      <ref.Members><![CDATA[)GacUISrc" + fragmentLoadImages + fragmentMembers + LR"GacUISrc(
+      ]]></ref.Members>)GacUISrc" + fragmentWindow + LR"GacUISrc(
+    </Instance>
+  </Instance>
+</Resource>
+)GacUISrc";
+
+		const WString resourceWithoutImage = LR"GacUISrc(
+<Resource>
+  <Instance name="MainWindowResource">
+    <Instance ref.Class="gacuisrc_unittest::MainWindow">
+      <ref.Members><![CDATA[)GacUISrc" + fragmentNotLoadImages + fragmentMembers + LR"GacUISrc(
+      ]]></ref.Members>)GacUISrc" + fragmentWindow + LR"GacUISrc(
     </Instance>
   </Instance>
 </Resource>
@@ -148,6 +145,12 @@ TEST_FILE
 			GuiVirtualListView_ViewAndImages_TestCases(
 				resourceWithImage,
 				WString::Unmanaged(L"GuiListView"));
+
+			GuiVirtualListView_TestCases(
+				resourceWithoutImage,
+				WString::Unmanaged(L"GuiListView"),
+				getItems,
+				notifyItemDataModified);
 		});
 	}
 }
