20f162d39395b285f0e3de1c6d4821e98261ff26
vczh <vczh@outlook.com>
2024-08-16 23:46:22 -0700

    Expand test cases to GuiBindableListView

--- Test/GacUISrc/UnitTest/TestControls_List_GuiListControl.cpp
+++ Test/GacUISrc/UnitTest/TestControls_List_GuiListControl.cpp
@@ -202,4 +202,68 @@ TEST_FILE
 				WString::Unmanaged(L"GuiListControl/GuiBindableTreeView"));
 		});
 	}
+
+	/***********************************************************************
+	GuiBindableListView
+	***********************************************************************/
+	{
+		const WString resourceListControl = LR"GacUISrc(
+<Resource>
+  <Instance name="MainWindowResource">
+    <Instance ref.Class="gacuisrc_unittest::MainWindow">
+      <ref.Members><![CDATA[
+        var items:observe TextItem^[] = {};
+        func InitializeItems(start:int, count:int) : void
+        {
+          for (item in range[1, count])
+          {
+            items.Add(new TextItem^($"Item $(start + item)"));
+          }
+        }
+      ]]></ref.Members>
+      <Window ref.Name="self" Text="GuiListControl" ClientSize="x:640 y:320">
+        <Table BorderVisible="false" CellPadding="5" AlignmentToParent="left:0 top:5 right:0 bottom:0">
+          <att.Rows>
+            <_>composeType:Percentage percentage:1.0</_>
+          </att.Rows>
+          <att.Columns>
+            <_>composeType:Percentage percentage:1.0</_>
+            <_>composeType:Absolute absolute:220</_>
+          </att.Columns>
+
+          <Cell Site="row:0 column:0">
+            <GroupBox Text="Items">
+              <att.BoundsComposition-set AlignmentToParent="left:0 top:0 right:0 bottom:0"/>
+              <BindableListView ref.Name="list" env.ItemType="TextItem^" View="Information" HorizontalAlwaysVisible="false" VerticalAlwaysVisible="false">
+                <att.BoundsComposition-set AlignmentToParent="left:5 top:5 right:5 bottom:5"/>
+                <att.ItemSource-eval>self.items</ItemSource-eval>
+                <att.Columns>
+                  <_ Text="Text" TextProperty="Text"/>
+                </att.Columns>
+              </BindableListView>
+            </GroupBox>
+          </Cell>
+
+          <Cell Site="row:0 column:1">
+            <GroupBox Text="Logs">
+              <att.BoundsComposition-set AlignmentToParent="left:0 top:0 right:0 bottom:0"/>
+              <TextList ref.Name="logs" HorizontalAlwaysVisible="false" VerticalAlwaysVisible="false">
+                <att.BoundsComposition-set AlignmentToParent="left:5 top:5 right:5 bottom:5"/>
+              </TextList>
+            </GroupBox>
+          </Cell>
+        </Table>
+      </Window>
+    </Instance>
+  </Instance>
+</Resource>
+)GacUISrc";
+
+		TEST_CATEGORY(L"GuiBindableListView")
+		{
+			GuiListControl_TestCases(
+				resourceListControl,
+				WString::Unmanaged(L"GuiListControl/GuiBindableListView"));
+		});
+	}
 }
--- Test/GacUISrc/UnitTest/TestControls_List_GuiSelectableListControl.cpp
+++ Test/GacUISrc/UnitTest/TestControls_List_GuiSelectableListControl.cpp
@@ -178,4 +178,61 @@ TEST_FILE
 				WString::Unmanaged(L"GuiSelectableListControl/GuiBindableTreeView"));
 		});
 	}
+
+	/***********************************************************************
+	GuiBindableListView
+	***********************************************************************/
+	{
+		const WString resourceListControl = LR"GacUISrc(
+<Resource>
+  <Instance name="MainWindowResource">
+    <Instance ref.Class="gacuisrc_unittest::MainWindow">
+      <ref.Members><![CDATA[
+        var items:observe TextItem^[] = {};
+        var counter : int = 0;
+      ]]></ref.Members>
+      <ref.Ctor><![CDATA[{
+        for (item in range[1, 20])
+        {
+          items.Add(new TextItem^($"Item $(item)"));
+        }
+      }]]></ref.Ctor>
+      <Window ref.Name="self" Text="GuiSelectableListControl" ClientSize="x:320 y:240">
+        <BindableListView ref.Name="list" env.ItemType="TextItem^" View="Information" HorizontalAlwaysVisible="false" VerticalAlwaysVisible="false">
+          <att.BoundsComposition-set PreferredMinSize="x:400 y:300" AlignmentToParent="left:0 top:5 right:0 bottom:0"/>
+          <att.ItemSource-eval>self.items</ItemSource-eval>
+          <att.TextProperty>Text</att.TextProperty>
+          <att.Columns>
+            <_ Text="Text" TextProperty="Text"/>
+          </att.Columns>
+          <ev.SelectionChanged-eval><![CDATA[{
+            self.counter = self.counter + 1;
+            var title = $"[$(self.counter)]";
+            for (item in list.SelectedItems)
+            {
+              title = title & $" $(item)";
+            }
+            self.Text = title & $" [$(list.SelectedItemIndex) -> $(list.SelectedItemText)]";
+          }]]></ev.SelectionChanged-eval>
+        </BindableListView>
+      </Window>
+    </Instance>
+  </Instance>
+</Resource>
+)GacUISrc";
+
+		TEST_CATEGORY(L"GuiBindableListView/SingleSelect")
+		{
+			GuiSelectableListControl_SingleSelect_TestCases(
+				resourceListControl,
+				WString::Unmanaged(L"GuiSelectableListControl/GuiBindableListView"));
+		});
+
+		TEST_CATEGORY(L"GuiBindableListView/MultiSelect")
+		{
+			GuiSelectableListControl_MultiSelect_TestCases(
+				resourceListControl,
+				WString::Unmanaged(L"GuiSelectableListControl/GuiBindableListView"));
+		});
+	}
 }
