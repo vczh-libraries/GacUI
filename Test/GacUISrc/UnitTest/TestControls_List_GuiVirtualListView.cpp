fcf89e23ec17b091574772515fb9b969a541b76b
vczh <vczh@outlook.com>
2024-08-17 05:32:44 -0700

    ...

--- Test/GacUISrc/UnitTest/TestControls_List_GuiVirtualListView.cpp
+++ Test/GacUISrc/UnitTest/TestControls_List_GuiVirtualListView.cpp
@@ -7,7 +7,40 @@ TEST_FILE
 	Shared
 	***********************************************************************/
 
-		const WString fragmentListViewData = LR"GacUISrc(
+	const WString fragmentImages = LR"GacUISrc(
+  <Folder name="UnitTestConfig">
+    <Xml name="ImageData">
+      <ImageData>
+        <Image Path="LargeImages/Cert" Format="Png" Width="32" Height="32"/>
+        <Image Path="LargeImages/Data" Format="Png" Width="32" Height="32"/>
+        <Image Path="LargeImages/Link" Format="Png" Width="32" Height="32"/>
+        <Image Path="LargeImages/Folder" Format="Png" Width="32" Height="32"/>
+        <Image Path="LargeImages/Light" Format="Png" Width="32" Height="32"/>
+        <Image Path="SmallImages/Cert" Format="Png" Width="16" Height="16"/>
+        <Image Path="SmallImages/Data" Format="Png" Width="16" Height="16"/>
+        <Image Path="SmallImages/Link" Format="Png" Width="16" Height="16"/>
+        <Image Path="SmallImages/Folder" Format="Png" Width="16" Height="16"/>
+        <Image Path="SmallImages/Light" Format="Png" Width="16" Height="16"/>
+      </ImageData>
+    </Xml>
+  </Folder>
+  <Folder name="LargeImages">
+    <Image name="Cert" content="File">Cert_32.png</Image>
+    <Image name="Data" content="File">Data_32.png</Image>
+    <Image name="Link" content="File">Link_32.png</Image>
+    <Image name="Folder" content="File">Folder_32.png</Image>
+    <Image name="Light" content="File">Light_32.png</Image>
+  </Folder>
+  <Folder name="SmallImages">
+    <Image name="Cert" content="File">Cert_16.png</Image>
+    <Image name="Data" content="File">Data_16.png</Image>
+    <Image name="Link" content="File">Link_16.png</Image>
+    <Image name="Folder" content="File">Folder_16.png</Image>
+    <Image name="Light" content="File">Light_16.png</Image>
+  </Folder>
+)GacUISrc";
+
+	const WString fragmentListViewData = LR"GacUISrc(
   <Script name="ListViewDataResource"><Workflow><![CDATA[
     module treeviewdata;
     using system::*;
@@ -24,10 +57,12 @@ TEST_FILE
 	***********************************************************************/
 	{
 		const WString resourceTextList = LR"GacUISrc(
-<Resource>
+<Resource>)GacUISrc" + fragmentImages + LR"GacUISrc(
   <Instance name="MainWindowResource">
     <Instance ref.Class="gacuisrc_unittest::MainWindow">
       <ref.Members><![CDATA[
+        var largeImages : GuiImageData^[] = null;
+        var smallImages : GuiImageData^[] = null;
         func InitializeItems(count:int) : void
         {
           for (item in range[1, count])
@@ -42,6 +77,22 @@ TEST_FILE
           }
         }
       ]]></ref.Members>
+      <ref.Ctor><![CDATA[{
+        largeImages = {
+          (cast (GuiImageData^) self.ResolveResource("res", "LargeImages/Cert", true));
+          (cast (GuiImageData^) self.ResolveResource("res", "LargeImages/Data", true));
+          (cast (GuiImageData^) self.ResolveResource("res", "LargeImages/Link", true));
+          (cast (GuiImageData^) self.ResolveResource("res", "LargeImages/Folder", true));
+          (cast (GuiImageData^) self.ResolveResource("res", "LargeImages/Light", true));
+        };
+        smallImages = {
+          (cast (GuiImageData^) self.ResolveResource("res", "SmallImages/Cert", true));
+          (cast (GuiImageData^) self.ResolveResource("res", "SmallImages/Data", true));
+          (cast (GuiImageData^) self.ResolveResource("res", "SmallImages/Link", true));
+          (cast (GuiImageData^) self.ResolveResource("res", "SmallImages/Folder", true));
+          (cast (GuiImageData^) self.ResolveResource("res", "SmallImages/Light", true));
+        };
+      }]]></ref.Ctor>
       <Window ref.Name="self" Text-format="GuiListView [$(list.SelectedItemIndex)] -&gt; [$(list.SelectedItemText)]" ClientSize="x:640 y:480">
         <ListView ref.Name="list" HorizontalAlwaysVisible="false" VerticalAlwaysVisible="false">
           <att.BoundsComposition-set PreferredMinSize="x:400 y:300" AlignmentToParent="left:0 top:5 right:0 bottom:0"/>
