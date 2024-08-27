dbe3fe7b77d20dbac86b1ff9ca21617d870703a0
vczh <vczh@outlook.com>
2024-08-27 04:45:05 -0700

    ...

--- Test/GacUISrc/UnitTest/TestControls_List_GuiVirtualTreeView.cpp
+++ Test/GacUISrc/UnitTest/TestControls_List_GuiVirtualTreeView.cpp
@@ -8,10 +8,55 @@ TEST_FILE
 	***********************************************************************/
 
 	const WString fragmentTreeListItemTemplate = LR"GacUISrc(
+  <Instance name="CheckedBulletTemplateResource">
+    <Instance ref.Class="gacuisrc_unittest::CheckedBulletTemplate">
+      <SelectableButtonTemplate ref.Name="self" PreferredMinSize="x:32 y:16" MinSizeLimitation="LimitToElementAndChildren">
+        <SolidBorder Color="#00FF00"/>
+        <Bounds Visible-bind="self.Selected" PreferredMinSize="x:12 y:12" AlignmentToParent="left:2 top:2 right:-1 bottom:2">
+          <SolidBackground Color="#00FF00"/>
+        </Bounds>
+        <Bounds Visible-bind="not self.Selected" PreferredMinSize="x:12 y:12" AlignmentToParent="left:-1 top:2 right:2 bottom:2">
+          <SolidBackground Color="#888888"/>
+        </Bounds>
+      </SelectableButtonTemplate>
+    </Instance>
+  </Instance>
+
   <Instance name="ItemTemplateResource">
     <Instance ref.Class="gacuisrc_unittest::ItemTemplate">
-      <TreeListItemTemplate ref.Name="self" MinSizeLimitation="LimitToElementAndChildren">
-      </TreeListItemTemplate>
+      <TreeItemTemplate ref.Name="self" MinSizeLimitation="LimitToElementAndChildren">
+        <Table BorderVisible="true" CellPadding="1" MinSizeLimitation="LimitToElementAndChildren" AlignmentToParent="left:0 top:0 right:0 bottom:0">
+          <att.Rows>
+            <_>composeType:Percentage percentage:0.5</_>
+            <_>composeType:MinSize</_>
+            <_>composeType:Percentage percentage:0.5</_>
+          </att.Rows>
+          <att.Columns>
+            <_>composeType:MinSize</_>
+            <_>composeType:MinSize</_>
+            <_>composeType:MinSize</_>
+            <_>composeType:Percentage percentage:1.0</_>
+          </att.Columns>
+
+          <Cell Site="row:1 column:0" PreferredMinSize-bind="{x:self.Level * 12 y:0}"/>
+          <Cell Site="row:1 column:1">
+            <CheckBox ref.Name="bullet" Selected-bind="self.Expanding" Visible-bind="self.Expandable" AutoFocus="false" AutoSelection="false">
+              <att.ControlTemplate>gacuisrc_unittest::CheckedBulletTemplate</att.ControlTemplate>
+              <att.BoundsComposition-set AlignmentToParent="left:0 top:0 right:0 bottom:0"/>
+              <ev.Clicked-eval><![CDATA[{
+                var textItemView = self.AssociatedListControl.ItemProvider.RequestView(ITextItemView::GetIdentifier()) as ITextItemView*;
+                textItemView.SetChecked(self.Index, not bullet.Selected);
+              }]]></ev.Clicked-eval>
+            </CheckBox>
+          </Cell>
+          <Cell Site="row:1 column:2" PreferredMinSize="x:16 y:16">
+            <ImageFrame Image-bind="self.Image.Image ?? null" FrameIndex-bind="self.Image.FrameIndex ?? 0" Enabled-bind="self.VisuallyEnabled" Stretch="true"/>
+          </Cell>
+          <Cell Site="row:1 column:3">
+            <SolidLabel Font-bind="self.Font" Color-bind="self.TextColor" Text-bind="self.Text" Ellipse="true" VerticalAlignment="Center" HorizontalAlignment="Left"/>
+          </Cell>
+        </Table>
+      </TreeItemTemplate>
     </Instance>
   </Instance>
 )GacUISrc";
@@ -112,13 +157,13 @@ TEST_FILE
 
 		TEST_CATEGORY(L"GuiTreeView/GuiTreeItemTemplate")
 		{
-			//GuiTreeItemTemplate_TestCases(
-			//	resourceTreeListItemTemplate,
-			//	WString::Unmanaged(L"GuiTreeView/GuiTreeItemTemplate"),
-			//	getRootItems,
-			//	getChildItems,
-			//	updateText,
-			//	notifyNodeDataModified);
+			GuiTreeItemTemplate_TestCases(
+				resourceTreeListItemTemplate,
+				WString::Unmanaged(L"GuiTreeView/GuiTreeItemTemplate"),
+				getRootItems,
+				getChildItems,
+				updateText,
+				notifyNodeDataModified);
 		});
 	}
 
@@ -240,13 +285,13 @@ TEST_FILE
 
 		TEST_CATEGORY(L"GuiBindableTreeView/GuiTreeItemTemplate")
 		{
-			//GuiTreeItemTemplate_TestCases(
-			//	resourceTreeListItemTemplate,
-			//	WString::Unmanaged(L"GuiBindableTreeView/GuiTreeItemTemplate"),
-			//	getRootItems,
-			//	getChildItems,
-			//	updateText,
-			//	notifyNodeDataModified);
+			GuiTreeItemTemplate_TestCases(
+				resourceTreeListItemTemplate,
+				WString::Unmanaged(L"GuiBindableTreeView/GuiTreeItemTemplate"),
+				getRootItems,
+				getChildItems,
+				updateText,
+				notifyNodeDataModified);
 		});
 	}
 }
