380711db7c1da28bb0aca3dc4cd415b02f27ac1c
vczh <vczh@outlook.com>
2024-09-04 21:35:19 -0700

    ...

--- Test/GacUISrc/UnitTest/TestControls_Toolstrip_GuiToolstripToolBar.cpp
+++ Test/GacUISrc/UnitTest/TestControls_Toolstrip_GuiToolstripToolBar.cpp
@@ -13,32 +13,32 @@ TEST_FILE
           <att.BoundsComposition-set AlignmentToParent="left:0 top:0 right:0 bottom:-1"/>
           <ToolstripGroupContainer>
             <ToolstripGroup>
-              <ToolstripDropdownButton Alt="L" Image-uri="ToolstripImages/Link">
+              <ToolstripDropdownButton ref.Name="buttonLink" Alt="L" Image-uri="ToolstripImages/Link">
                 <att.SubMenu-set>
-                  <MenuItemButton Alt="R" Text="Rtf" Image-uri="ToolstripImages/Rtf"/>
-                  <MenuItemButton Alt="H" Text="Html" Image-uri="ToolstripImages/Html"/>
-                  <MenuItemButton Alt="P" Text="Private Format" Image-uri="ToolstripImages/Private"/>
+                  <MenuItemButton ref.Name="buttonLinkRtf" Alt="R" Text="Rtf" Image-uri="ToolstripImages/Rtf"/>
+                  <MenuItemButton ref.Name="buttonLinkHtml" Alt="H" Text="Html" Image-uri="ToolstripImages/Html"/>
+                  <MenuItemButton ref.Name="buttonPrivate" Alt="P" Text="Private Format" Image-uri="ToolstripImages/Private"/>
                 </att.SubMenu-set>
               </ToolstripDropdownButton>
-              <ToolstripSplitButton Alt="A" Image-uri="ToolstripImages/AlignLeft">
+              <ToolstripSplitButton ref.Name="buttonAlign" Alt="A" Image-uri="ToolstripImages/AlignLeft">
                 <att.SubMenu-set>
-                  <MenuItemButton Alt="L" Text="Left" Image-uri="ToolstripImages/AlignLeft"/>
-                  <MenuItemButton Alt="C" Text="Center" Image-uri="ToolstripImages/AlignCenter"/>
-                  <MenuItemButton Alt="R" Text="Right Format" Image-uri="ToolstripImages/AlignRight"/>
+                  <MenuItemButton ref.Name="buttonAlignLeft" Alt="L" Text="Left" Image-uri="ToolstripImages/AlignLeft"/>
+                  <MenuItemButton ref.Name="buttonAlignCenter" Alt="C" Text="Center" Image-uri="ToolstripImages/AlignCenter"/>
+                  <MenuItemButton ref.Name="buttonAlignRight" Alt="R" Text="Right Format" Image-uri="ToolstripImages/AlignRight"/>
                 </att.SubMenu-set>
               </ToolstripSplitButton>
             </ToolstripGroup>
             <ToolstripGroup>
-              <ToolstripButton Alt="U" Image-uri="ToolstripImages/Undo"/>
-              <ToolstripButton Alt="R" Image-uri="ToolstripImages/Redo"/>
+              <ToolstripButton ref.Name="buttonUndo" Alt="U" Image-uri="ToolstripImages/Undo"/>
+              <ToolstripButton ref.Name="buttonRedo" Alt="R" Image-uri="ToolstripImages/Redo"/>
             </ToolstripGroup>
             <ToolstripGroup>
-              <ToolstripButton Alt="X" Image-uri="ToolstripImages/Cut"/>
-              <ToolstripButton Alt="C" Image-uri="ToolstripImages/Copy"/>
-              <ToolstripButton Alt="V" Image-uri="ToolstripImages/Paste"/>
+              <ToolstripButton ref.Name="buttonCut" Alt="X" Image-uri="ToolstripImages/Cut"/>
+              <ToolstripButton ref.Name="buttonCopy" Alt="C" Image-uri="ToolstripImages/Copy"/>
+              <ToolstripButton ref.Name="buttonPaste" Alt="V" Image-uri="ToolstripImages/Paste"/>
             </ToolstripGroup>
             <ToolstripGroup>
-              <ToolstripButton Alt="D" Image-uri="ToolstripImages/Delete"/>
+              <ToolstripButton ref.Name="buttonDelete" Alt="D" Image-uri="ToolstripImages/Delete"/>
             </ToolstripGroup>
           </ToolstripGroupContainer>
         </ToolstripToolBar>
