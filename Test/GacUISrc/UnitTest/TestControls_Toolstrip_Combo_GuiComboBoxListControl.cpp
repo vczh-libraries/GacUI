501f93de98c64a23eefaa7cdbe2fb978da825adc
vczh <vczh@outlook.com>
2024-09-09 20:22:59 -0700

    ...

--- Test/GacUISrc/UnitTest/TestControls_Toolstrip_Combo_GuiComboBoxListControl.cpp
+++ Test/GacUISrc/UnitTest/TestControls_Toolstrip_Combo_GuiComboBoxListControl.cpp
@@ -14,7 +14,7 @@ TEST_FILE
       }]]></ref.Ctor>
       <Window ref.Name="self" Text="GuiComboBox" ClientSize="x:320 y:240">
         <ComboBox ref.Name="combo">
-          <att.BoundsComposition-set AlignmentToParent="left:10 top:10 right:-1 bottom:-1" PreferredMinSize="x:160 y:30"/>
+          <att.BoundsComposition-set AlignmentToParent="left:10 top:10 right:-1 bottom:-1" PreferredMinSize="x:160 y:24"/>
           <att.ListControl>
             <TextList ref.Name="list" HorizontalAlwaysVisible="false" VerticalAlwaysVisible="false"/>
           </att.ListControl>
@@ -35,14 +35,29 @@ TEST_FILE
 				{
 					auto window = GetApplication()->GetMainWindow();
 					auto comboBox = FindObjectByName<GuiComboBoxListControl>(window, L"combo");
+					auto listControl = FindObjectByName<GuiListControl>(window, L"list");
+					TEST_ASSERT(comboBox->GetContainedListControl() == listControl);
+					TEST_ASSERT(comboBox->GetItemProvider() == listControl->GetItemProvider());
 					TEST_ASSERT(comboBox->GetSelectedIndex() == -1);
 					TEST_ASSERT(comboBox->GetSelectedItem().IsNull());
+					TEST_ASSERT(comboBox->GetText() == L"");
 					comboBox->SetSelectedIndex(0);
 					TEST_ASSERT(comboBox->GetSelectedIndex() == 0);
 					auto value = UnboxValue<Ptr<TextItem>>(comboBox->GetSelectedItem());
 					TEST_ASSERT(value->GetText() == L"Item 1");
+					TEST_ASSERT(comboBox->GetText() == L"Item 1");
 				});
 				protocol->OnNextIdleFrame(L"Select First", [=]()
+				{
+					auto window = GetApplication()->GetMainWindow();
+					auto comboBox = FindObjectByName<GuiComboBoxListControl>(window, L"combo");
+					comboBox->SetSelectedIndex(19);
+					TEST_ASSERT(comboBox->GetSelectedIndex() == 19);
+					auto value = UnboxValue<Ptr<TextItem>>(comboBox->GetSelectedItem());
+					TEST_ASSERT(value->GetText() == L"Item 20");
+					TEST_ASSERT(comboBox->GetText() == L"Item 20");
+				});
+				protocol->OnNextIdleFrame(L"Select Last", [=]()
 				{
 					auto window = GetApplication()->GetMainWindow();
 					window->Hide();
@@ -53,6 +68,18 @@ TEST_FILE
 				WString::Unmanaged(L"gacuisrc_unittest::MainWindow"),
 				resourceComboBox
 				);
+		});
+
+		TEST_CASE(L"Click")
+		{
+		});
+
+		TEST_CASE(L"Key")
+		{
+		});
+
+		TEST_CASE(L"Alt")
+		{
 		});
 	});
 }
