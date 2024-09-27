695db06854af198930a6c5cae945fb55de1c95da
vczh <vczh@outlook.com>
2024-09-27 03:44:08 -0700

    ...

--- Test/GacUISrc/UnitTest/TestControls_Toolstrip_Combo_GuiDatePicker.cpp
+++ Test/GacUISrc/UnitTest/TestControls_Toolstrip_Combo_GuiDatePicker.cpp
@@ -70,7 +70,89 @@ TEST_FILE
 			// C<date>: day button before the current month
 			// D<date>: day button of the current month
 			// E<date>: day button after the current month
-
+			
+			GacUIUnitTest_SetGuiMainProxy([](UnitTestRemoteProtocol* protocol, IUnitTestContext*)
+			{
+				protocol->OnNextIdleFrame(L"Ready", [=]()
+				{
+					protocol->KeyPress(VKEY::KEY_MENU);
+				});
+				protocol->OnNextIdleFrame(L"[ALT]", [=]()
+				{
+					protocol->KeyPress(VKEY::KEY_D);
+				});
+				protocol->OnNextIdleFrame(L"[D]", [=]()
+				{
+					protocol->KeyPress(VKEY::KEY_Y);
+				});
+				protocol->OnNextIdleFrame(L"[Y]", [=]()
+				{
+					protocol->KeyPress(VKEY::KEY_UP);
+				});
+				protocol->OnNextIdleFrame(L"[UP]", [=]()
+				{
+					auto window = GetApplication()->GetMainWindow();
+					auto datePicker = FindObjectByName<GuiDatePicker>(window, L"datePicker");
+					protocol->KeyPress(VKEY::KEY_RETURN);
+					TEST_ASSERT(datePicker->GetDate() == DateTime::FromDateTime(1999, 1, 1));
+				});
+				protocol->OnNextIdleFrame(L"[ENTER] -> 1999-1-1", [=]()
+				{
+					protocol->KeyPress(VKEY::KEY_MENU);
+				});
+				protocol->OnNextIdleFrame(L"[ALT]", [=]()
+				{
+					protocol->KeyPress(VKEY::KEY_D);
+				});
+				protocol->OnNextIdleFrame(L"[D]", [=]()
+				{
+					protocol->KeyPress(VKEY::KEY_M);
+				});
+				protocol->OnNextIdleFrame(L"[M]", [=]()
+				{
+					protocol->KeyPress(VKEY::KEY_END);
+				});
+				protocol->OnNextIdleFrame(L"[END]", [=]()
+				{
+					auto window = GetApplication()->GetMainWindow();
+					auto datePicker = FindObjectByName<GuiDatePicker>(window, L"datePicker");
+					protocol->KeyPress(VKEY::KEY_RETURN);
+					TEST_ASSERT(datePicker->GetDate() == DateTime::FromDateTime(1999, 12, 1));
+				});
+				protocol->OnNextIdleFrame(L"[ENTER] -> 1999-12-1", [=]()
+				{
+					protocol->KeyPress(VKEY::KEY_MENU);
+				});
+				protocol->OnNextIdleFrame(L"[ALT]", [=]()
+				{
+					protocol->KeyPress(VKEY::KEY_D);
+				});
+				protocol->OnNextIdleFrame(L"[D]", [=]()
+				{
+					protocol->KeyPress(VKEY::KEY_D);
+				});
+				protocol->OnNextIdleFrame(L"[D]", [=]()
+				{
+					protocol->KeyPress(VKEY::KEY_3);
+				});
+				protocol->OnNextIdleFrame(L"[3]", [=]()
+				{
+					auto window = GetApplication()->GetMainWindow();
+					auto datePicker = FindObjectByName<GuiDatePicker>(window, L"datePicker");
+					protocol->KeyPress(VKEY::KEY_1);
+					TEST_ASSERT(datePicker->GetDate() == DateTime::FromDateTime(1999, 12, 31));
+				});
+				protocol->OnNextIdleFrame(L"[1] -> 1999-12-31", [=]()
+				{
+					auto window = GetApplication()->GetMainWindow();
+					window->Hide();
+				});
+			});
+			GacUIUnitTest_StartFast_WithResourceAsText<darkskin::Theme>(
+				WString::Unmanaged(L"Controls/Toolstrip/Combo/GuiDatePicker/Alt"),
+				WString::Unmanaged(L"gacuisrc_unittest::MainWindow"),
+				resourceDatePicker
+				);
 		});
 
 		TEST_CASE(L"Mouse")
