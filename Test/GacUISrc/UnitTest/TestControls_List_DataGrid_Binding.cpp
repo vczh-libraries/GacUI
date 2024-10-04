e9f7eb1b0345172a32eaea9f587303c91752d828
vczh <vczh@outlook.com>
2024-10-04 06:16:07 -0700

    ...

--- Test/GacUISrc/UnitTest/TestControls_List_DataGrid_Binding.cpp
+++ Test/GacUISrc/UnitTest/TestControls_List_DataGrid_Binding.cpp
@@ -3,13 +3,47 @@ using namespace gacui_unittest_template;
 
 TEST_FILE
 {
-	const WString resourceDataGrid = LR"GacUISrc(
+	const WString resourceDataGridStringProperty = LR"GacUISrc(
 <Resource>
+  <Script name="DataGridItemResource"><Workflow><![CDATA[
+    module datagriditem;
+    using system::*;
+
+    class DataGridItem
+    {
+      prop Language:string = "" {not observe}
+      prop IDE:string = "" {not observe}
+      prop Company:string = "" {not observe}
+
+      new(){}
+      new(language:string, ide:string, company:string)
+      {
+        Language=language;
+        IDE=ide;
+        Company=company;
+      }
+    }
+  ]]></Workflow></Script>
   <Instance name="MainWindowResource">
     <Instance ref.Class="gacuisrc_unittest::MainWindow">
-      <Window ref.Name="self" Text="GuiBindableDataGrid" ClientSize="x:480 y:320">
-        <BindableDataGrid ref.Name="dataGrid" HorizontalAlwaysVisible="false" VerticalAlwaysVisible="false">
+      <ref.Members><![CDATA[
+        var items : observe DataGridItem^[] = {
+          new DataGridItem^("C++", "Visual Studio", "Microsoft");
+          new DataGridItem^("C#", "Visual Studio", "Microsoft");
+          new DataGridItem^("F#", "Visual Studio", "Microsoft");
+          new DataGridItem^("TypeScript", "Visual Studio Code", "Microsoft");
+          new DataGridItem^("Java", "Eclipse", "IBM");
+        };
+      ]]></ref.Members>
+      <Window ref.Name="self" Text="GuiBindableDataGrid" ClientSize="x:640 y:320">
+        <BindableDataGrid ref.Name="dataGrid" env.ItemType="DataGridItem^" HorizontalAlwaysVisible="false" VerticalAlwaysVisible="false">
           <att.BoundsComposition-set AlignmentToParent="left:5 top:5 right:5 bottom:5"/>
+          <att.ItemSource-eval>self.items</att.ItemSource-eval>
+          <att.Columns>
+            <_ Text="Language" TextProperty="Language"/>
+            <_ Text="IDE" TextProperty="IDE"/>
+            <_ Text="Company" TextProperty="Company"/>
+          </att.Columns>
         </BindableDataGrid>
       </Window>
     </Instance>
@@ -32,7 +66,7 @@ TEST_FILE
 			GacUIUnitTest_StartFast_WithResourceAsText<darkskin::Theme>(
 				WString::Unmanaged(L"Controls/List/GuiBindableDataGrid/Binding/DisplayStringProperties"),
 				WString::Unmanaged(L"gacuisrc_unittest::MainWindow"),
-				resourceDataGrid
+				resourceDataGridStringProperty
 				);
 		});
 	});
