cf05bd5cb380bd9f0a285c16a2a574c4be8c98a6
vczh <vczh@outlook.com>
2024-10-04 06:21:36 -0700

    ...

--- Test/GacUISrc/UnitTest/TestControls_List_DataGrid_Binding.cpp
+++ Test/GacUISrc/UnitTest/TestControls_List_DataGrid_Binding.cpp
@@ -51,6 +51,54 @@ TEST_FILE
 </Resource>
 )GacUISrc";
 
+	const WString resourceDataGridMixedProperty = LR"GacUISrc(
+<Resource>
+  <Script name="DataGridItemResource"><Workflow><![CDATA[
+    module datagriditem;
+    using system::*;
+
+    class DataGridItem
+    {
+      prop Language:string = "" {not observe}
+      prop MetaProgramming:bool = false {not observe}
+      prop IDEs:int = 0 {not observe}
+
+      new(){}
+      new(language:string, metaProgramming:bool, ides:int)
+      {
+        Language=language;
+        MetaProgramming=metaProgramming;
+        IDEs=ides;
+      }
+    }
+  ]]></Workflow></Script>
+  <Instance name="MainWindowResource">
+    <Instance ref.Class="gacuisrc_unittest::MainWindow">
+      <ref.Members><![CDATA[
+        var items : observe DataGridItem^[] = {
+          new DataGridItem^("C++", true, 4);
+          new DataGridItem^("C#", false, 3);
+          new DataGridItem^("F#", false, 2);
+          new DataGridItem^("TypeScript", true, 1);
+          new DataGridItem^("Java", false, 3);
+        };
+      ]]></ref.Members>
+      <Window ref.Name="self" Text="GuiBindableDataGrid" ClientSize="x:640 y:320">
+        <BindableDataGrid ref.Name="dataGrid" env.ItemType="DataGridItem^" HorizontalAlwaysVisible="false" VerticalAlwaysVisible="false">
+          <att.BoundsComposition-set AlignmentToParent="left:5 top:5 right:5 bottom:5"/>
+          <att.ItemSource-eval>self.items</att.ItemSource-eval>
+          <att.Columns>
+            <_ Text="Language" TextProperty="Language"/>
+            <_ Text="Meta Programming" TextProperty="MetaProgramming"/>
+            <_ Text="IDE Count" TextProperty="IDEs"/>
+          </att.Columns>
+        </BindableDataGrid>
+      </Window>
+    </Instance>
+  </Instance>
+</Resource>
+)GacUISrc";
+
 	TEST_CATEGORY(L"GuiBindableDataGrid")
 	{
 		TEST_CASE(L"DisplayStringProperties")
@@ -67,6 +115,23 @@ TEST_FILE
 				WString::Unmanaged(L"Controls/List/GuiBindableDataGrid/Binding/DisplayStringProperties"),
 				WString::Unmanaged(L"gacuisrc_unittest::MainWindow"),
 				resourceDataGridStringProperty
+				);
+		});
+		
+		TEST_CASE(L"DisplayMixedProperties")
+		{
+			GacUIUnitTest_SetGuiMainProxy([](UnitTestRemoteProtocol* protocol, IUnitTestContext*)
+			{
+				protocol->OnNextIdleFrame(L"Ready", [=]()
+				{
+					auto window = GetApplication()->GetMainWindow();
+					window->Hide();
+				});
+			});
+			GacUIUnitTest_StartFast_WithResourceAsText<darkskin::Theme>(
+				WString::Unmanaged(L"Controls/List/GuiBindableDataGrid/Binding/DisplayMixedProperties"),
+				WString::Unmanaged(L"gacuisrc_unittest::MainWindow"),
+				resourceDataGridMixedProperty
 				);
 		});
 	});
