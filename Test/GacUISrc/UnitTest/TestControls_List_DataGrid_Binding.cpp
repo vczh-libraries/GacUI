19d475b77868741eeb6ec1def212d6b6f0af2e1b
vczh <vczh@outlook.com>
2024-10-04 06:43:08 -0700

    ...

--- Test/GacUISrc/UnitTest/TestControls_List_DataGrid_Binding.cpp
+++ Test/GacUISrc/UnitTest/TestControls_List_DataGrid_Binding.cpp
@@ -105,18 +105,56 @@ TEST_FILE
     module datagriditem;
     using system::*;
 
+    enum IDEs
+    {
+      VisualStudio=0,
+      VSCode=1,
+      Eclipse=2,
+    }
+
+    enum Companys
+    {
+      Microsoft=0,
+      IBM=1,
+    }
+
     class DataGridItem
     {
       prop Language:string = "" {not observe}
-      prop IDE:string = "" {not observe}
-      prop Company:string = "" {not observe}
+      prop IDE:IDEs = VisualStudio {not observe}
+      prop Company:Companys = Microsoft {not observe}
 
       new(){}
-      new(language:string, ide:string, company:string)
+      new(language:string)
+      {
+        Language=language;
+      }
+      new(language:string, ide:IDEs, company:Companys)
       {
         Language=language;
         IDE=ide;
         Company=company;
+      }
+    }
+
+    func ToString(value:IDEs) : string
+    {
+      switch(value)
+      {
+        case VisualStudio: { return "Visual Studio"; }
+        case VSCode: { return "Visual Studio Code"; }
+        case Eclipse: { return "Eclipse"; }
+        default: { return "<ERROR>"; }
+      }
+    }
+    
+    func ToString(value:Companys) : string
+    {
+      switch(value)
+      {
+        case Microsoft: { return "Microsoft"; }
+        case IBM: { return "IBM"; }
+        default: { return "<ERROR>"; }
       }
     }
   ]]></Workflow></Script>
@@ -124,21 +162,21 @@ TEST_FILE
     <Instance ref.Class="gacuisrc_unittest::MainWindow">
       <ref.Members><![CDATA[
         var items : observe DataGridItem^[] = {
-          new DataGridItem^("C++", "Visual Studio", "Microsoft");
-          new DataGridItem^("C#", "Visual Studio", "Microsoft");
-          new DataGridItem^("F#", "Visual Studio", "Microsoft");
-          new DataGridItem^("TypeScript", "Visual Studio Code", "Microsoft");
-          new DataGridItem^("Java", "Eclipse", "IBM");
+          new DataGridItem^("C++");
+          new DataGridItem^("C#");
+          new DataGridItem^("F#");
+          new DataGridItem^("TypeScript", VSCode, Microsoft);
+          new DataGridItem^("Java", Eclipse, IBM);
         };
       ]]></ref.Members>
       <Window ref.Name="self" Text="GuiBindableDataGrid" ClientSize="x:640 y:320">
-        <BindableDataGrid ref.Name="dataGrid" env.ItemType="DataGridItem^" HorizontalAlwaysVisible="false" VerticalAlwaysVisible="false">
+        <BindableDataGrid ref.Name="dataGrid" env.ItemType="DataGridItem^" env.ItemName="itemToBind" HorizontalAlwaysVisible="false" VerticalAlwaysVisible="false">
           <att.BoundsComposition-set AlignmentToParent="left:5 top:5 right:5 bottom:5"/>
           <att.ItemSource-eval>self.items</att.ItemSource-eval>
           <att.Columns>
             <_ Text="Language" TextProperty="Language"/>
-            <_ Text="IDE" TextProperty="IDE"/>
-            <_ Text="Company" TextProperty="Company"/>
+            <_ Text="IDE" TextProperty="ToString(itemToBind.IDE)"/>
+            <_ Text="Company" TextProperty-eval="[ToString((cast DataGridItem^ $1).Company)]"/>
           </att.Columns>
         </BindableDataGrid>
       </Window>
