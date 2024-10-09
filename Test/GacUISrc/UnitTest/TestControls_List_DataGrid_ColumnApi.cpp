b5601c8efab67c5d4b67c15c624c83c23c360637
vczh <vczh@outlook.com>
2024-10-09 03:59:09 -0700

    ...

--- Test/GacUISrc/UnitTest/TestControls_List_DataGrid_ColumnApi.cpp
+++ Test/GacUISrc/UnitTest/TestControls_List_DataGrid_ColumnApi.cpp
@@ -261,6 +261,50 @@ TEST_FILE
 				);
 		});
 
+		TEST_CASE(L"SorterAndFilter")
+		{
+			GacUIUnitTest_SetGuiMainProxy([](UnitTestRemoteProtocol* protocol, IUnitTestContext*)
+			{
+				protocol->OnNextIdleFrame(L"Ready", [=]()
+				{
+					auto window = GetApplication()->GetMainWindow();
+					auto dataGrid = FindObjectByName<GuiBindableDataGrid>(window, L"dataGrid");
+					dataGrid->SetAdditionalFilter(UnboxValue<Ptr<IDataFilter>>(Value::From(window).GetProperty(L"filterByIDEs")));
+				});
+				protocol->OnNextIdleFrame(L"IDEs > 1", [=]()
+				{
+					auto window = GetApplication()->GetMainWindow();
+					auto dataGrid = FindObjectByName<GuiBindableDataGrid>(window, L"dataGrid");
+					auto dataGridView = dynamic_cast<IDataGridView*>(dataGrid->GetItemProvider()->RequestView(WString::Unmanaged(IDataGridView::Identifier)));
+					dataGrid->GetColumns()[3]->SetSorter(UnboxValue<Ptr<IDataSorter>>(Value::From(window).GetProperty(L"companySorterByName")));
+					dataGridView->SortByColumn(3, true);
+				});
+				protocol->OnNextIdleFrame(L"Sort by Company", [=]()
+				{
+					auto window = GetApplication()->GetMainWindow();
+					auto dataGrid = FindObjectByName<GuiBindableDataGrid>(window, L"dataGrid");
+					dataGrid->SetAdditionalFilter(nullptr);
+				});
+				protocol->OnNextIdleFrame(L"-IDEs", [=]()
+				{
+					auto window = GetApplication()->GetMainWindow();
+					auto dataGrid = FindObjectByName<GuiBindableDataGrid>(window, L"dataGrid");
+					auto dataGridView = dynamic_cast<IDataGridView*>(dataGrid->GetItemProvider()->RequestView(WString::Unmanaged(IDataGridView::Identifier)));
+					dataGridView->SortByColumn(-1, true);
+				});
+				protocol->OnNextIdleFrame(L"Unsort", [=]()
+				{
+					auto window = GetApplication()->GetMainWindow();
+					window->Hide();
+				});
+			});
+			GacUIUnitTest_StartFast_WithResourceAsText<darkskin::Theme>(
+				WString::Unmanaged(L"Controls/List/GuiBindableDataGrid/ColumnApi/SorterAndFilter"),
+				WString::Unmanaged(L"gacuisrc_unittest::MainWindow"),
+				resourceDataGrid
+				);
+		});
+
 		TEST_CASE(L"ResetDataSource with sorter and filter activated")
 		{
 		});
