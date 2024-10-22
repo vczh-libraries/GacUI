75757f160d4c73b930c34a86946e8cb0a5bd2d06
vczh <vczh@outlook.com>
2024-10-22 09:49:43 -0700

    ...

--- Test/GacUISrc/UnitTest/TestControls_List_DataGrid_CellVisualizer.cpp
+++ Test/GacUISrc/UnitTest/TestControls_List_DataGrid_CellVisualizer.cpp
@@ -5,6 +5,8 @@ TEST_FILE
 {
 	const WString resourceDataGrid = LR"GacUISrc(
 <Resource>
+  <Folder name="UnitTestConfig" content="Link">DataGrid/DataGridImagesData.xml</Folder>
+  <Folder name="DataGridImages" content="Link">DataGrid/DataGridImagesFolder.xml</Folder>
   <Script name="DataGridItemResource"><Workflow><![CDATA[
     module datagriditem;
     using system::*;
@@ -44,6 +46,38 @@ TEST_FILE
       }
     }
   ]]></Workflow></Script>
+  <Instance name="LanguageVisualizerResource">
+    <Instance ref.CodeBehind="false" ref.Class="gacuisrc_unittest::LanguageVisualizer">
+      <GridVisualizerTemplate ref.Name="self">
+        <Stack Padding="2" Direction="Horizontal" MinSizeLimitation="LimitToElementAndChildren" AlignmentToParent="left:2 top:0 right:2 bottom:0">
+          <StackItem>
+            <ImageFrame HorizontalAlignment="Left" VerticalAlignment="Center">
+              <att.Image-bind>self.Text == "C++" ? (cast (GuiImageData^) self.ResolveResource("res", $"DataGridImages/Male", true)).Image : null</att.Image-bind>
+            </ImageFrame>
+          </StackItem>
+          <StackItem>
+            <SolidLabel Font-bind="self.Font" Text-bind="self.Text" Color-bind="self.PrimaryTextColor" HorizontalAlignment="Left" VerticalAlignment="Center"/>
+          </StackItem>
+        </Stack>
+      </GridVisualizerTemplate>
+    </Instance>
+  </Instance>
+  <Instance name="CompanyVisualizerResource">
+    <Instance ref.CodeBehind="false" ref.Class="gacuisrc_unittest::CompanyVisualizer">
+      <GridVisualizerTemplate ref.Name="self">
+        <Stack Padding="2" Direction="Horizontal" MinSizeLimitation="LimitToElementAndChildren" AlignmentToParent="left:2 top:0 right:2 bottom:0">
+          <StackItem>
+            <ImageFrame HorizontalAlignment="Left" VerticalAlignment="Center">
+              <att.Image-bind>(cast Companies self.CellValue == Companies::Microsoft ? (cast (GuiImageData^) self.ResolveResource("res", $"DataGridImages/Female", true)).Image : null) ?? null</att.Image-bind>
+            </ImageFrame>
+          </StackItem>
+          <StackItem>
+            <SolidLabel Font-bind="self.Font" Text-bind="ToString((cast DataGridItem^ self.RowValue).Company) ?? ''" Color-bind="self.PrimaryTextColor" HorizontalAlignment="Left" VerticalAlignment="Center"/>
+          </StackItem>
+        </Stack>
+      </GridVisualizerTemplate>
+    </Instance>
+  </Instance>
   <Instance name="MainWindowResource">
     <Instance ref.Class="gacuisrc_unittest::MainWindow" xmlns:x="presentation::controls::GuiSelectableButton::*">
       <ref.Members><![CDATA[
@@ -64,13 +98,15 @@ TEST_FILE
           <att.ItemSource-eval>self.items</att.ItemSource-eval>
           <att.Columns>
             <_ Text="Language" Size="150" TextProperty="Language">
+              <att.VisualizerFactory>gacuisrc_unittest::LanguageVisualizer;FocusRectangleVisualizerTemplate;CellBorderVisualizerTemplate</att.VisualizerFactory>
               <att.Sorter>[Sys::Compare($1.Language, $2.Language)]</att.Sorter>
             </_>
             <_ Text="Meta Programming" Size="150" TextProperty="MetaProgramming">
             </_>
             <_ Text="IDE Count" Size="150" TextProperty="IDEs">
             </_>
-            <_ ref.Name="columnCompanies" Text="Company" Size="150" TextProperty-eval="[ToString((cast DataGridItem^ $1).Company)]">
+            <_ ref.Name="columnCompanies" Text="Company" Size="150" ValueProperty="Company">
+              <att.VisualizerFactory>gacuisrc_unittest::CompanyVisualizer;FocusRectangleVisualizerTemplate;CellBorderVisualizerTemplate</att.VisualizerFactory>
               <att.Sorter>[Sys::Compare(ToString($1.Company), ToString($2.Company))]</att.Sorter>
             </_>
           </att.Columns>
