4f845d5e5e4504c9dd0b4df60a6c1268063fad12
vczh <vczh@outlook.com>
2025-03-28 04:55:44 -0700

    Update GuiRemoteRendererSingle.h

--- Source/PlatformProviders/RemoteRenderer/GuiRemoteRendererSingle.h
+++ Source/PlatformProviders/RemoteRenderer/GuiRemoteRendererSingle.h
@@ -34,79 +34,17 @@ namespace vl::presentation::remote_renderer
 		void			Submit(bool& disconnected) override;
 		void			ProcessRemoteEvents() override;
 
-		/***********************************************************************
-		Controller
-		***********************************************************************/
 
-		void			RequestControllerGetFontConfig(vint id) override;
-		void			RequestControllerGetScreenConfig(vint id) override;
-		void			RequestControllerConnectionEstablished() override;
-		void			RequestControllerConnectionStopped() override;
-
-		/***********************************************************************
-		MainWindow
-		***********************************************************************/
-	
-		void			RequestWindowGetBounds(vint id) override;
-		void			RequestWindowNotifySetTitle(const WString& arguments) override;
-		void			RequestWindowNotifySetEnabled(const bool& arguments) override;
-		void			RequestWindowNotifySetTopMost(const bool& arguments) override;
-		void			RequestWindowNotifySetShowInTaskBar(const bool& arguments) override;
-		void			RequestWindowNotifySetBounds(const NativeRect& arguments) override;
-		void			RequestWindowNotifySetClientSize(const NativeSize& arguments) override;
-		void			RequestWindowNotifySetCustomFrameMode(const bool& arguments) override;
-		void			RequestWindowNotifySetMaximizedBox(const bool& arguments) override;
-		void			RequestWindowNotifySetMinimizedBox(const bool& arguments) override;
-		void			RequestWindowNotifySetBorder(const bool& arguments) override;
-		void			RequestWindowNotifySetSizeBox(const bool& arguments) override;
-		void			RequestWindowNotifySetIconVisible(const bool& arguments) override;
-		void			RequestWindowNotifySetTitleBar(const bool& arguments) override;
-		void			RequestWindowNotifyActivate() override;
-		void			RequestWindowNotifyShow(const remoteprotocol::WindowShowing& arguments) override;
-
-		/***********************************************************************
-		IO
-		***********************************************************************/
-
-		void			RequestIOUpdateGlobalShortcutKey(const Ptr<collections::List<remoteprotocol::GlobalShortcutKey>>& arguments) override;
-		void			RequestIORequireCapture() override;
-		void			RequestIOReleaseCapture() override;
-		void			RequestIOIsKeyPressing(vint id, const VKEY& arguments) override;
-		void			RequestIOIsKeyToggled(vint id, const VKEY& arguments) override;
-
-		/***********************************************************************
-		Rendering
-		***********************************************************************/
-
-		void			RequestRendererCreated(const Ptr<collections::List<remoteprotocol::RendererCreation>>& arguments) override;
-		void			RequestRendererDestroyed(const Ptr<collections::List<vint>>& arguments) override;
-		void			RequestRendererBeginRendering(const remoteprotocol::ElementBeginRendering& arguments) override;
-		void			RequestRendererEndRendering(vint id) override;
-		void			RequestRendererBeginBoundary(const remoteprotocol::ElementBoundary& arguments) override;
-		void			RequestRendererEndBoundary() override;
-		void			RequestRendererRenderElement(const remoteprotocol::ElementRendering& arguments) override;
-
-		/***********************************************************************
-		Rendering - Elements
-		***********************************************************************/
-
-		void			RequestRendererUpdateElement_SolidBorder(const remoteprotocol::ElementDesc_SolidBorder& arguments) override;
-		void			RequestRendererUpdateElement_SinkBorder(const remoteprotocol::ElementDesc_SinkBorder& arguments) override;
-		void			RequestRendererUpdateElement_SinkSplitter(const remoteprotocol::ElementDesc_SinkSplitter& arguments) override;
-		void			RequestRendererUpdateElement_SolidBackground(const remoteprotocol::ElementDesc_SolidBackground& arguments) override;
-		void			RequestRendererUpdateElement_GradientBackground(const remoteprotocol::ElementDesc_GradientBackground& arguments) override;
-		void			RequestRendererUpdateElement_InnerShadow(const remoteprotocol::ElementDesc_InnerShadow& arguments) override;
-		void			RequestRendererUpdateElement_Polygon(const remoteprotocol::ElementDesc_Polygon& arguments) override;
-		void			RequestRendererUpdateElement_SolidLabel(const remoteprotocol::ElementDesc_SolidLabel& arguments) override;
-		void			RequestImageCreated(vint id, const remoteprotocol::ImageCreation& arguments) override;
-		void			RequestImageDestroyed(const vint& arguments) override;
-		void			RequestRendererUpdateElement_ImageFrame(const remoteprotocol::ElementDesc_ImageFrame& arguments) override;
-
-		/***********************************************************************
-		Rendering - SyncDom
-		***********************************************************************/
-
-		void			RequestRendererRenderDom(const Ptr<remoteprotocol::RenderingDom>& arguments) override;
-		void			RequestRendererRenderDomDiff(const remoteprotocol::RenderingDom_DiffsInOrder& arguments) override;
+#define MESSAGE_NOREQ_NORES(NAME, REQUEST, RESPONSE)					void Request ## NAME() override;
+#define MESSAGE_NOREQ_RES(NAME, REQUEST, RESPONSE)						void Request ## NAME(vint id) override;
+#define MESSAGE_REQ_NORES(NAME, REQUEST, RESPONSE)						void Request ## NAME(const REQUEST& arguments) override;
+#define MESSAGE_REQ_RES(NAME, REQUEST, RESPONSE)						void Request ## NAME(vint id, const REQUEST& arguments) override;
+#define MESSAGE_HANDLER(NAME, REQUEST, RESPONSE, REQTAG, RESTAG, ...)	MESSAGE_ ## REQTAG ## _ ## RESTAG(NAME, REQUEST, RESPONSE)
+		GACUI_REMOTEPROTOCOL_MESSAGES(MESSAGE_HANDLER)
+#undef MESSAGE_HANDLER
+#undef MESSAGE_REQ_RES
+#undef MESSAGE_REQ_NORES
+#undef MESSAGE_NOREQ_RES
+#undef MESSAGE_NOREQ_NORES
 	};
 }
