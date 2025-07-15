184041021669766421b9052fdf39a9ec23445af6
vczh <vczh@outlook.com>
2025-07-15 11:51:18 -0700

    ...

---
+++ Test/GacUISrc/UnitTest/TestApplication_Theme.cpp
@@ -0,0 +1,59 @@
+#include "TestControls.h"
+
+using namespace gacui_unittest_template;
+
+TEST_FILE
+{
+	const auto resourceControlThemeName = LR"GacUISrc(
+<Resource>
+  <Instance name="MainWindowResource">
+    <Instance ref.Class="gacuisrc_unittest::MainWindow">
+      <Window ref.Name="self" Text="ControlThemeName" ClientSize="x:480 y:320">
+        <Table AlignmentToParent="left:0 top:0 right:0 bottom:0" MinSizeLimitation="LimitToElementAndChildren" CellPadding="5" BorderVisible="true">
+          <att.Rows>
+            <_>composeType:MinSize</_>
+            <_>composeType:MinSize</_>
+            <_>composeType:Percentage percentage:1.0</_>
+          </att.Rows>
+          <att.Columns>
+            <_>composeType:Percentage percentage:1.0</_>
+          </att.Columns>
+          
+          <Cell Site="row:0 column:0">
+            <CheckBox ref.Name="checkbox1" Text="Selectable Button" Selected="true">
+              <att.BoundsComposition-set AlignmentToParent="left:0 top:0 right:0 bottom:0"/>
+            </CheckBox>
+          </Cell>
+          
+          <Cell Site="row:1 column:0">
+            <CheckBox ref.Name="checkbox2" Text="Selectable Button" Selected="false">
+              <att.BoundsComposition-set AlignmentToParent="left:0 top:0 right:0 bottom:0"/>
+            </CheckBox>
+          </Cell>
+        </Table>
+      </Window>
+    </Instance>
+  </Instance>
+</Resource>
+)GacUISrc";
+
+	TEST_CATEGORY(L"Theme")
+	{
+		TEST_CASE(L"ControlThemeName")
+		{
+			GacUIUnitTest_SetGuiMainProxy([](UnitTestRemoteProtocol* protocol, IUnitTestContext*)
+			{
+				protocol->OnNextIdleFrame(L"Ready", [=]()
+				{
+					auto window = GetApplication()->GetMainWindow();
+					window->Hide();
+				});
+			});
+			GacUIUnitTest_StartFast_WithResourceAsText<darkskin::Theme>(
+				WString::Unmanaged(L"Application/ControlThemeName"),
+				WString::Unmanaged(L"gacuisrc_unittest::MainWindow"),
+				resourceControlThemeName
+			);
+		});
+	});
+}
