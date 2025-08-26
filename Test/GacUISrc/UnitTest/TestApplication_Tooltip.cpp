c7b98881f50eba9f54fef06f6ace9a2ffcfec483
vczh <vczh@outlook.com>
2025-08-26 13:50:32 -0700

    Add tooltip UI and unit test snapshots
    Introduced a table layout with two buttons, each displaying a tooltip, in the tooltip test application. Added corresponding unit test snapshots and command files for tooltip scenarios.

--- Test/GacUISrc/UnitTest/TestApplication_Tooltip.cpp
+++ Test/GacUISrc/UnitTest/TestApplication_Tooltip.cpp
@@ -9,6 +9,33 @@ TEST_FILE
   <Instance name="MainWindowResource">
     <Instance ref.Class="gacuisrc_unittest::MainWindow">
       <Window ref.Name="self" Text="Tooltip" ClientSize="x:480 y:320">
+        <Table CellPadding="5" AlignmentToParent="left:0 top:0 right:0 bottom:0">
+          <att.Rows>
+            <_>composeType:MinSize</_>
+            <_>composeType:MinSize</_>
+            <_>composeType:Percentage percentage:1.0</_>
+          </att.Rows>
+          <att.Columns>
+            <_>composeType:MinSize</_>
+            <_>composeType:Percentage percentage:1.0</_>
+          </att.Columns>
+          <Cell Site="row:0 column:0">
+            <Button ref.Name="buttonTooltipA" Text="TooltipA">
+              <att.BoundsComposition-set AlignmentToParent="left:0 top:0 right:0 bottom:0"/>
+              <att.TooltipControl>
+                <Label Text="This is Tooltip A"/>
+              </att.TooltipControl>
+            </Button>
+          </Cell>
+          <Cell Site="row:1 column:0">
+            <Button ref.Name="buttonTooltipB" Text="TooltipB">
+              <att.BoundsComposition-set AlignmentToParent="left:0 top:0 right:0 bottom:0"/>
+              <att.TooltipControl>
+                <Label Text="This is Tooltip B"/>
+              </att.TooltipControl>
+            </Button>
+          </Cell>
+        </Table>
       </Window>
     </Instance>
   </Instance>
