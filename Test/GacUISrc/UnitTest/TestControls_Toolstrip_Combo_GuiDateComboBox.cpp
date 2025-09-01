f2613128eded186371821e7ef29bd1da89d54c34
vczh <vczh@outlook.com>
2025-08-31 19:56:56 -0700

    Add GuiDateTimePicker ALT unit test

--- Test/GacUISrc/UnitTest/TestControls_Toolstrip_Combo_GuiDateComboBox.cpp
+++ Test/GacUISrc/UnitTest/TestControls_Toolstrip_Combo_GuiDateComboBox.cpp
@@ -9,7 +9,7 @@ TEST_FILE
     <Instance ref.Class="gacuisrc_unittest::MainWindow">
       <Window ref.Name="self" Text="GuiDatePicker" ClientSize="x:320 y:240">
         <att.ContainerComposition-set InternalMargin="left:10 top:10 right:10 bottom:10"/>
-        <DateComboBox ref.Name="dateCombo" SelectedDate="2000-01-01 00:00:00.000">
+        <DateComboBox ref.Name="dateCombo" Alt="X" SelectedDate="2000-01-01 00:00:00.000">
           <att.BoundsComposition-set PreferredMinSize="x:160 y:20"/>
           <att.DatePicker-set DateLocale="en-US"/>
         </DateComboBox>
@@ -89,6 +89,88 @@ TEST_FILE
 			});
 			GacUIUnitTest_StartFast_WithResourceAsText<darkskin::Theme>(
 				WString::Unmanaged(L"Controls/Toolstrip/Combo/GuiDateComboBox/Properties"),
+				WString::Unmanaged(L"gacuisrc_unittest::MainWindow"),
+				resourceDateComboBox
+				);
+		});
+
+		TEST_CASE(L"Alt")
+		{
+			// Y: year
+			// M: month
+			// C<date>: day button before the current month
+			// D<date>: day button of the current month
+			// E<date>: day button after the current month
+
+			GacUIUnitTest_SetGuiMainProxy([](UnitTestRemoteProtocol* protocol, IUnitTestContext*)
+			{
+				protocol->OnNextIdleFrame(L"Ready", [=]()
+				{
+					protocol->KeyPress(VKEY::KEY_MENU);
+				});
+				protocol->OnNextIdleFrame(L"[ALT]", [=]()
+				{
+					protocol->KeyPress(VKEY::KEY_X);
+				});
+				protocol->OnNextIdleFrame(L"[X] -> Dropdown", [=]()
+				{
+					protocol->KeyPress(VKEY::KEY_MENU);
+				});
+				protocol->OnNextIdleFrame(L"[ALT]", [=]()
+				{
+					protocol->KeyPress(VKEY::KEY_Y);
+				});
+				protocol->OnNextIdleFrame(L"[Y]", [=]()
+				{
+					protocol->KeyPress(VKEY::KEY_UP);
+				});
+				protocol->OnNextIdleFrame(L"[UP]", [=]()
+				{
+					protocol->KeyPress(VKEY::KEY_RETURN);
+				});
+				protocol->OnNextIdleFrame(L"[ENTER] -> 1999-1-1", [=]()
+				{
+					protocol->KeyPress(VKEY::KEY_MENU);
+				});
+				protocol->OnNextIdleFrame(L"[ALT]", [=]()
+				{
+					protocol->KeyPress(VKEY::KEY_M);
+				});
+				protocol->OnNextIdleFrame(L"[M]", [=]()
+				{
+					protocol->KeyPress(VKEY::KEY_END);
+				});
+				protocol->OnNextIdleFrame(L"[END]", [=]()
+				{
+					protocol->KeyPress(VKEY::KEY_RETURN);
+				});
+				protocol->OnNextIdleFrame(L"[ENTER] -> 1999-12-1", [=]()
+				{
+					protocol->KeyPress(VKEY::KEY_MENU);
+				});
+				protocol->OnNextIdleFrame(L"[ALT]", [=]()
+				{
+					protocol->KeyPress(VKEY::KEY_D);
+				});
+				protocol->OnNextIdleFrame(L"[D]", [=]()
+				{
+					protocol->KeyPress(VKEY::KEY_3);
+				});
+				protocol->OnNextIdleFrame(L"[3]", [=]()
+				{
+					auto window = GetApplication()->GetMainWindow();
+					auto dateCombo = FindObjectByName<GuiDateComboBox>(window, L"dateCombo");
+					protocol->KeyPress(VKEY::KEY_1);
+					TEST_ASSERT(dateCombo->GetSelectedDate() == DateTime::FromDateTime(1999, 12, 31));
+				});
+				protocol->OnNextIdleFrame(L"[1] -> 1999-12-31", [=]()
+				{
+					auto window = GetApplication()->GetMainWindow();
+					window->Hide();
+				});
+			});
+			GacUIUnitTest_StartFast_WithResourceAsText<darkskin::Theme>(
+				WString::Unmanaged(L"Controls/Toolstrip/Combo/GuiDateComboBox/Alt"),
 				WString::Unmanaged(L"gacuisrc_unittest::MainWindow"),
 				resourceDateComboBox
 				);
