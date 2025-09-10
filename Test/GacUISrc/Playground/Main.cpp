4aea7ff246ff0ec86664d18eae9e6e0ecc3f925d
vczh <vczh@outlook.com>
2025-09-09 23:19:11 -0700

    ...

--- Test/GacUISrc/Playground/Main.cpp
+++ Test/GacUISrc/Playground/Main.cpp
@@ -12,6 +12,24 @@ using namespace vl::stream;
 using namespace vl::filesystem;
 using namespace vl::reflection::description;
 
+class Playground : public Object, public Description<Playground>
+{
+public:
+	WString LoadBigJson()
+	{
+#ifdef VCZH_64
+		FilePath path = L"../../Playground/Resources/BigJson.json";
+#else
+		FilePath path = L"../Playground/Resources/BigJson.json";
+#endif
+		return File(path).ReadAllTextByBom();
+	}
+};
+
+namespace vl::presentation::description
+{
+}
+
 int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int CmdShow)
 {
 	int result = SetupWindowsDirect2DRenderer();
@@ -19,54 +37,6 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
 	_CrtDumpMemoryLeaks();
 #endif
 	return result;
-}
-
-void AttachDumpCompositionMenu(GuiWindow* window)
-{
-	auto menu = new GuiToolstripMenu(theme::ThemeName::Menu, window);
-	window->AddControlHostComponent(menu);
-	{
-		auto menuItem = new GuiToolstripButton(theme::ThemeName::MenuItemButton);
-		menuItem->SetText(L"Dump Composition to " + FilePath(L"TestXml.xml").GetFullPath());
-		menu->GetToolstripItems().Add(menuItem);
-
-		window->GetBoundsComposition()->GetEventReceiver()->rightButtonUp.AttachLambda([=](auto, auto arguments)
-		{
-			menu->ShowPopup(window, Point(arguments.x, arguments.y));
-		});
-
-		menuItem->Clicked.AttachLambda([=](auto, auto)
-		{
-			FileStream fileStream(L"TestXml.xml", FileStream::WriteOnly);
-			BomEncoder encoder(BomEncoder::Utf8);
-			EncoderStream encoderStream(fileStream, encoder);
-			StreamWriter writer(encoderStream);
-
-			GuiGraphicsComposition* composition = window->GetBoundsComposition();
-			while (composition->GetParent())
-			{
-				composition = composition->GetParent();
-			}
-
-			DumpComposition(composition, writer);
-			GetCurrentController()->DialogService()->ShowMessageBox(window->GetNativeWindow(), L"Finished!", L"Dump Composition");
-		});
-	}
-	{
-		auto menuItem = new GuiToolstripButton(theme::ThemeName::MenuItemButton);
-		menuItem->SetText(L"Open New Window");
-		menu->GetToolstripItems().Add(menuItem);
-
-		menuItem->Clicked.AttachLambda([=](auto, auto)
-		{
-			auto newWindow = new GuiWindow(theme::ThemeName::Window);
-			newWindow->SetText(L"New Window");
-			newWindow->SetClientSize(Size(640, 480));
-			newWindow->ForceCalculateSizeImmediately();
-			newWindow->MoveToScreenCenter();
-			newWindow->ShowModalAndDelete(window, []() {});
-		});
-	}
 }
 
 void OpenMainWindow()
@@ -81,7 +51,6 @@ void OpenMainWindow()
 	}
 	{
 		auto window = UnboxValue<GuiWindow*>(Value::Create(L"demo::TestWindow"));
-		//AttachDumpCompositionMenu(window);
 		window->ForceCalculateSizeImmediately();
 		window->MoveToScreenCenter();
 		GetApplication()->Run(window);
@@ -133,7 +102,7 @@ void GuiMain()
 	LoadDarkSkinTypes();
 
 	List<WString> names;
-	names.Add(L"ResourceDocument");
+	names.Add(L"ResourceBigJson");
 
 	Group<WString, WString> deps;
 
