ee7d4114cfc01d5b26a8f3f9c08511a13fc2ea1b
vczh <vczh@outlook.com>
2026-01-11 12:36:57 -0800

    Update TestControls_Editor_GuiSinglelineTextBox_Key.cpp

--- Test/GacUISrc/UnitTest/TestControls_Editor_GuiSinglelineTextBox_Key.cpp
+++ Test/GacUISrc/UnitTest/TestControls_Editor_GuiSinglelineTextBox_Key.cpp
@@ -643,6 +643,340 @@ void RunSinglelineTextBoxTestCases(const wchar_t* resource, const WString& contr
 		});
 	});
 
+	TEST_CATEGORY(L"UndoRedo")
+	{
+		TEST_CASE(L"CtrlZ_UndoesTyping_AndCtrlY_Redoes")
+		{
+			TooltipTimer timer;
+			GacUIUnitTest_SetGuiMainProxy([=](UnitTestRemoteProtocol* protocol, IUnitTestContext*)
+			{
+				protocol->OnNextIdleFrame(L"Init", [=]()
+				{
+					auto window = GetApplication()->GetMainWindow();
+					auto textBox = FindObjectByName<TTextBox>(window, L"textBox");
+					textBox->SetFocused();
+
+					protocol->TypeString(L"0123456789");
+					textBox->ClearUndoRedo();
+					textBox->NotifyModificationSaved();
+
+					TEST_ASSERT(!textBox->CanUndo());
+					TEST_ASSERT(!textBox->CanRedo());
+					TEST_ASSERT(!textBox->GetModified());
+				});
+
+				protocol->OnNextIdleFrame(L"Typing", [=]()
+				{
+					auto window = GetApplication()->GetMainWindow();
+					auto textBox = FindObjectByName<TTextBox>(window, L"textBox");
+
+					textBox->SetCaret(TextPos(0, 10), TextPos(0, 10));
+					protocol->TypeString(L"X");
+					TEST_ASSERT(textBox->GetText() == L"0123456789X");
+					TEST_ASSERT(textBox->CanUndo());
+					TEST_ASSERT(!textBox->CanRedo());
+					TEST_ASSERT(textBox->GetModified());
+
+					protocol->KeyPress(VKEY::KEY_Z, true, false, false);
+				});
+
+				protocol->OnNextIdleFrame(L"Undid", [=]()
+				{
+					auto window = GetApplication()->GetMainWindow();
+					auto textBox = FindObjectByName<TTextBox>(window, L"textBox");
+
+					TEST_ASSERT(textBox->GetText() == L"0123456789");
+					TEST_ASSERT(!textBox->CanUndo());
+					TEST_ASSERT(textBox->CanRedo());
+					TEST_ASSERT(!textBox->GetModified());
+				});
+
+				protocol->OnNextIdleFrame(L"RedoShortcut", [=]()
+				{
+					protocol->KeyPress(VKEY::KEY_Y, true, false, false);
+				});
+
+				protocol->OnNextIdleFrame(L"Redid", [=]()
+				{
+					auto window = GetApplication()->GetMainWindow();
+					auto textBox = FindObjectByName<TTextBox>(window, L"textBox");
+
+					TEST_ASSERT(textBox->GetText() == L"0123456789X");
+					TEST_ASSERT(textBox->CanUndo());
+					TEST_ASSERT(!textBox->CanRedo());
+					TEST_ASSERT(textBox->GetModified());
+
+					window->SetText(textBox->GetText());
+				});
+
+				protocol->OnNextIdleFrame(L"UndoRedo", [=]()
+				{
+					auto window = GetApplication()->GetMainWindow();
+					window->Hide();
+				});
+			});
+
+			GacUIUnitTest_StartFast_WithResourceAsText<darkskin::Theme>(
+				WString::Unmanaged(L"Controls/Editor/")
+				+ controlName
+				+ WString::Unmanaged(L"/Key/UndoRedo_CtrlZ_UndoesTyping_AndCtrlY_Redoes"),
+				WString::Unmanaged(L"gacuisrc_unittest::MainWindow"),
+				resource
+			);
+		});
+
+		TEST_CASE(L"CtrlZ_UndoesDeletion")
+		{
+			TooltipTimer timer;
+			GacUIUnitTest_SetGuiMainProxy([=](UnitTestRemoteProtocol* protocol, IUnitTestContext*)
+			{
+				protocol->OnNextIdleFrame(L"Init", [=]()
+				{
+					auto window = GetApplication()->GetMainWindow();
+					auto textBox = FindObjectByName<TTextBox>(window, L"textBox");
+					textBox->SetFocused();
+
+					protocol->TypeString(L"0123456789");
+					textBox->ClearUndoRedo();
+				});
+
+				protocol->OnNextIdleFrame(L"Deletion", [=]()
+				{
+					auto window = GetApplication()->GetMainWindow();
+					auto textBox = FindObjectByName<TTextBox>(window, L"textBox");
+
+					textBox->SetCaret(TextPos(0, 5), TextPos(0, 5));
+					protocol->KeyPress(VKEY::KEY_BACK);
+					TEST_ASSERT(textBox->GetText() == L"012356789");
+
+					protocol->KeyPress(VKEY::KEY_Z, true, false, false);
+				});
+
+				protocol->OnNextIdleFrame(L"Undid", [=]()
+				{
+					auto window = GetApplication()->GetMainWindow();
+					auto textBox = FindObjectByName<TTextBox>(window, L"textBox");
+					TEST_ASSERT(textBox->GetText() == L"0123456789");
+				});
+
+				protocol->OnNextIdleFrame(L"RedoShortcut", [=]()
+				{
+					protocol->KeyPress(VKEY::KEY_Y, true, false, false);
+				});
+
+				protocol->OnNextIdleFrame(L"Redid", [=]()
+				{
+					auto window = GetApplication()->GetMainWindow();
+					auto textBox = FindObjectByName<TTextBox>(window, L"textBox");
+					TEST_ASSERT(textBox->GetText() == L"012356789");
+				});
+
+				protocol->OnNextIdleFrame(L"UndoRedo", [=]()
+				{
+					auto window = GetApplication()->GetMainWindow();
+					window->Hide();
+				});
+			});
+
+			GacUIUnitTest_StartFast_WithResourceAsText<darkskin::Theme>(
+				WString::Unmanaged(L"Controls/Editor/")
+				+ controlName
+				+ WString::Unmanaged(L"/Key/UndoRedo_CtrlZ_UndoesDeletion"),
+				WString::Unmanaged(L"gacuisrc_unittest::MainWindow"),
+				resource
+			);
+		});
+
+		TEST_CASE(L"CtrlZ_UndoesPaste_FlattenedText")
+		{
+			TooltipTimer timer;
+			GacUIUnitTest_SetGuiMainProxy([=](UnitTestRemoteProtocol* protocol, IUnitTestContext*)
+			{
+				protocol->OnNextIdleFrame(L"Init", [=]()
+				{
+					auto window = GetApplication()->GetMainWindow();
+					auto textBox = FindObjectByName<TTextBox>(window, L"textBox");
+					textBox->SetFocused();
+
+					protocol->TypeString(L"0123456789");
+					textBox->ClearUndoRedo();
+				});
+
+				protocol->OnNextIdleFrame(L"Paste", [=]()
+				{
+					auto window = GetApplication()->GetMainWindow();
+					auto textBox = FindObjectByName<TTextBox>(window, L"textBox");
+					auto clipboard = GetCurrentController()->ClipboardService();
+
+					{
+						auto writer = clipboard->WriteClipboard();
+						writer->SetText(L"12\r\n34\n\r\n56");
+						TEST_ASSERT(writer->Submit());
+					}
+
+					textBox->SetCaret(TextPos(0, 0), TextPos(0, 0));
+					protocol->KeyPress(VKEY::KEY_V, true, false, false);
+					TEST_ASSERT(textBox->GetText() == L"1234560123456789");
+					TEST_ASSERT(textBox->GetText().IndexOf(L'\r') == -1);
+					TEST_ASSERT(textBox->GetText().IndexOf(L'\n') == -1);
+
+					protocol->KeyPress(VKEY::KEY_Z, true, false, false);
+				});
+
+				protocol->OnNextIdleFrame(L"Undid", [=]()
+				{
+					auto window = GetApplication()->GetMainWindow();
+					auto textBox = FindObjectByName<TTextBox>(window, L"textBox");
+					TEST_ASSERT(textBox->GetText() == L"0123456789");
+				});
+
+				protocol->OnNextIdleFrame(L"RedoShortcut", [=]()
+				{
+					protocol->KeyPress(VKEY::KEY_Y, true, false, false);
+				});
+
+				protocol->OnNextIdleFrame(L"Redid", [=]()
+				{
+					auto window = GetApplication()->GetMainWindow();
+					auto textBox = FindObjectByName<TTextBox>(window, L"textBox");
+					TEST_ASSERT(textBox->GetText() == L"1234560123456789");
+					TEST_ASSERT(textBox->GetText().IndexOf(L'\r') == -1);
+					TEST_ASSERT(textBox->GetText().IndexOf(L'\n') == -1);
+				});
+
+				protocol->OnNextIdleFrame(L"UndoRedo", [=]()
+				{
+					auto window = GetApplication()->GetMainWindow();
+					window->Hide();
+				});
+			});
+
+			GacUIUnitTest_StartFast_WithResourceAsText<darkskin::Theme>(
+				WString::Unmanaged(L"Controls/Editor/")
+				+ controlName
+				+ WString::Unmanaged(L"/Key/UndoRedo_CtrlZ_UndoesPaste_FlattenedText"),
+				WString::Unmanaged(L"gacuisrc_unittest::MainWindow"),
+				resource
+			);
+		});
+
+		TEST_CASE(L"ClearUndoRedo_DeletesAllHistory")
+		{
+			TooltipTimer timer;
+			GacUIUnitTest_SetGuiMainProxy([=](UnitTestRemoteProtocol* protocol, IUnitTestContext*)
+			{
+				protocol->OnNextIdleFrame(L"Init", [=]()
+				{
+					auto window = GetApplication()->GetMainWindow();
+					auto textBox = FindObjectByName<TTextBox>(window, L"textBox");
+					textBox->SetFocused();
+
+					textBox->LoadTextAndClearUndoRedo(L"");
+					TEST_ASSERT(!textBox->CanUndo());
+					TEST_ASSERT(!textBox->CanRedo());
+				});
+
+				protocol->OnNextIdleFrame(L"Typing", [=]()
+				{
+					auto window = GetApplication()->GetMainWindow();
+					auto textBox = FindObjectByName<TTextBox>(window, L"textBox");
+
+					protocol->TypeString(L"ABC");
+					TEST_ASSERT(textBox->GetText() == L"ABC");
+					TEST_ASSERT(textBox->CanUndo());
+					TEST_ASSERT(!textBox->CanRedo());
+
+					TEST_ASSERT(textBox->Undo());
+					TEST_ASSERT(textBox->GetText() == L"AB");
+					TEST_ASSERT(textBox->CanRedo());
+
+					textBox->ClearUndoRedo();
+					TEST_ASSERT(!textBox->CanUndo());
+					TEST_ASSERT(!textBox->CanRedo());
+
+					TEST_ASSERT(!textBox->Undo());
+					TEST_ASSERT(!textBox->Redo());
+					TEST_ASSERT(textBox->GetText() == L"AB");
+				});
+
+				protocol->OnNextIdleFrame(L"Cleared", [=]()
+				{
+					auto window = GetApplication()->GetMainWindow();
+					window->Hide();
+				});
+			});
+
+			GacUIUnitTest_StartFast_WithResourceAsText<darkskin::Theme>(
+				WString::Unmanaged(L"Controls/Editor/")
+				+ controlName
+				+ WString::Unmanaged(L"/Key/UndoRedo_ClearUndoRedo_DeletesAllHistory"),
+				WString::Unmanaged(L"gacuisrc_unittest::MainWindow"),
+				resource
+			);
+		});
+
+		TEST_CASE(L"UndoRedo_ReachingHistoryEnds_ReturnsFalse_AndStateCorrect")
+		{
+			TooltipTimer timer;
+			GacUIUnitTest_SetGuiMainProxy([=](UnitTestRemoteProtocol* protocol, IUnitTestContext*)
+			{
+				protocol->OnNextIdleFrame(L"Init", [=]()
+				{
+					auto window = GetApplication()->GetMainWindow();
+					auto textBox = FindObjectByName<TTextBox>(window, L"textBox");
+					textBox->SetFocused();
+
+					textBox->LoadTextAndClearUndoRedo(L"");
+				});
+
+				protocol->OnNextIdleFrame(L"Typing", [=]()
+				{
+					auto window = GetApplication()->GetMainWindow();
+					auto textBox = FindObjectByName<TTextBox>(window, L"textBox");
+
+					protocol->TypeString(L"ABC");
+					TEST_ASSERT(textBox->GetText() == L"ABC");
+					TEST_ASSERT(textBox->CanUndo());
+					TEST_ASSERT(!textBox->CanRedo());
+
+					while (textBox->CanUndo())
+					{
+						TEST_ASSERT(textBox->Undo());
+					}
+					TEST_ASSERT(!textBox->CanUndo());
+					TEST_ASSERT(textBox->CanRedo());
+					TEST_ASSERT(textBox->GetText() == L"");
+					TEST_ASSERT(!textBox->Undo());
+
+					while (textBox->CanRedo())
+					{
+						TEST_ASSERT(textBox->Redo());
+					}
+					TEST_ASSERT(!textBox->CanRedo());
+					TEST_ASSERT(textBox->CanUndo());
+					TEST_ASSERT(textBox->GetText() == L"ABC");
+					TEST_ASSERT(!textBox->Redo());
+
+					window->SetText(textBox->GetText());
+				});
+
+				protocol->OnNextIdleFrame(L"Exhausted", [=]()
+				{
+					auto window = GetApplication()->GetMainWindow();
+					window->Hide();
+				});
+			});
+
+			GacUIUnitTest_StartFast_WithResourceAsText<darkskin::Theme>(
+				WString::Unmanaged(L"Controls/Editor/")
+				+ controlName
+				+ WString::Unmanaged(L"/Key/UndoRedo_UndoRedo_ReachingHistoryEnds_ReturnsFalse_AndStateCorrect"),
+				WString::Unmanaged(L"gacuisrc_unittest::MainWindow"),
+				resource
+			);
+		});
+	}
+
 	TEST_CATEGORY(L"Deletion")
 	{
 		TEST_CASE(L"Backspace_DeletesPreviousChar")
