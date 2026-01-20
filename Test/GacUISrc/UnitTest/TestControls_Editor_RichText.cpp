9334b67681ba8ec68592bd741a5695f183b34def
vczh <vczh@outlook.com>
2026-01-20 13:05:29 -0800

    ...

--- Test/GacUISrc/UnitTest/TestControls_Editor_RichText.cpp
+++ Test/GacUISrc/UnitTest/TestControls_Editor_RichText.cpp
@@ -365,27 +365,44 @@ TEST_FILE
 	{
 		TEST_CASE(L"Font_OneCall")
 		{
+			Nullable<WString> baseS0hFace;
+			Nullable<DocumentFontSize> baseS0hSize;
+			Nullable<WString> baseS2tFace;
+			Nullable<DocumentFontSize> baseS2tSize;
+
 			TooltipTimer timer;
-			GacUIUnitTest_SetGuiMainProxy([](UnitTestRemoteProtocol* protocol, IUnitTestContext*)
+			GacUIUnitTest_SetGuiMainProxy([&](UnitTestRemoteProtocol* protocol, IUnitTestContext*)
 			{
-				protocol->OnNextIdleFrame(L"Init", [=]()
+				protocol->OnNextIdleFrame(L"Init", [&, protocol]()
 				{
 					auto window = GetApplication()->GetMainWindow();
 					auto textBox = FindObjectByName<GuiDocumentLabel>(window, L"textBox");
 					textBox->SetFocused();
 					textBox->LoadTextAndClearUndoRedo(L"0123456789\r\n\r\nabcdefghij\r\n\r\nKLMNOPQRST");
+
+					{
+						auto s0h = Summarize(textBox, 0, 0, 2);
+						baseS0hFace = s0h->face;
+						baseS0hSize = s0h->size;
+					}
+					{
+						auto s2t = Summarize(textBox, 2, 5, 10);
+						baseS2tFace = s2t->face;
+						baseS2tSize = s2t->size;
+					}
+
 					textBox->EditStyle(TextPos(0, 2), TextPos(2, 5), MakeFontFaceAndSizeStyle(WString::Unmanaged(L"Arial"), 16));
 				});
 
-				protocol->OnNextIdleFrame(L"Verify", [=]()
+				protocol->OnNextIdleFrame(L"Verify", [&, protocol]()
 				{
 					auto window = GetApplication()->GetMainWindow();
 					auto textBox = FindObjectByName<GuiDocumentLabel>(window, L"textBox");
 
 					{
 						auto s0h = Summarize(textBox, 0, 0, 2);
-						TEST_ASSERT(!s0h->face);
-						TEST_ASSERT(!s0h->size);
+						TEST_ASSERT(s0h->face == baseS0hFace);
+						TEST_ASSERT(s0h->size == baseS0hSize);
 					}
 					{
 						auto s0t = Summarize(textBox, 0, 2, 10);
@@ -410,8 +427,8 @@ TEST_FILE
 					}
 					{
 						auto s2t = Summarize(textBox, 2, 5, 10);
-						TEST_ASSERT(!s2t->face);
-						TEST_ASSERT(!s2t->size);
+						TEST_ASSERT(s2t->face == baseS2tFace);
+						TEST_ASSERT(s2t->size == baseS2tSize);
 					}
 
 					window->Hide();
@@ -426,29 +443,54 @@ TEST_FILE
 		
 		TEST_CASE(L"FontStyle_OneCall")
 		{
+			Nullable<bool> baseS0hBold;
+			Nullable<bool> baseS0hItalic;
+			Nullable<bool> baseS0hUnderline;
+			Nullable<bool> baseS0hStrikeline;
+			Nullable<bool> baseS2tBold;
+			Nullable<bool> baseS2tItalic;
+			Nullable<bool> baseS2tUnderline;
+			Nullable<bool> baseS2tStrikeline;
+
 			TooltipTimer timer;
-			GacUIUnitTest_SetGuiMainProxy([](UnitTestRemoteProtocol* protocol, IUnitTestContext*)
+			GacUIUnitTest_SetGuiMainProxy([&](UnitTestRemoteProtocol* protocol, IUnitTestContext*)
 			{
-				protocol->OnNextIdleFrame(L"Init", [=]()
+				protocol->OnNextIdleFrame(L"Init", [&, protocol]()
 				{
 					auto window = GetApplication()->GetMainWindow();
 					auto textBox = FindObjectByName<GuiDocumentLabel>(window, L"textBox");
 					textBox->SetFocused();
 					textBox->LoadTextAndClearUndoRedo(L"0123456789\r\n\r\nabcdefghij\r\n\r\nKLMNOPQRST");
+
+					{
+						auto s0h = Summarize(textBox, 0, 0, 2);
+						baseS0hBold = s0h->bold;
+						baseS0hItalic = s0h->italic;
+						baseS0hUnderline = s0h->underline;
+						baseS0hStrikeline = s0h->strikeline;
+					}
+					{
+						auto s2t = Summarize(textBox, 2, 5, 10);
+						baseS2tBold = s2t->bold;
+						baseS2tItalic = s2t->italic;
+						baseS2tUnderline = s2t->underline;
+						baseS2tStrikeline = s2t->strikeline;
+					}
+
 					textBox->EditStyle(TextPos(0, 2), TextPos(2, 5), MakeTextStyle(true, true, false, false));
 				});
 
-				protocol->OnNextIdleFrame(L"Verify", [=]()
+				protocol->OnNextIdleFrame(L"Verify", [&, protocol]()
 				{
 					auto window = GetApplication()->GetMainWindow();
 					auto textBox = FindObjectByName<GuiDocumentLabel>(window, L"textBox");
 
 					{
 						auto s0h = Summarize(textBox, 0, 0, 2);
-						TEST_ASSERT(!s0h->bold);
-						TEST_ASSERT(!s0h->italic);
-						TEST_ASSERT(!s0h->underline);
-						TEST_ASSERT(!s0h->strikeline);
+						TEST_ASSERT(s0h->bold == baseS0hBold);
+						TEST_ASSERT(s0h->italic == baseS0hItalic);
+						TEST_ASSERT(s0h->underline == baseS0hUnderline);
+						TEST_ASSERT(s0h->strikeline == baseS0hStrikeline);
 					}
 					{
 						auto s0t = Summarize(textBox, 0, 2, 10);
@@ -485,10 +527,10 @@ TEST_FILE
 					}
 					{
 						auto s2t = Summarize(textBox, 2, 5, 10);
-						TEST_ASSERT(!s2t->bold);
-						TEST_ASSERT(!s2t->italic);
-						TEST_ASSERT(!s2t->underline);
-						TEST_ASSERT(!s2t->strikeline);
+						TEST_ASSERT(s2t->bold == baseS2tBold);
+						TEST_ASSERT(s2t->italic == baseS2tItalic);
+						TEST_ASSERT(s2t->underline == baseS2tUnderline);
+						TEST_ASSERT(s2t->strikeline == baseS2tStrikeline);
 					}
 
 					window->Hide();
@@ -503,26 +545,39 @@ TEST_FILE
 
 		TEST_CASE(L"Color_OneCall")
 		{
+			Nullable<Color> baseS0hColor;
+			Nullable<Color> baseS2tColor;
+
 			TooltipTimer timer;
-			GacUIUnitTest_SetGuiMainProxy([](UnitTestRemoteProtocol* protocol, IUnitTestContext*)
+			GacUIUnitTest_SetGuiMainProxy([&](UnitTestRemoteProtocol* protocol, IUnitTestContext*)
 			{
-				protocol->OnNextIdleFrame(L"Init", [=]()
+				protocol->OnNextIdleFrame(L"Init", [&, protocol]()
 				{
 					auto window = GetApplication()->GetMainWindow();
 					auto textBox = FindObjectByName<GuiDocumentLabel>(window, L"textBox");
 					textBox->SetFocused();
 					textBox->LoadTextAndClearUndoRedo(L"0123456789\r\n\r\nabcdefghij\r\n\r\nKLMNOPQRST");
+
+					{
+						auto s0h = Summarize(textBox, 0, 0, 2);
+						baseS0hColor = s0h->color;
+					}
+					{
+						auto s2t = Summarize(textBox, 2, 5, 10);
+						baseS2tColor = s2t->color;
+					}
+
 					textBox->EditStyle(TextPos(0, 2), TextPos(2, 5), MakeColorStyle(Color(255, 0, 0)));
 				});
 
-				protocol->OnNextIdleFrame(L"Verify", [=]()
+				protocol->OnNextIdleFrame(L"Verify", [&, protocol]()
 				{
 					auto window = GetApplication()->GetMainWindow();
 					auto textBox = FindObjectByName<GuiDocumentLabel>(window, L"textBox");
 
 					{
 						auto s0h = Summarize(textBox, 0, 0, 2);
-						TEST_ASSERT(!s0h->color);
+						TEST_ASSERT(s0h->color == baseS0hColor);
 					}
 					{
 						auto s0t = Summarize(textBox, 0, 2, 10);
@@ -541,7 +596,7 @@ TEST_FILE
 					}
 					{
 						auto s2t = Summarize(textBox, 2, 5, 10);
-						TEST_ASSERT(!s2t->color);
+						TEST_ASSERT(s2t->color == baseS2tColor);
 					}
 
 					window->Hide();
@@ -556,15 +611,51 @@ TEST_FILE
 
 		TEST_CASE(L"AllProperties_OneCall")
 		{
+			Nullable<WString> baseS0hFace;
+			Nullable<DocumentFontSize> baseS0hSize;
+			Nullable<bool> baseS0hBold;
+			Nullable<bool> baseS0hItalic;
+			Nullable<bool> baseS0hUnderline;
+			Nullable<bool> baseS0hStrikeline;
+			Nullable<Color> baseS0hColor;
+			Nullable<WString> baseS2tFace;
+			Nullable<DocumentFontSize> baseS2tSize;
+			Nullable<bool> baseS2tBold;
+			Nullable<bool> baseS2tItalic;
+			Nullable<bool> baseS2tUnderline;
+			Nullable<bool> baseS2tStrikeline;
+			Nullable<Color> baseS2tColor;
+
 			TooltipTimer timer;
-			GacUIUnitTest_SetGuiMainProxy([](UnitTestRemoteProtocol* protocol, IUnitTestContext*)
+			GacUIUnitTest_SetGuiMainProxy([&](UnitTestRemoteProtocol* protocol, IUnitTestContext*)
 			{
-				protocol->OnNextIdleFrame(L"Init", [=]()
+				protocol->OnNextIdleFrame(L"Init", [&, protocol]()
 				{
 					auto window = GetApplication()->GetMainWindow();
 					auto textBox = FindObjectByName<GuiDocumentLabel>(window, L"textBox");
 					textBox->SetFocused();
 					textBox->LoadTextAndClearUndoRedo(L"0123456789\r\n\r\nabcdefghij\r\n\r\nKLMNOPQRST");
+
+					{
+						auto s0h = Summarize(textBox, 0, 0, 2);
+						baseS0hFace = s0h->face;
+						baseS0hSize = s0h->size;
+						baseS0hBold = s0h->bold;
+						baseS0hItalic = s0h->italic;
+						baseS0hUnderline = s0h->underline;
+						baseS0hStrikeline = s0h->strikeline;
+						baseS0hColor = s0h->color;
+					}
+					{
+						auto s2t = Summarize(textBox, 2, 5, 10);
+						baseS2tFace = s2t->face;
+						baseS2tSize = s2t->size;
+						baseS2tBold = s2t->bold;
+						baseS2tItalic = s2t->italic;
+						baseS2tUnderline = s2t->underline;
+						baseS2tStrikeline = s2t->strikeline;
+						baseS2tColor = s2t->color;
+					}
 
 					auto style = Ptr(new DocumentStyleProperties);
 					style->face = WString::Unmanaged(L"Arial");
@@ -577,20 +668,20 @@ TEST_FILE
 					textBox->EditStyle(TextPos(0, 2), TextPos(2, 5), style);
 				});
 
-				protocol->OnNextIdleFrame(L"Verify", [=]()
+				protocol->OnNextIdleFrame(L"Verify", [&, protocol]()
 				{
 					auto window = GetApplication()->GetMainWindow();
 					auto textBox = FindObjectByName<GuiDocumentLabel>(window, L"textBox");
 
 					{
 						auto s0h = Summarize(textBox, 0, 0, 2);
-						TEST_ASSERT(!s0h->face);
-						TEST_ASSERT(!s0h->size);
-						TEST_ASSERT(!s0h->bold);
-						TEST_ASSERT(!s0h->italic);
-						TEST_ASSERT(!s0h->underline);
-						TEST_ASSERT(!s0h->strikeline);
-						TEST_ASSERT(!s0h->color);
+						TEST_ASSERT(s0h->face == baseS0hFace);
+						TEST_ASSERT(s0h->size == baseS0hSize);
+						TEST_ASSERT(s0h->bold == baseS0hBold);
+						TEST_ASSERT(s0h->italic == baseS0hItalic);
+						TEST_ASSERT(s0h->underline == baseS0hUnderline);
+						TEST_ASSERT(s0h->strikeline == baseS0hStrikeline);
+						TEST_ASSERT(s0h->color == baseS0hColor);
 					}
 					{
 						auto s0t = Summarize(textBox, 0, 2, 10);
@@ -645,13 +736,13 @@ TEST_FILE
 					}
 					{
 						auto s2t = Summarize(textBox, 2, 5, 10);
-						TEST_ASSERT(!s2t->face);
-						TEST_ASSERT(!s2t->size);
-						TEST_ASSERT(!s2t->bold);
-						TEST_ASSERT(!s2t->italic);
-						TEST_ASSERT(!s2t->underline);
-						TEST_ASSERT(!s2t->strikeline);
-						TEST_ASSERT(!s2t->color);
+						TEST_ASSERT(s2t->face == baseS2tFace);
+						TEST_ASSERT(s2t->size == baseS2tSize);
+						TEST_ASSERT(s2t->bold == baseS2tBold);
+						TEST_ASSERT(s2t->italic == baseS2tItalic);
+						TEST_ASSERT(s2t->underline == baseS2tUnderline);
+						TEST_ASSERT(s2t->strikeline == baseS2tStrikeline);
+						TEST_ASSERT(s2t->color == baseS2tColor);
 					}
 
 					window->Hide();
@@ -666,21 +757,28 @@ TEST_FILE
 
 		TEST_CASE(L"Overlap_SameProperty_FontFace")
 		{
+			Nullable<WString> baseS2tFace;
+
 			TooltipTimer timer;
-			GacUIUnitTest_SetGuiMainProxy([](UnitTestRemoteProtocol* protocol, IUnitTestContext*)
+			GacUIUnitTest_SetGuiMainProxy([&](UnitTestRemoteProtocol* protocol, IUnitTestContext*)
 			{
-				protocol->OnNextIdleFrame(L"Init", [=]()
+				protocol->OnNextIdleFrame(L"Init", [&, protocol]()
 				{
 					auto window = GetApplication()->GetMainWindow();
 					auto textBox = FindObjectByName<GuiDocumentLabel>(window, L"textBox");
 					textBox->SetFocused();
 					textBox->LoadTextAndClearUndoRedo(L"0123456789\r\n\r\nabcdefghij\r\n\r\nKLMNOPQRST");
 
+					{
+						auto s2t = Summarize(textBox, 2, 9, 10);
+						baseS2tFace = s2t->face;
+					}
+
 					textBox->EditStyle(TextPos(0, 1), TextPos(2, 6), MakeFontFaceStyle(WString::Unmanaged(L"FontA")));
 					textBox->EditStyle(TextPos(1, 4), TextPos(2, 9), MakeFontFaceStyle(WString::Unmanaged(L"FontB")));
 				});
 
-				protocol->OnNextIdleFrame(L"Verify", [=]()
+				protocol->OnNextIdleFrame(L"Verify", [&, protocol]()
 				{
 					auto window = GetApplication()->GetMainWindow();
 					auto textBox = FindObjectByName<GuiDocumentLabel>(window, L"textBox");
@@ -711,7 +809,7 @@ TEST_FILE
 					}
 					{
 						auto s2t = Summarize(textBox, 2, 9, 10);
-						TEST_ASSERT(!s2t->face);
+						TEST_ASSERT(s2t->face == baseS2tFace);
 					}
 
 					window->Hide();
@@ -726,21 +824,33 @@ TEST_FILE
 
 		TEST_CASE(L"Overlap_DifferentProperties_BoldThenColor")
 		{
+			Nullable<Color> baseS1aColor;
+			Nullable<bool> baseS2bBold;
+
 			TooltipTimer timer;
-			GacUIUnitTest_SetGuiMainProxy([](UnitTestRemoteProtocol* protocol, IUnitTestContext*)
+			GacUIUnitTest_SetGuiMainProxy([&](UnitTestRemoteProtocol* protocol, IUnitTestContext*)
 			{
-				protocol->OnNextIdleFrame(L"Init", [=]()
+				protocol->OnNextIdleFrame(L"Init", [&, protocol]()
 				{
 					auto window = GetApplication()->GetMainWindow();
 					auto textBox = FindObjectByName<GuiDocumentLabel>(window, L"textBox");
 					textBox->SetFocused();
 					textBox->LoadTextAndClearUndoRedo(L"0123456789\r\n\r\nabcdefghij\r\n\r\nKLMNOPQRST");
 
+					{
+						auto s1a = Summarize(textBox, 1, 0, 4);
+						baseS1aColor = s1a->color;
+					}
+					{
+						auto s2b = Summarize(textBox, 2, 6, 9);
+						baseS2bBold = s2b->bold;
+					}
+
 					textBox->EditStyle(TextPos(0, 1), TextPos(2, 6), MakeTextStyle(true, false, false, false));
 					textBox->EditStyle(TextPos(1, 4), TextPos(2, 9), MakeColorStyle(Color(255, 0, 0)));
 				});
 
-				protocol->OnNextIdleFrame(L"Verify", [=]()
+				protocol->OnNextIdleFrame(L"Verify", [&, protocol]()
 				{
 					auto window = GetApplication()->GetMainWindow();
 					auto textBox = FindObjectByName<GuiDocumentLabel>(window, L"textBox");
@@ -749,7 +859,7 @@ TEST_FILE
 						auto s1a = Summarize(textBox, 1, 0, 4);
 						TEST_ASSERT(s1a->bold);
 						TEST_ASSERT(s1a->bold.Value() == true);
-						TEST_ASSERT(!s1a->color);
+						TEST_ASSERT(s1a->color == baseS1aColor);
 					}
 					{
 						auto s1b = Summarize(textBox, 1, 4, 10);
@@ -767,7 +877,7 @@ TEST_FILE
 					}
 					{
 						auto s2b = Summarize(textBox, 2, 6, 9);
-						TEST_ASSERT(!s2b->bold);
+						TEST_ASSERT(s2b->bold == baseS2bBold);
 						TEST_ASSERT(s2b->color);
 						TEST_ASSERT(s2b->color.Value() == Color(255, 0, 0));
 					}
@@ -789,21 +899,28 @@ TEST_FILE
 
 		TEST_CASE(L"Overlap_SameProperty_BoldTrueThenFalse")
 		{
+			Nullable<bool> baseS2tBold;
+
 			TooltipTimer timer;
-			GacUIUnitTest_SetGuiMainProxy([](UnitTestRemoteProtocol* protocol, IUnitTestContext*)
+			GacUIUnitTest_SetGuiMainProxy([&](UnitTestRemoteProtocol* protocol, IUnitTestContext*)
 			{
-				protocol->OnNextIdleFrame(L"Init", [=]()
+				protocol->OnNextIdleFrame(L"Init", [&, protocol]()
 				{
 					auto window = GetApplication()->GetMainWindow();
 					auto textBox = FindObjectByName<GuiDocumentLabel>(window, L"textBox");
 					textBox->SetFocused();
 					textBox->LoadTextAndClearUndoRedo(L"0123456789\r\n\r\nabcdefghij\r\n\r\nKLMNOPQRST");
 
+					{
+						auto s2t = Summarize(textBox, 2, 9, 10);
+						baseS2tBold = s2t->bold;
+					}
+
 					textBox->EditStyle(TextPos(0, 1), TextPos(2, 9), MakeTextStyle(true, false, false, false));
 					textBox->EditStyle(TextPos(1, 4), TextPos(2, 6), MakeTextStyle(false, false, false, false));
 				});
 
-				protocol->OnNextIdleFrame(L"Verify", [=]()
+				protocol->OnNextIdleFrame(L"Verify", [&, protocol]()
 				{
 					auto window = GetApplication()->GetMainWindow();
 					auto textBox = FindObjectByName<GuiDocumentLabel>(window, L"textBox");
@@ -835,7 +952,7 @@ TEST_FILE
 					}
 					{
 						auto s2t = Summarize(textBox, 2, 9, 10);
-						TEST_ASSERT(!s2t->bold);
+						TEST_ASSERT(s2t->bold == baseS2tBold);
 					}
 
 					window->Hide();
