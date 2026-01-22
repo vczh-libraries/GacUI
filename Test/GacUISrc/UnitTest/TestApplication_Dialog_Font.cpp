531b9ae50bb153ceec0a90025050a0ab003fd6e8
vczh <vczh@outlook.com>
2026-01-22 09:29:16 -0800

    Begin FontDialog

--- Test/GacUISrc/UnitTest/TestApplication_Dialog_Font.cpp
+++ Test/GacUISrc/UnitTest/TestApplication_Dialog_Font.cpp
@@ -9,6 +9,51 @@ TEST_FILE
   <Instance name="MainWindowResource">
     <Instance ref.Class="gacuisrc_unittest::MainWindow">
       <Window ref.Name="self" Text="Dialog Font Test" ClientSize="x:480 y:320">
+        <FontDialog ref.Name="fontDialog" ShowEffect="true"/>
+        <Table AlignmentToParent="left:0 top:0 right:0 bottom:0" CellPadding="5">
+          <att.Rows>
+            <_>composeType:MinSize</_>
+            <_>composeType:MinSize</_>
+            <_>composeType:MinSize</_>
+            <_>composeType:Percentage percentage:1.0</_>
+          </att.Rows>
+          <att.Columns>
+            <_>composeType:MinSize</_>
+            <_>composeType:Percentage percentage:1.0</_>
+          </att.Columns>
+
+          <Cell Site="row:0 column:0" PreferredMinSize="y:20">
+            <Label ref.Name="labelFont" Text="Sample Text"/>
+          </Cell>
+
+          <Cell Site="row:1 column:0">
+            <Button Text="Change Font Simple">
+              <att.BoundsComposition-set AlignmentToParent="left:0 top:0 right:0 bottom:0"/>
+              <ev.Clicked-eval><![CDATA[{
+                fontDialog.ShowEffect = false;
+                fontDialog.SelectedFont = labelFont.DisplayFont;
+                if (fontDialog.ShowDialog())
+                {
+                  labelFont.Font = fontDialog.SelectedFont;
+                }
+              }]]></ev.Clicked-eval>
+            </Button>
+          </Cell>
+
+          <Cell Site="row:2 column:0">
+            <Button Text="Change Font">
+              <att.BoundsComposition-set AlignmentToParent="left:0 top:0 right:0 bottom:0"/>
+              <ev.Clicked-eval><![CDATA[{
+                fontDialog.ShowEffect = true;
+                fontDialog.SelectedFont = labelFont.DisplayFont;
+                if (fontDialog.ShowDialog())
+                {
+                  labelFont.Font = fontDialog.SelectedFont;
+                }
+              }]]></ev.Clicked-eval>
+            </Button>
+          </Cell>
+        </Table>
       </Window>
     </Instance>
   </Instance>
