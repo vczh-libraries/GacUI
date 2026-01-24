a5f192ed9e960eb6d0fcf433ecd96e6796b9b366
vczh <vczh@outlook.com>
2026-01-23 16:16:16 -0800

    ...

--- Test/GacUISrc/UnitTest/TestApplication_Dialog_File.cpp
+++ Test/GacUISrc/UnitTest/TestApplication_Dialog_File.cpp
@@ -1,5 +1,44 @@
 #include "TestControls.h"
 
+namespace gacui_unittest_template
+{
+	Ptr<FileItemMock> AddFolder(Ptr<FileItemMock> parent, const WString& name)
+	{
+		auto item = Ptr(new FileItemMock);
+		item->isFile = false;
+		parent->children.Add(name, item);
+		return item;
+	}
+
+	Ptr<FileItemMock> AddFile(Ptr<FileItemMock> parent, const WString& name)
+	{
+		auto item = Ptr(new FileItemMock);
+		item->isFile = true;
+		parent->children.Add(name, item);
+		return item;
+	}
+
+	Ptr<FileItemMock> CreateFileItemRoot()
+	{
+		auto root = Ptr(new FileItemMock);
+
+		AddFile(root, L"root.txt");
+		AddFile(root, L"README");
+
+		auto folderA = AddFolder(root, L"A");
+		auto folderB = AddFolder(root, L"B");
+
+		AddFile(folderA, L"a.txt");
+		AddFile(folderA, L"noext");
+
+		auto folderAA = AddFolder(folderA, L"AA");
+		AddFile(folderAA, L"deep.bin");
+
+		AddFolder(folderB, L"BA");
+
+		return root;
+	}
+}
 using namespace gacui_unittest_template;
 
 TEST_FILE
@@ -9,22 +48,25 @@ TEST_FILE
   <Instance name="MainWindowResource">
     <Instance ref.Class="gacuisrc_unittest::MainWindow">
       <Window ref.Name="self" Text="Dialog File Test" ClientSize="x:480 y:320">
+        <OpenFileDialog ref.Name="dialogOpen" Filter="All Files (*.*)|*.*|Text Files *.txt)|*.txt" Directory=""/>
       </Window>
     </Instance>
   </Instance>
 </Resource>
 )GacUISrc";
 
-	TEST_CATEGORY(L"Dialog File")
+	TEST_CATEGORY(L"File Dialog Navigation")
 	{
 		TEST_CASE(L"Open and Close")
 		{
 			GacUIUnitTest_SetGuiMainProxy([](UnitTestRemoteProtocol* protocol, IUnitTestContext*)
 			{
-				protocol->OnNextIdleFrame(L"Ready", [=]()
+				auto fsMock = Ptr(new FileSystemMock(CreateFileItemRoot()));
+				protocol->OnNextIdleFrame(L"Ready", [=, &fsMock]()
 				{
 					auto window = GetApplication()->GetMainWindow();
 					window->Hide();
+					fsMock = {};
 				});
 			});
 			GacUIUnitTest_StartFast_WithResourceAsText<darkskin::Theme>(
