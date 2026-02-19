92f6beb6b6a8c5ce52d5ffa4abd817a69686108b
vczh <vczh@outlook.com>
2026-02-19 15:01:07 -0800

    Update bot.ps1

--- .github/bot.ps1
+++ .github/bot.ps1
@@ -2,7 +2,7 @@ Push-Location $PSScriptRoot\Agent
 try {
     yarn install
     yarn compile
-    start powershell {yarn portal}
+    start powershell {yarn portal --port 9999}
 }
 finally {
     Pop-Location
